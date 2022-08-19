d1e3b31fd8dfb6a909af0820cae252e2b961a6e0
yecksin <yecksin@gmail.com>
2022-08-19 14:52:30 -0500

    :sparkles:  feat(Routing): result-creator routing moved to the results routing parent

--- onecgiar-pr-client/src/app/app-routing.module.ts
+++ onecgiar-pr-client/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResultsModule } from './pages/results/results.module';
 
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'results', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'type-one-report', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'result-creator', loadChildren: () => import('./pages/result-creator/result-creator.module').then(m => m.ResultCreatorModule) },
+  { path: 'results', loadChildren: () => import('./pages/results/results.module').then(m => m.ResultsModule) },
+  { path: 'type-one-report', loadChildren: () => import('./pages/type-one-report/type-one-report.module').then(m => m.TypeOneReportModule) },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
 
--- onecgiar-pr-client/src/app/pages/results/results-routing.module.ts
+++ onecgiar-pr-client/src/app/pages/results/results-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ResultsComponent } from './results.component';
 
-const routes: Routes = [{path:'', component:ResultsComponent}];
+const routes: Routes = [
+  {path:'', component:ResultsComponent, children:[
+    { path: 'result-creator', loadChildren: () => import('./pages/result-creator/result-creator.module').then(m => m.ResultCreatorModule) },
+
+  ]}]
+  ;
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
