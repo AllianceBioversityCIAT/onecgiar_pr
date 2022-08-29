a94e3d500b769a7150dc42db63f423a2929af691
yecksin <yecksin@gmail.com>
2022-08-29 15:36:37 -0500

    :sparkles: feat(Components): Scroll event added and and fix the navigation bar

--- onecgiar-pr-client/src/app/shared/components/navigation-bar/navigation-bar.component.ts
+++ onecgiar-pr-client/src/app/shared/components/navigation-bar/navigation-bar.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation-bar.component.scss']
 })
 export class NavigationBarComponent implements OnInit {
-
+  navbar_fixed = false;
   constructor() { }
 
   ngOnInit(): void {
+    window.addEventListener('scroll', (e)=> {
+      // console.log(e);
+      let scrollTopValue = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+      console.log(scrollTopValue);
+      if (scrollTopValue > 100) {
+        this.navbar_fixed = true;
+      }else{
+        this.navbar_fixed = false;
+      }
+      // console.log(document.getElementById('sdsdsdsd')?.getBoundingClientRect().top);
+    })
   }
 
 }
