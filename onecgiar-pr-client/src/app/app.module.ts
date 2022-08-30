12d651a2b8b723dfd7bfbb5c8c71419e441ef115
yecksin <yecksin@gmail.com>
2022-08-30 08:57:13 -0500

    :art: feat(Font-icons): header-panel styles and information added

--- onecgiar-pr-client/src/app/app.module.ts
+++ onecgiar-pr-client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './shared/components/navigation-bar/navigation-bar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderPanelComponent } from './shared/components/header-panel/header-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderPanelComponent
   ],
   imports: [
     BrowserModule,
