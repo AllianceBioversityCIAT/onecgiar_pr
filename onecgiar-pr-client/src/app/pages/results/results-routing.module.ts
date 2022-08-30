b17253b6906e76ba6431351b1e42249407b19f40
yecksin <yecksin@gmail.com>
2022-08-30 16:15:04 -0500

    :sparkles: feat(Routing): Open result-detail with id in route

--- onecgiar-pr-client/src/app/pages/results/results-routing.module.ts
+++ onecgiar-pr-client/src/app/pages/results/results-routing.module.ts
@@ -5,7 +5,7 @@ import { ResultsComponent } from './results.component';
 const routes: Routes = [
   {path:'', component:ResultsComponent, children:[
     { path: 'result-creator', loadChildren: () => import('./pages/result-creator/result-creator.module').then(m => m.ResultCreatorModule) },
-    { path: 'result-detail', loadChildren: () => import('./pages/result-detail/result-detail.module').then(m => m.ResultDetailModule) },
+    { path: 'result-detail/:id', loadChildren: () => import('./pages/result-detail/result-detail.module').then(m => m.ResultDetailModule) },
     { path: 'results-list', loadChildren: () => import('./pages/results-list/results-list.module').then(m => m.ResultsListModule) },
     { path: '**', pathMatch: 'full', redirectTo: 'results-list' },
   ]}]
