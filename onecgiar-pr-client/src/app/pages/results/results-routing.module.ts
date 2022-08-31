72a296a05bf9395beb6dd21cf3aecb9af6aad31f
yecksin <yecksin@gmail.com>
2022-08-31 10:22:39 -0500

    :art: feat(Scss): General styles improved

--- onecgiar-pr-client/src/app/pages/results/results-routing.module.ts
+++ onecgiar-pr-client/src/app/pages/results/results-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ResultsComponent } from './results.component';
 
 const routes: Routes = [
-  {path:'', component:ResultsComponent, children:[
-    { path: 'result-creator', loadChildren: () => import('./pages/result-creator/result-creator.module').then(m => m.ResultCreatorModule) },
-    { path: 'result-detail/:id', loadChildren: () => import('./pages/result-detail/result-detail.module').then(m => m.ResultDetailModule) },
-    { path: 'results-list', loadChildren: () => import('./pages/results-list/results-list.module').then(m => m.ResultsListModule) },
-    { path: '**', pathMatch: 'full', redirectTo: 'results-list' },
-  ]}]
-  ;
+  {
+    path: '', component: ResultsComponent, children: [
+      { path: 'result-creator', loadChildren: () => import('./pages/result-creator/result-creator.module').then(m => m.ResultCreatorModule) },
+      { path: 'result-detail/:id', loadChildren: () => import('./pages/result-detail/result-detail.module').then(m => m.ResultDetailModule) },
+      { path: 'results-list', loadChildren: () => import('./pages/results-list/results-list.module').then(m => m.ResultsListModule) },
+      { path: '**', pathMatch: 'full', redirectTo: 'results-list' },
+    ]
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
