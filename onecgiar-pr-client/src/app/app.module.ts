77c463944e23a83e762ab9618dcaf5c7754e142b
yecksin <yecksin@gmail.com>
2022-09-19 16:21:47 -0500

    :sparkles: feat(Login): http config added in the app module

--- onecgiar-pr-client/src/app/app.module.ts
+++ onecgiar-pr-client/src/app/app.module.ts
@@ -9,10 +9,11 @@ import { HeaderPanelComponent } from './shared/components/header-panel/header-pa
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UtilsComponentsModule } from './shared/components/utils-components/utils-components.module';
 import { ExternalToolsComponent } from './shared/components/external-tools/external-tools.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NavigationBarComponent, FooterComponent, HeaderPanelComponent, ExternalToolsComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, UtilsComponentsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, UtilsComponentsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
