eb8643e97ca0f2b84e2472387c874bd79d8ca273
yecksin <yecksin@gmail.com>
2022-09-29 10:25:19 -0500

    :sparkles: feat(Internationalization File): InternationalizationData.ts created to add platform information

---
+++ onecgiar-pr-client/src/app/shared/data/internationalizationData.ts
@@ -0,0 +1,7 @@
+export const internationalizationData: InternationalizationData = {
+  platformName: 'PRMS Reporting tool'
+};
+
+export interface InternationalizationData {
+  platformName: string;
+}
