f2ea3d00bb48f202369a8eda7c044331465959d2
yecksin <yecksin@gmail.com>
2022-09-29 10:26:28 -0500

    :sparkles: feat(Auth): Manage token and user information with localStorage

--- onecgiar-pr-client/src/app/shared/services/auth.service.ts
+++ onecgiar-pr-client/src/app/shared/services/auth.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { UserAuth } from '../interfaces/user';
+import { LocalStorageUser, UserAuth } from '../interfaces/user';
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   inLogin = false;
-  constructor(public http: HttpClient) {}
+  constructor(public http: HttpClient, private router: Router) {}
 
   userAuth(body: UserAuth) {
     return this.http.post<any>(`${environment.apiBaseUrl}auth/singin`, body);
+  }
+
+  getAllUsers() {
+    return this.http.get<any>(`${environment.apiBaseUrl}auth/user/all`);
   }
 
   set localStorageToken(token: string) {
@@ -25,7 +30,13 @@ export class AuthService {
     localStorage.setItem('user', JSON.stringify(user));
   }
 
-  get localStorageUser() {
-    return JSON.parse(localStorage.getItem('token'));
+  get localStorageUser(): LocalStorageUser {
+    // console.log('localStorageUser');
+    return JSON.parse(localStorage.getItem('user'));
+  }
+
+  logout() {
+    this.router.navigate(['/login']);
+    localStorage.clear();
   }
 }
