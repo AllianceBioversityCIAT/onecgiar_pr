486da6ba802939c92be1b6088928fe575395f0e3
yecksin <yecksin@gmail.com>
2022-10-06 16:16:04 -0500

    :recycle: refactor(Texts): login and internationalizationData some text fixed

--- onecgiar-pr-client/src/app/shared/data/internationalizationData.ts
+++ onecgiar-pr-client/src/app/shared/data/internationalizationData.ts
@@ -5,13 +5,21 @@ export const internationalizationData = {
   home: {
     a: 'Welcome to the PRMS Reporting tool. According to your profile, you can report for the Initiative(s):',
     b: 'Welcome to the PRMS Reporting tool. As a guest user you are welcome to explore the results reported at the links below.',
-    c: 'Welcome to the PRMS Reporting tool. As per your profile, you have an admin account, which provides you with full access to the tool and full editing rights.'
+    c: 'Welcome to the PRMS Reporting tool. As per your profile, you have an admin account, which provides you with full access to the tool and full editing rights.',
+
+    alerts: {}
+  },
+  resultsList: {
+    alerts: {
+      info: 'Remember that there is a possibility that the result you want to report has already been entered into the PRMS Reporting tool. If this is the case, it is only necessary to map the result to your Initiative. There is no need to enter it as a new result. Please use the results table below to ensure that your result has not been previously entered.'
+    }
+  },
+  reportNewResult: {
+    greeting: (name: string) => `Hello ${name}, you can report for INIT-17 and INIT-28. If you would like to report for another Initiative, please contact Project Coordination Unit.`,
+    alerts: {
+      info: 'Remember that there is a possibility that the result you want to report has already been entered into the PRMS Reporting tool. If this is the case, it is only necessary to map the result to your Initiative. There is no need to enter it as a new result. Please check the results section to see if your result has been previously entered.'
+    }
   }
 };
-
-export interface InternationalizationData {
-  global: any;
-  home: any;
-}
 
 // platformName: string;
