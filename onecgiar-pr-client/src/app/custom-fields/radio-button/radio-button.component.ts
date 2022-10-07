717555ce9b2e0fbd7e126a620b6dc3613ea0c8d2
yecksin <yecksin@gmail.com>
2022-10-06 19:26:30 -0500

    :sparkles: feat(Component): Custom radio-button component created and implement

---
+++ onecgiar-pr-client/src/app/custom-fields/radio-button/radio-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-radio-button',
+  templateUrl: './radio-button.component.html',
+  styleUrls: ['./radio-button.component.scss']
+})
+export class RadioButtonComponent {
+  @Input() options: any;
+  @Input() optionLabel: string;
+
+  nnsssd: string;
+  constructor() {}
+}
