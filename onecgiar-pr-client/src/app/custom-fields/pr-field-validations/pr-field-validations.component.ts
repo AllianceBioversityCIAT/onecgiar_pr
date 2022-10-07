714219408cc4a2e96a03a9fe028385368230d496
yecksin <yecksin@gmail.com>
2022-10-06 22:34:01 -0500

    :sparkles: feat(Component): pr-field-validations component added and implemented

---
+++ onecgiar-pr-client/src/app/custom-fields/pr-field-validations/pr-field-validations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pr-field-validations',
+  templateUrl: './pr-field-validations.component.html',
+  styleUrls: ['./pr-field-validations.component.scss']
+})
+export class PrFieldValidationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
