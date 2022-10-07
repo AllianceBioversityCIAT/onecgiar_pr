ca97bcb9286e88b75a470ae30be15a55f7e0298b
Juan Delgado <juandelgadog98@gmail.com>
2022-10-07 09:22:50 -0500

    :sparkles: feat(migration): insert data on year table

---
+++ onecgiar-pr-server/src/migrations/1665152438076-yearInsert.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class yearInsert1665152438076 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const year = await queryRunner.query(`INSERT IGNORE INTO \`year\` (year) VALUES ('2022')`);
+        await queryRunner.query(`INSERT IGNORE INTO \`year\` (year) VALUES ('2023')`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
