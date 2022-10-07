2b9b0200ee0684aefa132291b605fcd95b13e7f6
yecksin <yecksin@gmail.com>
2022-10-06 19:31:02 -0500

    :sparkles: feat(Component): Result creator results styles and front added

--- onecgiar-pr-client/src/app/pages/results/pages/result-creator/result-creator.component.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-creator/result-creator.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { internationalizationData } from '../../../../shared/data/internationalizationData';
+import { ApiService } from '../../../../shared/services/api/api.service';
+import { ResultLevelService } from './services/result-level.service';
 
 @Component({
   selector: 'app-result-creator',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./result-creator.component.scss']
 })
 export class ResultCreatorComponent {
-  constructor() {}
+  naratives = internationalizationData.reportNewResult;
+  some: any;
+  constructor(private api: ApiService, public resultLevelSE: ResultLevelService) {}
+  initiatives = [
+    { name: 'INIT-17 SAPLING', code: 1 },
+    { name: 'INIT-28 Nexus Gains', code: 2 }
+  ];
+  ngOnInit(): void {
+    this.api.alertsFs.show({
+      id: 'indoasd',
+      status: 'success',
+      title: '',
+      description: this.naratives.alerts.info,
+      querySelector: '.report_container'
+    });
+  }
 }
--- onecgiar-pr-client/src/app/pages/results/pages/result-creator/result-creator.module.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-creator/result-creator.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ResultCreatorRoutingModule } from './result-creator-routing.module';
 import { ResultCreatorComponent } from './result-creator.component';
 import { UtilsComponentsModule } from '../../../../shared/components/utils-components/utils-components.module';
+import { CustomFieldsModule } from '../../../../custom-fields/custom-fields.module';
+import { FormsModule } from '@angular/forms';
+import { ResultLevelButtonsComponent } from './components/result-level-buttons/result-level-buttons.component';
 
 @NgModule({
-  declarations: [ResultCreatorComponent],
-  imports: [CommonModule, ResultCreatorRoutingModule, UtilsComponentsModule]
+  declarations: [ResultCreatorComponent, ResultLevelButtonsComponent],
+  imports: [CommonModule, ResultCreatorRoutingModule, UtilsComponentsModule, CustomFieldsModule, FormsModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ResultCreatorModule {}
