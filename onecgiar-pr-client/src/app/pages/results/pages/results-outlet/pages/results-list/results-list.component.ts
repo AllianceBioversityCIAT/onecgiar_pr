702f4d1a5e5ea6ad8bc16359d943eb10111e25eb
yecksin <yecksin@gmail.com>
2022-10-10 11:25:57 -0500

    :recycle: refactor(Component):  reported_year_id changed to reported_year

--- onecgiar-pr-client/src/app/pages/results/pages/results-outlet/pages/results-list/results-list.component.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/results-outlet/pages/results-list/results-list.component.ts
@@ -15,7 +15,7 @@ export class ResultsListComponent implements OnInit {
   columnOrder = [
     { title: 'ID', attr: 'id' },
     { title: 'Title', attr: 'title', class: 'notCenter' },
-    { title: 'Reported year', attr: 'reported_year_id' },
+    { title: 'Reported year', attr: 'reported_year' },
     { title: 'Result type', attr: 'result_type' },
     { title: 'Submitter', attr: 'submitter' },
     { title: 'Status', attr: 'status_name' },
@@ -47,6 +47,7 @@ export class ResultsListComponent implements OnInit {
     });
 
     this.api.resultsSE.getAllResults().subscribe(resp => {
+      console.log(resp.response);
       this.resultsList = resp.response;
     });
   }
