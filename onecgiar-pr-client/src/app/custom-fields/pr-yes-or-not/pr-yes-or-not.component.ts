7c384cfae03882350071a3853acb6d48ccea12d4
yecksin <yecksin@gmail.com>
2022-10-18 21:59:00 -0500

    :recycle: refactor(pr-yes-or-not component): pr-yes-or-not field component created

---
+++ onecgiar-pr-client/src/app/custom-fields/pr-yes-or-not/pr-yes-or-not.component.ts
@@ -0,0 +1,55 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-pr-yes-or-not',
+  templateUrl: './pr-yes-or-not.component.html',
+  styleUrls: ['./pr-yes-or-not.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PrYesOrNotComponent),
+      multi: true
+    }
+  ]
+})
+export class PrYesOrNotComponent {
+  @Input() label: string;
+  @Input() description: string;
+  @Input() readOnly: boolean;
+  @Input() required: boolean = true;
+  private _value: boolean;
+
+  get value() {
+    return this._value;
+  }
+
+  set value(v: boolean) {
+    if (v !== this._value) {
+      this._value = v;
+      this.onChange(v);
+    }
+  }
+
+  onChange(_) {}
+
+  onTouch() {}
+
+  writeValue(value: any): void {
+    this._value = value;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  onclickYes() {
+    this.value = true;
+  }
+
+  onClickNo() {
+    this.value = false;
+  }
+}
