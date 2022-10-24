45563d4a8845e340c3fd68ba52445a807650abfc
yecksin <yecksin@gmail.com>
2022-10-24 00:12:00 -0500

    :sparkles: feat (Service): General information services added

--- onecgiar-pr-client/src/app/shared/services/api/results-api.service.ts
+++ onecgiar-pr-client/src/app/shared/services/api/results-api.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { ResultBody } from '../../interfaces/result';
 import { map } from 'rxjs';
+import { ResultBody } from '../../interfaces/result.interface';
+import { GeneralInfoBody } from '../../../pages/results/pages/result-detail/pages/rd-general-information/models/generalInfoBody';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,14 @@ import { map } from 'rxjs';
 export class ResultsApiService {
   constructor(public http: HttpClient) {}
   apiBaseUrl = environment.apiBaseUrl + 'api/results/';
+  currentResultId: number | string = null;
   GET_AllResultLevel() {
     return this.http.get<any>(`${this.apiBaseUrl}levels/all`);
   }
   GET_TypeByResultLevel() {
     return this.http.get<any>(`${this.apiBaseUrl}type-by-level/get/all`);
   }
-  getAllResults() {
+  GET_AllResults() {
     return this.http.get<any>(`${this.apiBaseUrl}get/all`);
   }
   GET_AllResultsWithUseRole(userId) {
@@ -29,5 +31,24 @@ export class ResultsApiService {
   }
   POST_resultCreateHeader(body: ResultBody) {
     return this.http.post<any>(`${this.apiBaseUrl}create/header`, body);
+  }
+
+  GET_allGenderTag() {
+    return this.http.get<any>(`${this.apiBaseUrl}gender-tag-levels/all`);
+  }
+
+  GET_institutionTypes() {
+    return this.http.get<any>(`${this.apiBaseUrl}get/institutions-type/all`);
+  }
+
+  GET_allInstitutions() {
+    return this.http.get<any>(`${this.apiBaseUrl}get/institutions/all`);
+  }
+
+  GET_generalInformationByResultId() {
+    return this.http.get<any>(`${this.apiBaseUrl}get/general-information/result/${this.currentResultId}`);
+  }
+  PATCH_generalInformation(body: GeneralInfoBody) {
+    return this.http.patch<any>(`${this.apiBaseUrl}create/general-information`, body);
   }
 }
