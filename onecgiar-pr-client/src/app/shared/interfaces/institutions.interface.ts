7276658655a08106d1f16bbcb437cd7a59e739ca
yecksin <yecksin@gmail.com>
2022-10-24 00:06:07 -0500

    :sparkles: feat (Interfaces): Score and institutions interfaces created

---
+++ onecgiar-pr-client/src/app/shared/interfaces/institutions.interface.ts
@@ -0,0 +1,12 @@
+interface Institution {
+  code: number;
+  name: string;
+}
+
+interface InstitutionType {
+  acronym: string;
+  id: number;
+  institution_type_code: number;
+  name: string;
+  website_link: string;
+}
