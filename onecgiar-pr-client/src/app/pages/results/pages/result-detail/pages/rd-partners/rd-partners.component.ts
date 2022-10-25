d145155614d71342070fe7a6aafa5e33e1f67826
yecksin <yecksin@gmail.com>
2022-10-24 23:11:43 -0500

    :sparkles: feat(component): Partners multi select, checkbox and alerts added

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-partners/rd-partners.component.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-partners/rd-partners.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../../../../../shared/services/api/api.service';
+import { InstitutionsService } from '../../../../../../shared/services/global/institutions.service';
+import { PartnersBody } from './models/partnersBody';
 
 @Component({
   selector: 'app-rd-partners',
@@ -7,7 +9,9 @@ import { ApiService } from '../../../../../../shared/services/api/api.service';
   styleUrls: ['./rd-partners.component.scss']
 })
 export class RdPartnersComponent {
-  constructor(private api: ApiService) {}
+  partnersBody = new PartnersBody();
+
+  constructor(private api: ApiService, public institutionsSE: InstitutionsService) {}
   checkboxExample = null;
   ngOnInit(): void {
     this.showAlerts();
@@ -18,6 +22,13 @@ export class RdPartnersComponent {
       title: 'sd',
       description: 'Partner organization that you collaborated with to generate this result or that contributed to this result.',
       querySelector: '.detail_container',
+      position: 'afterbegin'
+    });
+    this.api.alertsFs.show({
+      status: 'success',
+      title: 'sd',
+      description: `If you don't find the partner you are looking for, <a href="">request</a> to have it added to the list.`,
+      querySelector: '.partnerRequestAlert',
       position: 'afterbegin'
     });
   }
