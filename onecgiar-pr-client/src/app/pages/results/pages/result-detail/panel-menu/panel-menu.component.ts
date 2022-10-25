cd970efeef1bef2a9b1806904652bd749d67dcfe
yecksin <yecksin@gmail.com>
2022-10-25 08:56:32 -0500

    :recycle: refactor(Routing): Send and get result id fixed

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/panel-menu/panel-menu.component.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/panel-menu/panel-menu.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PrRoute, resultDetailRouting } from '../../../../../shared/routing/routing-data';
 import { ResultLevelService } from '../../result-creator/services/result-level.service';
+import { ResultsApiService } from '../../../../../shared/services/api/results-api.service';
 
 @Component({
   selector: 'app-panel-menu',
@@ -10,5 +11,5 @@ import { ResultLevelService } from '../../result-creator/services/result-level.s
 export class PanelMenuComponent {
   navigationOptions: PrRoute[] = resultDetailRouting;
 
-  constructor(public ResultLevelSE: ResultLevelService) {}
+  constructor(public resultLevelSE: ResultLevelService, public resultsListSE: ResultsApiService) {}
 }
