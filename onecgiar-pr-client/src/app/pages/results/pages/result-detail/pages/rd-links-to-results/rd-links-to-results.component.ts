8110205ebbcaf624318a3dff6b351b9923d4b9ba
yecksin <yecksin@gmail.com>
2022-11-01 09:39:03 -0500

    :sparkles: feat(links-to-results): links-to-results front developed

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-links-to-results/rd-links-to-results.component.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-links-to-results/rd-links-to-results.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../../../../../shared/services/api/api.service';
+import { ResultsListService } from '../../../results-outlet/pages/results-list/services/results-list.service';
 
 @Component({
   selector: 'app-rd-links-to-results',
@@ -6,12 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./rd-links-to-results.component.scss']
 })
 export class RdLinksToResultsComponent {
-  constructor() {}
+  constructor(public api: ApiService, public resultsListService: ResultsListService) {}
+  results_linked: any[] = [];
+  text_to_search: string = '';
+  columnOrder = [
+    // { title: 'ID', attr: 'id' },
+    { title: 'Title', attr: 'title', class: 'notCenter' },
+    // { title: 'Reporting year', attr: 'reported_year' },
+    { title: 'Result type', attr: 'result_type' },
+    { title: 'Submitter', attr: 'submitter' },
+    { title: 'Status', attr: 'status_name' },
+    { title: 'Creation date	', attr: 'created_date' }
+  ];
+
   ngOnInit(): void {
-    this.showAlerts();
+    this.api.updateResultsList();
     this.getSectionInformation();
+  }
+  onLinkResult(result) {
+    console.log(result);
+    this.results_linked.push(result);
   }
   getSectionInformation() {}
   onSaveSection() {}
-  showAlerts() {}
 }
--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-links-to-results/rd-links-to-results.module.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-links-to-results/rd-links-to-results.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { RdLinksToResultsRoutingModule } from './rd-links-to-results-routing.module';
 import { RdLinksToResultsComponent } from '../rd-links-to-results/rd-links-to-results.component';
+import { CustomFieldsModule } from '../../../../../../custom-fields/custom-fields.module';
+import { TableModule } from 'primeng/table';
+import { ResultsListFilterPipe } from '../../../results-outlet/pages/results-list/pipes/results-list-filter.pipe';
 
 @NgModule({
-  declarations: [RdLinksToResultsComponent],
-  imports: [CommonModule, RdLinksToResultsRoutingModule]
+  declarations: [RdLinksToResultsComponent, ResultsListFilterPipe],
+  imports: [CommonModule, RdLinksToResultsRoutingModule, CustomFieldsModule, TableModule]
 })
 export class RdLinksToResultsModule {}
