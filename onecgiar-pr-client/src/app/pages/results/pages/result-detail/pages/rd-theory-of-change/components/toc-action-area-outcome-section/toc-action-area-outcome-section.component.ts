6f8a823cde3dac83630e03044a81dd1c7d01a70f
yecksin <yecksin@gmail.com>
2022-11-03 23:40:18 -0500

    :sparkles: feat(theory-of-change): Components for the 4 variations of the toc form were created.

---
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-theory-of-change/components/toc-action-area-outcome-section/toc-action-area-outcome-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toc-action-area-outcome-section',
+  templateUrl: './toc-action-area-outcome-section.component.html',
+  styleUrls: ['./toc-action-area-outcome-section.component.scss']
+})
+export class TocActionAreaOutcomeSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-theory-of-change/components/toc-initiative-output-section/toc-initiative-output-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toc-initiative-output-section',
+  templateUrl: './toc-initiative-output-section.component.html',
+  styleUrls: ['./toc-initiative-output-section.component.scss']
+})
+export class TocInitiativeOutputSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
