9e95cff01c889745d1fd12934fa05272976cf765
yecksin <yecksin@gmail.com>
2022-11-07 16:25:28 -0500

    :sparkles: feat(toc-action-area-outcome-section): Front section developed

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-theory-of-change/components/toc-action-area-outcome-section/toc-action-area-outcome-section.component.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-theory-of-change/components/toc-action-area-outcome-section/toc-action-area-outcome-section.component.ts
@@ -7,4 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TocActionAreaOutcomeSectionComponent {
   constructor() {}
+  value;
+  lista = [{ name: 'Resilient Agrifood Systems - </strong>NARES and Regional Agricultural Research Institutes develop farming system innovations with the potential to increase the food security of smallholders in targeted areas. ' }];
+  listb = [{ name: 'Resilient Agrifood Systems - </strong>NARES and Regional Agricultural Research Institutes develop farming system innovations with the potential to increase the food security of smallholders in targeted areas. ' }];
 }
