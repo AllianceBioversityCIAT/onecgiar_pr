66e3adc4a6c5604e778316523830faf0f9f7eb5b
yecksin <yecksin@gmail.com>
2022-11-09 14:52:02 -0500

    :sparkles: feat(Toc):  Consume Action Area Outcome

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/result-detail.component.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/result-detail.component.ts
@@ -19,6 +19,7 @@ export class ResultDetailComponent {
 
   GET_resultById() {
     this.api.resultsSE.GET_resultById().subscribe(({ response }) => {
+      console.log(response);
       this.rolesSE.validateReadOnly(response);
       this.resultLevelSE.currentResultLevelName = response.result_level_name;
       this.resultLevelSE.currentResultLevelId = response.result_level_id;
