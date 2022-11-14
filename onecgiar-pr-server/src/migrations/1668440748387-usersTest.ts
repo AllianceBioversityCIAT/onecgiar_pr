2edbaf79987c7d4c70370e60cb0a3ebe9fcf6aeb
Juan Delgado <juandelgadog98@gmail.com>
2022-11-14 10:53:13 -0500

    :recycle: refactor(migration users): change users migration name

--- onecgiar-pr-server/src/migrations/1668440748387-usersTest.ts
+++ onecgiar-pr-server/src/migrations/1668440748387-usersTest.ts
@@ -1,8 +1,45 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class migrations1668440748387 implements MigrationInterface {
+export class usersTest1668440748387 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`INSERT INTO
+        \`prdb\`.\`users\` (
+            id,
+            first_name,
+            last_name,
+            email,
+            password,
+            is_cgiar
+        )
+    VALUES
+        (
+            2,
+            'Yeckzin',
+            'Zuñiga',
+            'y.zuniga@cgiar.org',
+            NULL,
+            1
+        );`);
+
+        await queryRunner.query(`INSERT INTO
+        \`prdb\`.\`users\` (
+            id,
+            first_name,
+            last_name,
+            email,
+            password,
+            is_cgiar
+        )
+    VALUES
+        (
+            3,
+            'Hector',
+            'Tobon',
+            'h.f.tobon@cgiar.org',
+            NULL,
+            1
+        );`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
