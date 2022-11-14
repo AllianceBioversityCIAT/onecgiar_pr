d93e2a11cb258ad9d550f91ce035c85048c45871
Juan Delgado <juandelgadog98@gmail.com>
2022-11-14 11:23:18 -0500

    :recycle: refactor(migration result by init): added missed init

--- onecgiar-pr-server/src/migrations/1668442157335-resultsByInitiativeTest.ts
+++ onecgiar-pr-server/src/migrations/1668442157335-resultsByInitiativeTest.ts
@@ -3,6 +3,25 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class resultsByInitiativeTest1668442157335 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`INSERT INTO
+        \`prdb\`.\`result\` (
+                                       id,
+                                        version_id,
+                                        result_type_id,
+                                        created_by,
+                                        result_level_id,
+                                        title
+                                    )
+                                VALUES
+                                    (
+                                        54,
+                                        1,
+                                        1,
+                                        307,
+                                        2,
+                                        'Set of training manuals for improving the quality and safety of informal dairy and pork value chains in Assam (India)'
+                                    );
+        `);
         await queryRunner.query(`INSERT INTO \`results_by_inititiative\` (result_id,inititiative_id,initiative_role_id,version_id,created_by) VALUES (1,1,1,1,307)`);
         await queryRunner.query(`INSERT INTO \`results_by_inititiative\` (result_id,inititiative_id,initiative_role_id,version_id,created_by) VALUES (2,1,1,1,307)`);
         await queryRunner.query(`INSERT INTO \`results_by_inititiative\` (result_id,inititiative_id,initiative_role_id,version_id,created_by) VALUES (3,2,1,1,307)`);
