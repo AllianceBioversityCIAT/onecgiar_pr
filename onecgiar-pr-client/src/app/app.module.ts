b9cece907843eca82dd82ec92187a945861a3774
Santiago Gálvez <santiagogalvez17@gmail.com>
2022-11-15 01:21:42 -0500

    :sparkles: feat(tawkto): add tawkto

--- onecgiar-pr-client/src/app/app.module.ts
+++ onecgiar-pr-client/src/app/app.module.ts
@@ -12,9 +12,10 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { GeneralInterceptorService } from './shared/interceptors/general-interceptor.service';
 import { TestEnvironmentLabelComponent } from './shared/components/test-environment-label/test-environment-label.component';
 import { CustomFieldsModule } from './custom-fields/custom-fields.module';
+import { TawkComponent } from './shared/components/tawk/tawk.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationBarComponent, FooterComponent, HeaderPanelComponent, ExternalToolsComponent, TestEnvironmentLabelComponent],
+  declarations: [AppComponent, NavigationBarComponent, FooterComponent, HeaderPanelComponent, ExternalToolsComponent, TestEnvironmentLabelComponent, TawkComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule, CustomFieldsModule],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: GeneralInterceptorService, multi: true }],
   bootstrap: [AppComponent]
