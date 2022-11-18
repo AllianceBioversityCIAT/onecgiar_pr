9341c5093c7de26447b4cf1edf4f6af7534d1709
yecksin <yecksin@gmail.com>
2022-11-18 18:34:06 -0500

    :recycle: refactor(knowledge-product): Fair circles added

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-result-types-pages/knowledge-product-info/knowledge-product-info.module.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-result-types-pages/knowledge-product-info/knowledge-product-info.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { KnowledgeProductInfoRoutingModule } from './knowledge-product-info-routing.module';
 import { KnowledgeProductInfoComponent } from './knowledge-product-info.component';
 import { CustomFieldsModule } from '../../../../../../../custom-fields/custom-fields.module';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [KnowledgeProductInfoComponent],
-  imports: [CommonModule, KnowledgeProductInfoRoutingModule, CustomFieldsModule]
+  imports: [CommonModule, KnowledgeProductInfoRoutingModule, CustomFieldsModule, NgCircleProgressModule.forRoot()]
 })
 export class KnowledgeProductInfoModule {}
