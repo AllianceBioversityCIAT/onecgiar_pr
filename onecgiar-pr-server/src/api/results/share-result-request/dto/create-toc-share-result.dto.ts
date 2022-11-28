ddf9196ba796013e211ed32023b29625964effe4
davinzifc <47566127+davinzifc@users.noreply.github.com>
2022-11-28 13:10:21 -0500

    :recycle: refactor(share): added the extra column in result share

--- onecgiar-pr-server/src/api/results/share-result-request/dto/create-toc-share-result.dto.ts
+++ onecgiar-pr-server/src/api/results/share-result-request/dto/create-toc-share-result.dto.ts
@@ -2,5 +2,6 @@ export class CreateTocShareResult{
     public initiativeShareId: number[];
     public toc_result_id!: number;
     public action_area_outcome_id!: number;
+    public planned_result!: number;
     public isToc: boolean;
 }
