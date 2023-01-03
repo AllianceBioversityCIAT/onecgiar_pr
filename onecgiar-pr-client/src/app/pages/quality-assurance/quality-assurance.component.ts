9258d23a6f13bb6cbb4c1e4a12637cbda6ef3779
yecksin <yecksin@gmail.com>
2023-01-03 14:32:17 -0500

    :sparkles: feat(Quality Assurance): Module and Component added

---
+++ onecgiar-pr-client/src/app/pages/quality-assurance/quality-assurance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quality-assurance',
+  templateUrl: './quality-assurance.component.html',
+  styleUrls: ['./quality-assurance.component.scss']
+})
+export class QualityAssuranceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ onecgiar-pr-client/src/app/pages/quality-assurance/quality-assurance.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QualityAssuranceComponent } from './quality-assurance.component';
+
+
+
+@NgModule({
+  declarations: [
+    QualityAssuranceComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class QualityAssuranceModule { }
