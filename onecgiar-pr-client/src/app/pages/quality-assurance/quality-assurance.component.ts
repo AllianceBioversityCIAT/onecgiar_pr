dac46585bf60130fd029b9d46b857debc97f92e1
yecksin <yecksin@gmail.com>
2023-01-04 07:37:11 -0500

    :sparkles: feat(quality-assurance): page and routing created

--- onecgiar-pr-client/src/app/pages/quality-assurance/quality-assurance.component.ts
+++ onecgiar-pr-client/src/app/pages/quality-assurance/quality-assurance.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../shared/services/api/api.service';
+import { ResultLevelService } from '../results/pages/result-creator/services/result-level.service';
 
 @Component({
   selector: 'app-quality-assurance',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quality-assurance.component.scss']
 })
 export class QualityAssuranceComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(public api: ApiService, public resultLevelSE: ResultLevelService) {}
+  allInitiatives = [];
   ngOnInit(): void {
+    this.GET_AllInitiatives();
   }
 
+  GET_AllInitiatives() {
+    // console.log(this.api.rolesSE.isAdmin);
+    if (!this.api.rolesSE.isAdmin) return;
+    this.api.resultsSE.GET_AllInitiatives().subscribe(({ response }) => {
+      console.log(response);
+      this.allInitiatives = response;
+    });
+  }
 }
