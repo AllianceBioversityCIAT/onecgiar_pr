8244098a104799ec78717257924b2770b0c22189
yecksin <yecksin@gmail.com>
2023-01-11 11:32:24 -0500

    :recycle: refactor(Submit button): Validation to hide, improved (Member)

--- onecgiar-pr-client/src/app/shared/services/current-result.service.ts
+++ onecgiar-pr-client/src/app/shared/services/current-result.service.ts
@@ -14,12 +14,12 @@ export class CurrentResultService {
   GET_resultById() {
     this.api.resultsSE.GET_resultById().subscribe(
       ({ response }) => {
-        console.log(response);
+        // console.log(response);
         this.rolesSE.validateReadOnly(response);
         this.resultLevelSE.currentResultLevelName = response.result_level_name;
         this.resultLevelSE.currentResultLevelId = response.result_level_id;
         this.resultLevelSE.currentResultTypeId = response.result_type_id;
-        console.log(response);
+        // console.log(response);
         this.dataControlSE.currentResult = response;
       },
       err => {
