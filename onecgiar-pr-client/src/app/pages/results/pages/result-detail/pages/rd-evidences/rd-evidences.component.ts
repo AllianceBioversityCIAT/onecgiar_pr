d6addaa06f592437da56e67f960e93ed679d5dd2
yecksin <yecksin@gmail.com>
2023-01-13 09:41:29 -0500

    :sparkles: feat(evidence): validateCheckBoxes improved

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-evidences/rd-evidences.component.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-evidences/rd-evidences.component.ts
@@ -51,13 +51,13 @@ export class RdEvidencesComponent {
     this.evidencesBody.supplementary.splice(index, 1);
   }
   validateCheckBoxes() {
-    if (this.evidencesBody?.gender_tag_level != '3' && this.evidencesBody?.climate_change_tag_level != '3') return false;
     let text = '<ul>';
     const gender_related = this.evidencesBody.evidences.some(evidence => evidence.gender_related === true);
     const youth_related = this.evidencesBody.evidences.some(evidence => evidence.youth_related === true);
     if (!gender_related && this.evidencesBody?.gender_tag_level == '3') text += '<li>At least one of the evidence sources must have the gender checkbox marked if the gender tag has a score of 2.</li>';
     if (!youth_related && this.evidencesBody?.climate_change_tag_level == '3') text += '<li>At least one of the evidence sources must have the climate checkbox marked if the climate change tag has a score of 2.</li>';
     text += '</ul>';
+    if (text == '<ul></ul>') return false;
     if (gender_related && youth_related) return false;
     return text;
   }
