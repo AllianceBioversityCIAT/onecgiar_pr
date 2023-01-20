1279803c7e4f02b0406ff088da7b79caa112ac66
yecksin <yecksin@gmail.com>
2023-01-20 10:17:22 -0500

    :recycle: refactor(all admin modules): init selector and open in full screen option added

--- onecgiar-pr-client/src/app/shared/services/global/green-checks.service.ts
+++ onecgiar-pr-client/src/app/shared/services/global/green-checks.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../api/api.service';
+import { ResultsApiService } from '../api/results-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GreenChecksService {
   submit = null;
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService, private resultsApiSE: ResultsApiService) {}
 
   updateGreenChecks() {
     setTimeout(() => {
-      this.api.resultsSE.GET_greenChecksByResultId().subscribe(({ response }) => {
-        this.submit = Boolean(response?.submit);
-        this.api.dataControlSE.green_checks = response?.green_checks;
-        this.api.resultsSE.PATCH_greenChecksByResultId().subscribe();
-      });
+      if (this.resultsApiSE.currentResultId) {
+        this.api.resultsSE.GET_greenChecksByResultId().subscribe(({ response }) => {
+          this.submit = Boolean(response?.submit);
+          this.api.dataControlSE.green_checks = response?.green_checks;
+          this.api.resultsSE.PATCH_greenChecksByResultId().subscribe();
+        });
+      }
     }, 1000);
   }
 }
