20d5794f693c2f96cec42fc0f34a07431d5976e0
yecksin <yecksin@gmail.com>
2023-01-23 15:59:35 -0500

    :recycle: refactor(init-completeness-status): filter inits by role coord and lead

---
+++ onecgiar-pr-client/src/app/pages/init-admin-section/pipes/filter-init-with-role-coord-and-lead.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterInitWithRoleCoordAndLeadPipe } from './filter-init-with-role-coord-and-lead.pipe';
+
+describe('FilterInitWithRoleCoordAndLeadPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterInitWithRoleCoordAndLeadPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
