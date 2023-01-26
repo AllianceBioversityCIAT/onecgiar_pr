0a9d36d7c8202649a835cfb6a15866cbffec5ff1
yecksin <yecksin@gmail.com>
2023-01-26 16:11:15 -0500

    :sparkles: feat(type-one-report): lazy loading developed

--- onecgiar-pr-client/src/app/pages/type-one-report/pages/tor-ipi-cgiar-portfolio-linkages/tor-ipi-cgiar-portfolio-linkages-routing.module.ts
+++ onecgiar-pr-client/src/app/pages/type-one-report/pages/tor-ipi-cgiar-portfolio-linkages/tor-ipi-cgiar-portfolio-linkages-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TorIpiCgiarPortfolioLinkagesComponent } from './tor-ipi-cgiar-portfolio-linkages.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: TorIpiCgiarPortfolioLinkagesComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class TorIpiCgiarPortfolioLinkagesRoutingModule { }
+export class TorIpiCgiarPortfolioLinkagesRoutingModule {}
