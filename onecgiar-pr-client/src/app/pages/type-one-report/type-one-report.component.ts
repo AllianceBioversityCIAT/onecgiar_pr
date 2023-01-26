f2442a88fc21b5688a78503d79b2b399cabc36de
yecksin <yecksin@gmail.com>
2023-01-26 16:06:26 -0500

    :sparkles: feat(type-one-report): router outlet added and ynamic-panel-menu added

--- onecgiar-pr-client/src/app/pages/type-one-report/type-one-report.component.ts
+++ onecgiar-pr-client/src/app/pages/type-one-report/type-one-report.component.ts
@@ -7,6 +7,13 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./type-one-report.component.scss']
 })
 export class TypeOneReportComponent {
+  sections = [
+    { path: '1', icon: '', name: 'Fact sheet' },
+    { path: '2', icon: '', name: 'Initiative progress & Key results' },
+    { path: '3', icon: '', name: 'Impact pathway integration - External partners' },
+    { path: '4', icon: '', name: 'Impact pathway integration - CGIAR portfolio linkages' },
+    { path: '5', icon: '', name: 'Key result story' }
+  ];
   constructor(private titleService: Title) {}
   ngOnInit(): void {
     this.titleService.setTitle('Type one report');
--- onecgiar-pr-client/src/app/pages/type-one-report/type-one-report.module.ts
+++ onecgiar-pr-client/src/app/pages/type-one-report/type-one-report.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { TypeOneReportRoutingModule } from './type-one-report-routing.module';
 import { TypeOneReportComponent } from './type-one-report.component';
+import { DynamicPanelMenuModule } from '../../shared/components/dynamic-panel-menu/dynamic-panel-menu.module';
 
 @NgModule({
   declarations: [TypeOneReportComponent],
-  imports: [CommonModule, TypeOneReportRoutingModule]
+  imports: [CommonModule, TypeOneReportRoutingModule, DynamicPanelMenuModule]
 })
 export class TypeOneReportModule {}
