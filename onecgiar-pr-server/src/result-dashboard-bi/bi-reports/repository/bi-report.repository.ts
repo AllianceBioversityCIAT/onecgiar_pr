60dff7b718398f13ebf0747b2279c70184799fa6
jdmunozg <juan.david.m.g1004@gmail.com>
2023-02-03 10:40:14 -0500

    feat(new services for report and create table for save tokens existing)

--- onecgiar-pr-server/src/result-dashboard-bi/bi-reports/repository/bi-report.repository.ts
+++ onecgiar-pr-server/src/result-dashboard-bi/bi-reports/repository/bi-report.repository.ts
@@ -4,15 +4,24 @@ import { BiReport } from '../entities/bi-report.entity';
 import { HttpService } from '@nestjs/axios';
 import { ClarisaCredentialsBiService } from 'src/result-dashboard-bi/clarisa-credentials-bi.service';
 import { CredentialsClarisaBi } from '../dto/crendentials-clarisa.dto';
-import { lastValueFrom, map } from 'rxjs';
+import { filter, lastValueFrom, map } from 'rxjs';
 import { BodyPowerBiDTO, EmbedCredentialsDTO, ReportInformation } from '../dto/embed-credentials.dto';
 import { CreateBiReportDto } from '../dto/create-bi-report.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TokenBiReport } from '../entities/token-bi-reports.entity';
+import { log } from 'console';
+import { TokenReportBiDto } from '../dto/create-token-bi-report.dto';
 
 @Injectable()
 export class BiReportRepository extends Repository<BiReport> {
     private linkAzure: string;
     private credentialsBi: CredentialsClarisaBi;
-  constructor(private dataSource: DataSource, private readonly _httpService: HttpService,private _servicesClarisaCredentials: ClarisaCredentialsBiService) {
+  constructor(
+    private dataSource: DataSource, 
+    private readonly _httpService: HttpService,
+    private _servicesClarisaCredentials: ClarisaCredentialsBiService,
+    @InjectRepository(TokenBiReport)
+    private tokenBireports:Repository<TokenBiReport>,) {
     super(BiReport, dataSource.createEntityManager());
   }
 
@@ -43,48 +52,60 @@ export class BiReportRepository extends Repository<BiReport> {
   }
 
   async getTokenPowerBi(){
-    const barerTokenAzure = await this.getBarerTokenAzure();
-
     const reportsBi: BiReport[] = await this.getReportsBi();
+
+    if(reportsBi.length > 0 ){
 
     let datasets: BodyPowerBiDTO[] = [];
     let reportsId: BodyPowerBiDTO[] = [];
 
-    reportsBi.map((resp) =>{
-      let auxIdReportBi: BodyPowerBiDTO = new BodyPowerBiDTO;
-      let auxIdDataSetsBi: BodyPowerBiDTO = new BodyPowerBiDTO;
-      auxIdReportBi.id = resp.report_id;
-      reportsId.push(auxIdReportBi);
-      auxIdDataSetsBi.id = resp.dataset_id;
-      datasets.push(auxIdDataSetsBi);
-    })
+    const barerTokenAzure = await this.getBarerTokenAzure();
+      reportsBi.map((resp) =>{
+        let auxIdReportBi: BodyPowerBiDTO = new BodyPowerBiDTO;
+        let auxIdDataSetsBi: BodyPowerBiDTO = new BodyPowerBiDTO;
+        auxIdReportBi.id = resp.report_id;
+        reportsId.push(auxIdReportBi);
+        auxIdDataSetsBi.id = resp.dataset_id;
+        datasets.push(auxIdDataSetsBi);
+      })
+  
+      const bodyRequestPowerBi = {
+        datasets: datasets,
+        reports:reportsId
+      }
+  
+      const tokenPowerBi = await lastValueFrom(
+          await this._httpService
+            .post(`${this.credentialsBi.api_token_url}`,bodyRequestPowerBi ,{headers:{Authorization: `Bearer ${barerTokenAzure.access_token}`}})
+            .pipe(map((resp) => resp.data)),
+        );
+        
+      const reportTokenBiDto = new TokenReportBiDto;
+      reportTokenBiDto.token_bi = tokenPowerBi.token;
+      reportTokenBiDto.expiration_toke_id = tokenPowerBi.expiration;
+      reportTokenBiDto.is_active = true;
+      await this.tokenBireports.save(reportTokenBiDto);
 
-    const bodyRequestPowerBi = {
-      datasets: datasets,
-      reports:reportsId
+      let informationEmbedBi: EmbedCredentialsDTO = new EmbedCredentialsDTO;
+      informationEmbedBi.embed_token = tokenPowerBi.token;
+      let auxReportsInfo: ReportInformation[] = []
+      reportsBi.map((resp)=>{
+        let reportsInfo: ReportInformation = new ReportInformation;
+        reportsInfo.id = resp.id;
+        reportsInfo.resport_id = resp.report_id;
+        reportsInfo.name = resp.report_name;
+        reportsInfo.description = resp.report_description;
+        reportsInfo.embed_url = this.credentialsBi.embed_url_base+resp.report_id+'&groupId='+resp.group_id+'&config='+this.credentialsBi.config_id;
+        auxReportsInfo.push(reportsInfo)
+      })
+      informationEmbedBi.reportsInformation = auxReportsInfo;
+      
+     return informationEmbedBi;
     }
 
-    const tokenPowerBi = await lastValueFrom(
-        await this._httpService
-          .post(`${this.credentialsBi.api_token_url}`,bodyRequestPowerBi ,{headers:{Authorization: `Bearer ${barerTokenAzure.access_token}`}})
-          .pipe(map((resp) => resp.data)),
-      );
-
-    let informationEmbedBi: EmbedCredentialsDTO = new EmbedCredentialsDTO;
-    informationEmbedBi.embed_token = tokenPowerBi.token;
-    let auxReportsInfo: ReportInformation[] = []
-    reportsBi.map((resp)=>{
-      let reportsInfo: ReportInformation = new ReportInformation;
-
-      reportsInfo.resport_id = resp.report_id;
-      reportsInfo.name = resp.report_name;
-      reportsInfo.description = resp.report_description;
-      reportsInfo.embed_url = this.credentialsBi.embed_url_base+resp.report_id+'&groupId='+resp.group_id+'&config='+this.credentialsBi.config_id;
-      auxReportsInfo.push(reportsInfo)
-    })
-    informationEmbedBi.reportsInformation = auxReportsInfo;
-    
-   return informationEmbedBi;
+    return {
+      Error: 'At this time there are no reports or none are active for viewing.'
+    }
   }
 
   async getReportsBi(){
@@ -93,6 +114,39 @@ export class BiReportRepository extends Repository<BiReport> {
     }});
 
     return getResportBi;
+  }
+
+  async getTokenAndReportById(id: number){
+    this.credentialsBi = await this._servicesClarisaCredentials.getCredentialsBi();
+    const tokensReports: TokenBiReport[] = await this.tokenBireports.query(`select * from token_report_bi trb order by id desc limit 1;`)
+    const today = new Date();
+    const reportsExist = await this.findOneBy({id, is_active:true})
+    if(reportsExist != null){
+      if(tokensReports.length <= 0 || Date.parse(tokensReports[0].expiration_toke_id.toString()) < Date.parse(today.toString())){
+        const registerInToken = await this.getTokenPowerBi();
+        const responseToken = await registerInToken['reportsInformation'].filter(report => report.id == id);
+        return {
+          token:registerInToken['embed_token'],
+          report: responseToken[0]
+        };
+      }else{
+        let reportsInfo: ReportInformation = new ReportInformation;
+        reportsInfo.id = reportsExist.id;
+        reportsInfo.resport_id = reportsExist.report_id;
+        reportsInfo.name = reportsExist.report_name;
+        reportsInfo.description = reportsExist.report_description;
+        reportsInfo.embed_url = this.credentialsBi.embed_url_base+reportsExist.report_id+'&groupId='+reportsExist.group_id+'&config='+this.credentialsBi.config_id;
+
+        return {
+          token: tokensReports[0].token_bi,
+          report:reportsInfo
+        }
+      }
+    }else{
+      return {
+        error:'This Report not exists'
+      }
+    }
   }
 
   async createNewRegisterBi(createBiReport: CreateBiReportDto){
