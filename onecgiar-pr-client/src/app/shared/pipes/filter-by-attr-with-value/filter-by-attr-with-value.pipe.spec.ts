b4d4279100400df178af5fe52ba8ace179505c6a
yecksin <yecksin@gmail.com>
2023-02-08 14:57:25 -0500

    :recycle: refactor(filter-by-attr-with-value): pipe created

---
+++ onecgiar-pr-client/src/app/shared/pipes/filter-by-attr-with-value/filter-by-attr-with-value.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterByAttrWithValuePipe } from './filter-by-attr-with-value.pipe';
+
+describe('FilterByAttrWithValuePipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterByAttrWithValuePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
