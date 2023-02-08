ab01521688182d15650a8848667d01e2d95e85cb
yecksin <yecksin@gmail.com>
2023-02-08 15:57:16 -0500

    :recycle: refactor(init-general-results-report): year filter added

--- onecgiar-pr-client/src/app/pages/init-admin-section/pages/init-general-results-report/init-general-results-report.component.ts
+++ onecgiar-pr-client/src/app/pages/init-admin-section/pages/init-general-results-report/init-general-results-report.component.ts
@@ -14,6 +14,7 @@ export class InitGeneralResultsReportComponent {
   resultsList;
   requesting = false;
   valueToFilter = null;
+  yearToFilter = null;
   constructor(public api: ApiService, private exportTablesSE: ExportTablesService) {}
 
   onSelectInit() {
