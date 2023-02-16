2ddad0d1a430f4d4b7acfed99fedf50a7e6f2d66
yecksin <yecksin@gmail.com>
2023-02-16 09:25:03 -0500

    :sparkles: feat(type-one-report) Select first initiative in the drop down

--- onecgiar-pr-client/src/app/pages/type-one-report/type-one-report.component.ts
+++ onecgiar-pr-client/src/app/pages/type-one-report/type-one-report.component.ts
@@ -10,13 +10,15 @@ import { ApiService } from '../../shared/services/api/api.service';
 export class TypeOneReportComponent {
   sections = [
     { path: 'fact-sheet', icon: '', name: 'Fact sheet' },
-    { path: 'initiative-progress-and-key-results', icon: '', name: 'Initiative progress & Key results' },
-    { path: 'ipi-external-partners', icon: '', name: 'Impact pathway integration - External partners' },
-    { path: 'ipi-cgiar-portfolio-linkages', icon: '', name: 'Impact pathway integration - CGIAR portfolio linkages' },
-    { path: 'key-result-story', icon: '', name: 'Key result story' }
+    { path: 'initiative-progress-and-key-results', icon: '', name: 'Initiative progress & Key results' }
+    // { path: 'ipi-external-partners', icon: '', name: 'Impact pathway integration - External partners' },
+    // { path: 'ipi-cgiar-portfolio-linkages', icon: '', name: 'Impact pathway integration - CGIAR portfolio linkages' },
+    // { path: 'key-result-story', icon: '', name: 'Key result story' }
   ];
+  initiativeSelected = null;
   constructor(private titleService: Title, public api: ApiService) {}
   ngOnInit(): void {
+    this.api.rolesSE.validateReadOnly();
     this.titleService.setTitle('Type one report');
     this.GET_AllInitiatives();
   }
@@ -24,10 +26,13 @@ export class TypeOneReportComponent {
 
   allInitiatives = [];
   GET_AllInitiatives() {
-    // console.log(this.api.rolesSE.isAdmin);
-    if (!this.api.rolesSE.isAdmin) return;
+    if (!this.api.rolesSE.isAdmin) return this.selectFirstInitiative();
     this.api.resultsSE.GET_AllInitiatives().subscribe(({ response }) => {
       this.allInitiatives = response;
+      this.initiativeSelected = this.allInitiatives[0]?.official_code;
     });
+  }
+  selectFirstInitiative() {
+    this.initiativeSelected = this.api.dataControlSE.myInitiativesList[0]?.official_code;
   }
 }
