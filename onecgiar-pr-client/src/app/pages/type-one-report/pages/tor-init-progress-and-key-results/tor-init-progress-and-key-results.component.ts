86e3399aa57ed1c6df2c67d1153bf9f4679270a0
yecksin <yecksin@gmail.com>
2023-02-17 09:40:39 -0500

    :recycle: refactor(tor-init-progress-and-key-result): No data text added

--- onecgiar-pr-client/src/app/pages/type-one-report/pages/tor-init-progress-and-key-results/tor-init-progress-and-key-results.component.ts
+++ onecgiar-pr-client/src/app/pages/type-one-report/pages/tor-init-progress-and-key-results/tor-init-progress-and-key-results.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TypeOneReportService } from '../../type-one-report.service';
+import { ApiService } from '../../../../shared/services/api/api.service';
 
 @Component({
   selector: 'app-tor-init-progress-and-key-results',
@@ -8,5 +9,18 @@ import { TypeOneReportService } from '../../type-one-report.service';
 })
 export class TorInitProgressAndKeyResultsComponent {
   full_screen = false;
-  constructor(public typeOneReportSE: TypeOneReportService) {}
+  constructor(public typeOneReportSE: TypeOneReportService, private api: ApiService) {}
+
+  exportExcel(initiativeSelected) {
+    console.log(initiativeSelected);
+    // this.api.resultsSE.POST_excelFullReport([result]).subscribe(
+    //   ({ response }) => {
+    //     // console.log(response);
+    //     // console.log(response);
+    //   },
+    //   err => {
+    //     this.api.alertsFe.show({ id: 'loginAlert', title: 'Oops!', description: 'There was an error in the system while generating the report. If the issue persists, please contact the technical team.', status: 'error' });
+    //   }
+    // );
+  }
 }
