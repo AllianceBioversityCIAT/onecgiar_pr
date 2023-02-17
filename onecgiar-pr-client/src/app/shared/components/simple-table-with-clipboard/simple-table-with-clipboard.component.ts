898055a3ea07809ba73fcd451251c51e8ee9edfd
yecksin <yecksin@gmail.com>
2023-02-17 08:24:34 -0500

    :recycle: refactor(type-one-report): Copy format improved

--- onecgiar-pr-client/src/app/shared/components/simple-table-with-clipboard/simple-table-with-clipboard.component.ts
+++ onecgiar-pr-client/src/app/shared/components/simple-table-with-clipboard/simple-table-with-clipboard.component.ts
@@ -18,26 +18,32 @@ export class SimpleTableWithClipboardComponent {
   @Input() title: string;
   @Input() header = [];
   @Input() data = [];
+  flatFormat = false;
 
   constructor(private messageService: MessageService) {}
 
   copyTable(table) {
+    this.flatFormat = true;
     // const s = window.getSelection();
     // if (s.rangeCount > 1) {
     //   for (let i = 1; i < s.rangeCount; i++) {
     //     s.removeRange(s.getRangeAt(i));
     //   }
     // }
+    setTimeout(() => {
+      let range = new Range();
+      // s.removeRange(range);
 
-    let range = new Range();
-    // s.removeRange(range);
-
-    range.setStart(table, 0);
-    range.setEnd(table, table.childNodes.length);
-    document.getSelection().removeAllRanges();
-    document.getSelection().addRange(range);
-    document.execCommand('copy');
-    this.messageService.add({ key: 'myKey1', severity: 'info', summary: 'Copied', detail: 'Table copied to clipboard' });
-    document.getSelection().removeAllRanges();
+      range.setStart(table, 0);
+      range.setEnd(table, table.childNodes.length);
+      document.getSelection().removeAllRanges();
+      document.getSelection().addRange(range);
+      document.execCommand('copy');
+      this.messageService.add({ key: 'myKey1', severity: 'info', summary: 'Copied', detail: 'Table copied to clipboard' });
+      document.getSelection().removeAllRanges();
+      setTimeout(() => {
+        this.flatFormat = false;
+      }, 200);
+    }, 200);
   }
 }
