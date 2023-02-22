c5caa2a0f5d9e50ce1f3e6216b2a2b0eaeda3945
Juan Delgado <juandelgadog98@gmail.com>
2023-02-22 15:27:30 -0500

    :sparkles: feat(resource) created new resource for primary impact area for krs section in T1R

---
+++ onecgiar-pr-server/src/api/type-one-report/primary-impact-area/dto/update-primary-impact-area.dto.ts
@@ -0,0 +1,4 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreatePrimaryImpactAreaDto } from './create-primary-impact-area.dto';
+
+export class UpdatePrimaryImpactAreaDto extends PartialType(CreatePrimaryImpactAreaDto) {}
---
+++ onecgiar-pr-server/src/api/type-one-report/primary-impact-area/entities/primary-impact-area.entity.ts
@@ -0,0 +1,50 @@
+import { Result } from "../../../../api/results/entities/result.entity";
+import { User } from "../../../../auth/modules/user/entities/user.entity";
+import { ClarisaImpactArea } from "../../../../clarisa/clarisa-impact-area/entities/clarisa-impact-area.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('primary_impact_area')
+export class PrimaryImpactArea {
+    @PrimaryGeneratedColumn({
+        name: 'id',
+        type: 'bigint'
+    })
+    id: number;
+
+    @OneToOne(() => Result, r => r.id, { nullable: false })
+    @JoinColumn({
+        name: 'result_id'
+    })
+    result_id: number;
+
+    @ManyToOne(() => ClarisaImpactArea, c => c.id, { nullable: false })
+    @JoinColumn({
+        name: 'impact_area_id'
+    })
+    impact_area_id: number;
+
+    @CreateDateColumn({
+        type: 'timestamp',
+        name: 'created_date',
+    })
+    created_date: Date;
+
+    @UpdateDateColumn({
+        type: 'timestamp',
+        name: 'last_updated_date',
+        nullable: true,
+    })
+    last_updated_date: Date;
+
+    @ManyToOne(() => User, u => u.id, { nullable: true })
+    @JoinColumn({
+        name: 'created_by'
+    })
+    created_by: number;
+
+    @ManyToOne(() => User, u => u.id, { nullable: true })
+    @JoinColumn({
+        name: 'updated_by'
+    })
+    updated_by: number;
+}
