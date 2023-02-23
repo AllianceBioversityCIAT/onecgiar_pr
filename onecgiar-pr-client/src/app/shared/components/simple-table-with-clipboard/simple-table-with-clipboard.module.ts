dc623adbdab57df5e52282b5910cd9a8308bb450
yecksin <yecksin@gmail.com>
2023-02-23 10:59:33 -0500

    :recycle: refactor(simple-table-with-clipboard): skeleton and progress bar loading added

--- onecgiar-pr-client/src/app/shared/components/simple-table-with-clipboard/simple-table-with-clipboard.module.ts
+++ onecgiar-pr-client/src/app/shared/components/simple-table-with-clipboard/simple-table-with-clipboard.module.ts
@@ -4,10 +4,12 @@ import { SimpleTableWithClipboardComponent } from './simple-table-with-clipboard
 import { TooltipModule } from 'primeng/tooltip';
 import { ToastModule } from 'primeng/toast';
 import { TorKrsPrimaryImpactAreaSelectorModule } from './components/tor-krs-primary-impact-area-selector/tor-krs-primary-impact-area-selector.module';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { SkeletonModule } from 'primeng/skeleton';
 
 @NgModule({
   declarations: [SimpleTableWithClipboardComponent],
   exports: [SimpleTableWithClipboardComponent],
-  imports: [CommonModule, TooltipModule, ToastModule, TorKrsPrimaryImpactAreaSelectorModule]
+  imports: [CommonModule, TooltipModule, ToastModule, TorKrsPrimaryImpactAreaSelectorModule, ProgressBarModule, SkeletonModule]
 })
 export class SimpleTableWithClipboardModule {}
