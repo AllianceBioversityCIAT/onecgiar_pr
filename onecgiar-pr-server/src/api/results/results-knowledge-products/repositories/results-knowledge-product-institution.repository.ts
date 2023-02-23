9b6f641f7cb58dd3731cea2418977c076441abb8
davinzifc <47566127+davinzifc@users.noreply.github.com>
2023-02-23 11:00:06 -0500

    :recycle: refactor(knowledge products):  added functions to remove knowledge products

--- onecgiar-pr-server/src/api/results/results-knowledge-products/repositories/results-knowledge-product-institution.repository.ts
+++ onecgiar-pr-server/src/api/results/results-knowledge-products/repositories/results-knowledge-product-institution.repository.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { ResultsKnowledgeProductInstitution } from '../entities/results-knowledge-product-institution.entity';
+import { HandlersError } from '../../../../shared/handlers/error.utils';
 
 @Injectable()
 export class ResultsKnowledgeProductInstitutionRepository extends Repository<ResultsKnowledgeProductInstitution> {
-  constructor(private dataSource: DataSource) {
+  constructor(
+    private dataSource: DataSource,
+    private readonly _handlersError: HandlersError
+    ) {
     super(ResultsKnowledgeProductInstitution, dataSource.createEntityManager());
+  }
+
+  async statusElement(kpId: number, status: boolean){
+    const query = `
+    UPDATE results_kp_mqap_institutions
+    SET is_active = ?
+    WHERE result_knowledge_product_id  = ?;
+    `;
+    try{
+      return await this.query(query, [status?1:0, kpId]);
+    }catch(error){
+      throw this._handlersError.returnErrorRepository({
+        className: ResultsKnowledgeProductInstitutionRepository.name,
+        error: error,
+        debug: true,
+      });
+    }
   }
 }
