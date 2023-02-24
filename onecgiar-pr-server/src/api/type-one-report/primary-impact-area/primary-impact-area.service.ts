148941fdaa18d768a5a35ea0f538e08d0410156f
yecksin <yecksin@gmail.com>
2023-02-23 23:49:29 -0500

    :recycle: refactor(T1R / primary-impact-area.service): save primary impact area fixed

--- onecgiar-pr-server/src/api/type-one-report/primary-impact-area/primary-impact-area.service.ts
+++ onecgiar-pr-server/src/api/type-one-report/primary-impact-area/primary-impact-area.service.ts
@@ -9,46 +9,49 @@ import { TokenDto } from 'src/shared/globalInterfaces/token.dto';
 export class PrimaryImpactAreaService {
   constructor(
     private readonly _handlersError: HandlersError,
-    private readonly _primaryImpactAreaRepository: PrimaryImpactAreaRepository
-  ) { }
+    private readonly _primaryImpactAreaRepository: PrimaryImpactAreaRepository,
+  ) {}
   async create(createPrimaryImpactAreaDto: any[], user: TokenDto) {
     try {
-      let listImpactAreaPrimary = []
-      createPrimaryImpactAreaDto.forEach(async (resp) => {
-        if (resp.impact_area_id != null && resp.result_code != null) {
-          const primaryExists = resp.result_code;
+      const listImpactAreaPrimary = [];
+
+      for (const key in createPrimaryImpactAreaDto) {
+        const resp = createPrimaryImpactAreaDto[key];
+
+        if (resp.impact_area_id && resp.result_code) {
+          const primaryExists = await this._primaryImpactAreaRepository.findBy({
+            result_code: resp.result_code,
+          });
+          const resultCode = primaryExists.map((e) => e.result_code);
           const impactArea = resp.impact_area_id;
 
-          if (primaryExists) {
-            const updatedImpact = await this._primaryImpactAreaRepository.update(primaryExists, { impact_area_id: impactArea });
-
-            return {
-              response: updatedImpact,
-              message: 'Successful Update',
-              status: HttpStatus.OK
-            }
+          if (primaryExists.length) {
+            await this._primaryImpactAreaRepository.update(resultCode, {
+              impact_area_id: impactArea,
+            });
+          } else {
+            console.log('push');
+            listImpactAreaPrimary.push({
+              result_code: resp.result_code,
+              impact_area_id: resp.impact_area_id,
+              created_by: user.id,
+              updated_by: user.id,
+            });
           }
-
-          listImpactAreaPrimary.push({
-            result_code: resp.result_code,
-            impact_area_id: resp.impact_area_id,
-            created_by: user.id,
-            updated_by: user.id
-          })
         }
-      })
+      }
 
-      const saveImpactAre = await this._primaryImpactAreaRepository.save(listImpactAreaPrimary);
+      const saveImpactAre = await this._primaryImpactAreaRepository.save(
+        listImpactAreaPrimary,
+      );
 
       return {
         response: saveImpactAre,
         message: 'Successful Response',
-        status: HttpStatus.OK
-      }
+        status: HttpStatus.OK,
+      };
     } catch (error) {
       return this._handlersError.returnErrorRes({ error, debug: true });
     }
   }
-
-
 }
