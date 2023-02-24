00eb3dffb1bcb2692a0fed43dff3c7d85f18ff23
jdmunozg <juan.david.m.g1004@gmail.com>
2023-02-24 08:25:37 -0500

    refactor(changes in query impact area)

--- onecgiar-pr-server/src/api/results/results.service.ts
+++ onecgiar-pr-server/src/api/results/results.service.ts
@@ -545,7 +545,7 @@ export class ResultsService {
     try {
       const result: Result = await this._resultRepository.findOne({
         where: { id: resultId },
-        //relations: { legacy_id: true },
+        relations: { legacy_id: true },
       });
       if (!result) {
         throw {
