3a91a1b5142496730eae0e5bb3dfd3c9bc13a5be
davinzifc <47566127+davinzifc@users.noreply.github.com>
2023-02-28 15:56:15 -0500

    :sparkles: feat (risk server): database connection created

--- onecgiar-pr-risk-server/src/app.module.ts
+++ onecgiar-pr-risk-server/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { dataSource } from './config/orm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...dataSource.options,
+      keepConnectionAlive: true,
+      autoLoadEntities: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
