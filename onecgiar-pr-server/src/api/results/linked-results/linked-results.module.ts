9cea7bfe65b74d350a1ccfac3bb61d2d38d6894a
davinzifc <47566127+davinzifc@users.noreply.github.com>
2023-03-16 12:00:02 -0500

    :bug: fix(results): version added in geoscope and linkedResults creation

--- onecgiar-pr-server/src/api/results/linked-results/linked-results.module.ts
+++ onecgiar-pr-server/src/api/results/linked-results/linked-results.module.ts
@@ -4,10 +4,12 @@ import { LinkedResultsController } from './linked-results.controller';
 import { LinkedResultRepository } from './linked-results.repository';
 import { HandlersError } from '../../../shared/handlers/error.utils';
 import { ResultRepository } from '../result.repository';
+import { VersionRepository } from '../versions/version.repository';
+import { VersionsService } from '../versions/versions.service';
 
 @Module({
   controllers: [LinkedResultsController],
-  providers: [LinkedResultsService, LinkedResultRepository, HandlersError, ResultRepository],
+  providers: [LinkedResultsService, LinkedResultRepository, HandlersError, ResultRepository, VersionsService, VersionRepository],
   exports: [
     LinkedResultRepository
   ]
