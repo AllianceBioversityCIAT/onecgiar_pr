e5d3d2db0a9cbc7f02ab334881ab9156d73cb30d
yecksin <yecksin@gmail.com>
2023-03-29 14:19:09 -0500

    :recycle: refactor(IPSR/step 1) LFUB-116: step-n1-sdg-targets component created

---
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-sdg-targets/step-n1-sdg-targets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-n1-sdg-targets',
+  templateUrl: './step-n1-sdg-targets.component.html',
+  styleUrls: ['./step-n1-sdg-targets.component.scss']
+})
+export class StepN1SdgTargetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
