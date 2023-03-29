9eb37feac886c1225a6ebbe02119eb2eca661866
yecksin <yecksin@gmail.com>
2023-03-29 14:21:39 -0500

    :recycle: refactor(IPSR/step 1) LFUB-116: step-n1-experts component created

---
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-experts/step-n1-experts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-n1-experts',
+  templateUrl: './step-n1-experts.component.html',
+  styleUrls: ['./step-n1-experts.component.scss']
+})
+export class StepN1ExpertsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
