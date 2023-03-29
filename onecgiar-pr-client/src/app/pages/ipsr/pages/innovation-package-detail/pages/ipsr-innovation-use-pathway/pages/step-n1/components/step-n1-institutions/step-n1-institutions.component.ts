4d2e802a48b94f063b372da63ff691193c7f8032
yecksin <yecksin@gmail.com>
2023-03-29 14:19:45 -0500

    :recycle: refactor(IPSR/step 1) LFUB-116: step-n1-institutions component created

---
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-institutions/step-n1-institutions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-n1-institutions',
+  templateUrl: './step-n1-institutions.component.html',
+  styleUrls: ['./step-n1-institutions.component.scss']
+})
+export class StepN1InstitutionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
