15793e81008df4b50655920b75859539fc604403
yecksin <yecksin@gmail.com>
2023-03-29 14:23:25 -0500

    :recycle: refactor(IPSR/step 1) LFUB-116: step-n1-action-area-outcomes component created

---
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-action-area-outcomes/step-n1-action-area-outcomes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-n1-action-area-outcomes',
+  templateUrl: './step-n1-action-area-outcomes.component.html',
+  styleUrls: ['./step-n1-action-area-outcomes.component.scss']
+})
+export class StepN1ActionAreaOutcomesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
