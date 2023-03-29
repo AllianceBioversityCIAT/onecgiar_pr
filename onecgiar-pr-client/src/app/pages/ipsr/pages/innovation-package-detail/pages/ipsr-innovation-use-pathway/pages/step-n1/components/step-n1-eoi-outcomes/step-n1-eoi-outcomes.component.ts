471830617cb08fc90c861e2c9df2ecffca39ea95
yecksin <yecksin@gmail.com>
2023-03-29 14:22:36 -0500

    :recycle: refactor(IPSR/step 1) LFUB-116: step-n1-eoi-outcomes component created

---
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-eoi-outcomes/step-n1-eoi-outcomes.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-n1-eoi-outcomes',
+  templateUrl: './step-n1-eoi-outcomes.component.html',
+  styleUrls: ['./step-n1-eoi-outcomes.component.scss']
+})
+export class StepN1EoiOutcomesComponent {
+  constructor() {}
+}
