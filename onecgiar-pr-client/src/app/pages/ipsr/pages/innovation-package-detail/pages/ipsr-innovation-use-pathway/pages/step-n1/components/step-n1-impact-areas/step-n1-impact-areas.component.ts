bee1f8a321e4c3af5e6c9cfe9ab772176c848d08
yecksin <yecksin@gmail.com>
2023-03-29 14:21:06 -0500

    :recycle: refactor(IPSR/step 1) LFUB-116: step-n1-impact-areas component created

---
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-impact-areas/step-n1-impact-areas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-n1-impact-areas',
+  templateUrl: './step-n1-impact-areas.component.html',
+  styleUrls: ['./step-n1-impact-areas.component.scss']
+})
+export class StepN1ImpactAreasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
