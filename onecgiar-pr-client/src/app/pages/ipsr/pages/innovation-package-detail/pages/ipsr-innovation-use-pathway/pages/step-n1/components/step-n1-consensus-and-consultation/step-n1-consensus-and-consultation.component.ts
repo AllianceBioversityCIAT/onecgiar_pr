80b471598fe502b2c4a8e6eca5332b5ca0aea033
yecksin <yecksin@gmail.com>
2023-03-29 14:23:01 -0500

    :recycle: refactor(IPSR/step 1) LFUB-116: step-n1-consensus-and-consultation component created

---
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-consensus-and-consultation/step-n1-consensus-and-consultation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-n1-consensus-and-consultation',
+  templateUrl: './step-n1-consensus-and-consultation.component.html',
+  styleUrls: ['./step-n1-consensus-and-consultation.component.scss']
+})
+export class StepN1ConsensusAndConsultationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
