f3ff01a111a08418352f9971d42bb9535c8afb66
yecksin <yecksin@gmail.com>
2023-03-29 10:24:03 -0500

    :recycle: refactor(IPSR/step 1) LFUB-116: steps_status, step_divider html added and steper-navigation options improved

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/ipsr-innovation-use-pathway.module.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/ipsr-innovation-use-pathway.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { IpsrInnovationUsePathwayRoutingModule } from './ipsr-innovation-use-pathway-routing.module';
 import { CustomFieldsModule } from '../../../../../../custom-fields/custom-fields.module';
 import { IpsrInnovationUsePathwayComponent } from './ipsr-innovation-use-pathway.component';
-
+import { SteperNavigationModule } from '../../../../../../shared/components/steper-navigation/steper-navigation.module';
 @NgModule({
   declarations: [IpsrInnovationUsePathwayComponent],
-  imports: [CommonModule, IpsrInnovationUsePathwayRoutingModule, CustomFieldsModule]
+  imports: [CommonModule, IpsrInnovationUsePathwayRoutingModule, CustomFieldsModule, SteperNavigationModule]
 })
 export class IpsrInnovationUsePathwayModule {}
