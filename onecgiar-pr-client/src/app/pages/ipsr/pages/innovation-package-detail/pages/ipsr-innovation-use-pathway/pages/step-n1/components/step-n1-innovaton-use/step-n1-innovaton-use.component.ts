26ae99943f99d86993cda8a314edcd15b55901c7
yecksin <yecksin@gmail.com>
2023-03-29 14:20:33 -0500

    :recycle: refactor(IPSR/step 1) LFUB-116: step-n1-innovaton-use component created

---
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-innovaton-use/step-n1-innovaton-use.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-n1-innovaton-use',
+  templateUrl: './step-n1-innovaton-use.component.html',
+  styleUrls: ['./step-n1-innovaton-use.component.scss']
+})
+export class StepN1InnovatonUseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
