efc9649b65f2536f6584fb03d42f2e9f821da9fc
yecksin <yecksin@gmail.com>
2023-03-29 10:16:17 -0500

    :recycle: refactor(IPSR/steper navigation) LFUB-125: show routeName

--- onecgiar-pr-client/src/app/shared/components/steper-navigation/steper-navigation.module.ts
+++ onecgiar-pr-client/src/app/shared/components/steper-navigation/steper-navigation.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SteperNavigationComponent } from './steper-navigation.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SteperNavigationComponent],
   exports: [SteperNavigationComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, RouterModule]
 })
 export class SteperNavigationModule {}
