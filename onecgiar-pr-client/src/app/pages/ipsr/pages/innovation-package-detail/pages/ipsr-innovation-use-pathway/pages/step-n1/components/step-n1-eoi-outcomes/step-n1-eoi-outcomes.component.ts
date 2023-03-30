0e1a85f765c0a9b5516e2fbd737f2b20d582885f
yecksin <yecksin@gmail.com>
2023-03-30 09:52:47 -0500

    :recycle: refactor(IPSR/step 1) LFUB-116: Consume GET_tocLevelsByresultId to get EOI list

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-eoi-outcomes/step-n1-eoi-outcomes.component.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-eoi-outcomes/step-n1-eoi-outcomes.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TocApiService } from '../../../../../../../../../../shared/services/api/toc-api.service';
+import { IpsrDataControlService } from '../../../../../../../../services/ipsr-data-control.service';
+import { IpsrStep1Body } from '../../model/Ipsr-step-1-body.model';
 
 @Component({
   selector: 'app-step-n1-eoi-outcomes',
@@ -6,5 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./step-n1-eoi-outcomes.component.scss']
 })
 export class StepN1EoiOutcomesComponent {
-  constructor() {}
+  @Input() body = new IpsrStep1Body();
+  eoiList = [];
+  constructor(private tocApiSE: TocApiService, private ipsrDataControlSE: IpsrDataControlService) {}
+  ngOnInit(): void {
+    this.GET_EOIList();
+  }
+
+  GET_EOIList() {
+    console.log(this.ipsrDataControlSE?.detailData?.inititiative_id);
+    this.tocApiSE.GET_tocLevelsByresultId(this.ipsrDataControlSE?.detailData?.inititiative_id, 3).subscribe(
+      ({ response }) => {
+        console.log(response);
+        this.eoiList = response;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 }
