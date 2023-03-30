396c204e428d4a6be43eb394a2324b6140f4eca6
yecksin <yecksin@gmail.com>
2023-03-30 09:50:37 -0500

    :recycle: refactor(IPSR/step 1) LFUB-116: Show coreResult data

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/step-n1.component.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/step-n1.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../../../../../../../shared/services/api/api.service';
-import { IpsrStep1Body } from './model/Ipsr-step-1-body.model';
+import { IpsrStep1Body, CoreResult } from './model/Ipsr-step-1-body.model';
+import { IpsrDataControlService } from '../../../../../../services/ipsr-data-control.service';
 
 @Component({
   selector: 'app-step-n1',
@@ -9,15 +10,19 @@ import { IpsrStep1Body } from './model/Ipsr-step-1-body.model';
 })
 export class StepN1Component implements OnInit {
   ipsrStep1Body = new IpsrStep1Body();
-  constructor(private api: ApiService) {}
+  coreResult = new CoreResult();
+  constructor(private api: ApiService, public ipsrDataControlSE: IpsrDataControlService) {}
 
   ngOnInit(): void {
     this.getSectionInformation();
   }
 
   getSectionInformation() {
-    this.api.resultsSE.GETInnovationPathwayByStepOneResultId().subscribe(resp => {
-      // console.log(resp);
+    this.api.resultsSE.GETInnovationPathwayByStepOneResultId().subscribe(({ response }) => {
+      console.log(response);
+      this.ipsrStep1Body = response;
+      this.coreResult = response?.coreResult;
+      console.log(this.coreResult);
     });
   }
   onSaveSection() {
