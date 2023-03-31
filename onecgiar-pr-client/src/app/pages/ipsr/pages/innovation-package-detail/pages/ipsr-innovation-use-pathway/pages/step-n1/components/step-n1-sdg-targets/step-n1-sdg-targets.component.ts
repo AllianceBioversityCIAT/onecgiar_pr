1e329b8f7096d0fa7ea9b38ed4b89d86e2c35b97
yecksin <yecksin@gmail.com>
2023-03-31 08:37:43 -0500

    :sparkles: feat(IPSR/step 1) LFUB-116: step-n1-sdg-targets impact areas icon added and interaction developed

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-sdg-targets/step-n1-sdg-targets.component.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-sdg-targets/step-n1-sdg-targets.component.ts
@@ -8,5 +8,18 @@ import { IpsrStep1Body } from '../../model/Ipsr-step-1-body.model';
 })
 export class StepN1SdgTargetsComponent {
   @Input() body = new IpsrStep1Body();
+  currentImpactAreaID = null;
+  impactAreasData = [
+    { id: 1, imageRoute: '1', selected: false, color: '#ec7427' },
+    { id: 2, imageRoute: '2', selected: false, color: '#1275ba' },
+    { id: 3, imageRoute: '3', selected: false, color: '#fdca3d' },
+    { id: 4, imageRoute: '4', selected: false, color: '#377431' },
+    { id: 5, imageRoute: '5', selected: false, color: '#8ebf3e' }
+  ];
   constructor() {}
+  selectImpactArea(impactAreaItem) {
+    this.impactAreasData.map((iaitem: any) => (iaitem.selected = false));
+    impactAreaItem.selected = true;
+    this.currentImpactAreaID = impactAreaItem.id;
+  }
 }
