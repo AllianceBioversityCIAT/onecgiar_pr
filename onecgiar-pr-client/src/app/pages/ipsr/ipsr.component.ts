25d11360a950e7d5c04a19153cd99f492a456685
yecksin <yecksin@gmail.com>
2023-04-03 10:35:53 -0500

    :sparkles: feat(IPSR/Innovation Packages) LFUB-155: select the "all initiatives" button if is an admin user.

--- onecgiar-pr-client/src/app/pages/ipsr/ipsr.component.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/ipsr.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/shared/services/api/api.service';
 
 @Component({
   selector: 'app-ipsr',
   templateUrl: './ipsr.component.html',
   styleUrls: ['./ipsr.component.scss']
 })
-export class IpsrComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class IpsrComponent {
+  constructor(public api: ApiService) {}
 }
