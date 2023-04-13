039ef8136fce462522d3ab22942bc755d933f18f
yecksin <yecksin@gmail.com>
2023-04-12 21:34:44 -0500

    :sparkles: feat(IPSR/step 1/IPSR Innovation use pathway) LFUB-164: Range and radio button fields added

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n3/step-n3.component.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n3/step-n3.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IpsrStep3Body } from './model/Ipsr-step-3-body.model';
 
 @Component({
   selector: 'app-step-n3',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./step-n3.component.scss']
 })
 export class StepN3Component implements OnInit {
-
-  constructor() { }
+  ipsrStep3Body = new IpsrStep3Body();
+  radioOptions = [
+    { id: 1, name: 'Yes, an expert workshop was organized' },
+    { id: 2, name: 'No expert workshop was organized' }
+  ];
+  constructor() {}
 
   ngOnInit(): void {
+    this.getSectionInformation();
   }
 
+  getSectionInformation() {}
+  onSaveSection() {}
+
+  readinessLevelSelfAssessmentText() {
+    return `<a href="https://drive.google.com/file/d/1muDLtqpeaSCIX60g6qQG_GGOPR61Rq7E/view"  class="open_route" target="_blank">Click here</a> to see the definition of all readiness levels`;
+  }
+  useLevelDelfAssessment() {
+    return `<a href="https://drive.google.com/file/d/1muDLtqpeaSCIX60g6qQG_GGOPR61Rq7E/view"  class="open_route" target="_blank">Click here</a> to see the definition of all readiness levels`;
+  }
 }
--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n3/step-n3.module.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n3/step-n3.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { StepN3RoutingModule } from './step-n3-routing.module';
 import { StepN3Component } from './step-n3.component';
-
+import { CustomFieldsModule } from 'src/app/custom-fields/custom-fields.module';
 
 @NgModule({
-  declarations: [
-    StepN3Component
-  ],
-  imports: [
-    CommonModule,
-    StepN3RoutingModule
-  ]
+  declarations: [StepN3Component],
+  imports: [CommonModule, StepN3RoutingModule, CustomFieldsModule]
 })
-export class StepN3Module { }
+export class StepN3Module {}
