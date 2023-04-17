7a83d16a2f84eeabd1ae0de821e223a9fccad53b
yecksin <yecksin@gmail.com>
2023-04-17 08:13:30 -0500

    :sparkles: feat(step 4) LFUB-330: Ipsr-step-4-body.model model updated

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n4/model/Ipsr-step-4-body.model.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n4/model/Ipsr-step-4-body.model.ts
@@ -1,4 +1,51 @@
 export class IpsrStep4Body {
-  list1 = [];
-  list2 = [];
+  ipsr_pictures: IpsrpictureStep4[] = [];
+  ipsr_materials: IpsrpictureStep4[] = [];
+  link_workshop_list: string = null;
+  initiative_expected_investment: Initiativeexpectedinvestment[] = [];
+  initiative_unit_time_id: number = null;
+  initiative_expected_time: string = null;
+  bilateral_expected_investment: BilateralexpectedinvestmentStep4[] = [];
+  bilateral_unit_time_id: number = null;
+  bilateral_expected_time: string = null;
+  institutions_expected_investment: InstitutionsexpectedinvestmentStep4[] = [];
+  partner_unit_time_id: number = null;
+  partner_expected_time: string = null;
+}
+
+export class InstitutionsexpectedinvestmentStep4 {
+  institution: Institution = new Institution();
+  budget: Budget = new Budget();
+}
+
+class Budget {
+  result_institutions_budget_id: number;
+  in_kind: string;
+  in_cash: string;
+  is_determined: number;
+}
+
+class Institution {
+  id: number;
+  is_active: boolean;
+  deliveries: number[];
+}
+
+export class BilateralexpectedinvestmentStep4 {
+  non_pooled_projetct_budget_id: number;
+  in_cash: string;
+  in_kind: string;
+  is_determined: number;
+}
+
+interface Initiativeexpectedinvestment {
+  result_initiative_budget_id: number;
+  current_year: string;
+  next_year: string;
+  is_determined: number;
+  obj_result_initiative: any;
+}
+
+export class IpsrpictureStep4 {
+  link: string;
 }
