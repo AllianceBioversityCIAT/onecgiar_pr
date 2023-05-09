9a5431b7bfd7417d17ffd967e7cb315b6b321ae3
yecksin <yecksin@gmail.com>
2023-05-09 09:31:18 -0500

    :sparkles: feat(step n1 / copy scaling ambition body) LFUB-422: toast fixed

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-scaling-ambition-blurb/step-n1-scaling-ambition-blurb.component.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-scaling-ambition-blurb/step-n1-scaling-ambition-blurb.component.ts
@@ -5,11 +5,12 @@ import { MessageService } from 'primeng/api';
 @Component({
   selector: 'app-step-n1-scaling-ambition-blurb',
   templateUrl: './step-n1-scaling-ambition-blurb.component.html',
-  styleUrls: ['./step-n1-scaling-ambition-blurb.component.scss']
+  styleUrls: ['./step-n1-scaling-ambition-blurb.component.scss'],
+  providers: [MessageService]
 })
 export class StepN1ScalingAmbitionBlurbComponent {
   @Input() body = new IpsrStep1Body();
-  constructor(private messageSE: MessageService) {}
+  constructor(public messageSE: MessageService) {}
   onCopy() {
     this.messageSE.add({ key: 'copykey', severity: 'info', summary: 'Copied', detail: 'Description copied' });
   }
