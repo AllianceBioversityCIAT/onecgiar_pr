7beed9e5f1fd55d325ded2a1575f6d8941cd37cb
Juan Delgado <juandelgadog98@gmail.com>
2023-05-11 10:37:52 -0500

    :sparkles: feat(Submission):  Added services to submit an Innovation Package result. LFUB-490

--- onecgiar-pr-server/src/api/ipsr/results-innovation-packages-validation-module/results-innovation-packages-validation-module.service.ts
+++ onecgiar-pr-server/src/api/ipsr/results-innovation-packages-validation-module/results-innovation-packages-validation-module.service.ts
@@ -1,7 +1,7 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreateResultsInnovationPackagesValidationModuleDto } from './dto/create-results-innovation-packages-validation-module.dto';
 import { UpdateResultsInnovationPackagesValidationModuleDto } from './dto/update-results-innovation-packages-validation-module.dto';
-import { GetValidationSectionInnoPckgDto } from './dto/get-validation-section-inno-pckg.dto';
+import { GetValidationSectionInnoPckgDto, GreenchecksResponse } from './dto/get-validation-section-inno-pckg.dto';
 import { IpsrRepository } from '../ipsr.repository';
 import { HandlersError } from '../../../shared/handlers/error.utils';
 import { ResultRepository } from '../../results/result.repository';
@@ -17,7 +17,7 @@ export class ResultsInnovationPackagesValidationModuleService {
     private readonly _resultInnovationPackageValidationModuleRepository: ResultsInnovationPackagesValidationModuleRepository,
   ) { }
 
-  async getGreenchecksByinnovationPackage(resultId: number) {
+  async getGreenchecksByinnovationPackage(resultId: number): Promise<GreenchecksResponse> {
     try {
       const resultExist = await this._resultRepository.findOneBy({ id: resultId, is_active: true });
       const ipExist = await this._ipsrReposotory.findOneBy({ result_innovation_package_id: resultId, ipsr_role_id: 1, is_active: true });
@@ -31,11 +31,10 @@ export class ResultsInnovationPackagesValidationModuleService {
       if (!ipExist) {
         throw {
           response: {},
-          message: 'Results Not Found',
+          message: 'Innovation package Not Found',
           status: HttpStatus.NOT_FOUND,
         };
       }
-
       let response: GetValidationSectionInnoPckgDto[] = [];
 
       const gi = await this._resultInnovationPackageValidationModuleRepository.generalInformation(resultId);
@@ -46,9 +45,13 @@ export class ResultsInnovationPackagesValidationModuleService {
       const stepFour: GetValidationSectionInnoPckgDto = await this._resultInnovationPackageValidationModuleRepository.stepFour(resultId);
       const pathway = {
         sectionName: 'IPSR Innovation use pathway',
-        validation: parseInt(stepOne?.validation) && parseInt(stepTwo?.validation) && parseInt(stepThree?.validation) && parseInt(stepFour?.validation)
+        validation: parseInt(stepOne?.validation) && parseInt(stepTwo?.validation) && parseInt(stepThree?.validation) && parseInt(stepFour?.validation),
       }
       const links = this._resultInnovationPackageValidationModuleRepository.links();
+
+      const validResult = {
+        validation: parseInt(gi?.validation) && parseInt(contributors?.validation) && pathway?.validation && parseInt(links?.validation)
+      }
 
       return {
         response: {
@@ -63,13 +66,38 @@ export class ResultsInnovationPackagesValidationModuleService {
             stepTwo,
             stepThree,
             stepFour
-          ]
+          ],
+          validResult
         },
         message: 'Sections have been successfully validated',
         status: HttpStatus.OK,
       };
     } catch (error) {
-      return this._handlersError.returnErrorRes({ error });
+      return {
+        response: {
+          mainSection: [],
+          stepSections: [
+          ],
+          validResult: { validation: 0 }
+        },
+        message: error,
+        status: HttpStatus.BAD_REQUEST,
+      };
     }
   }
+
+  // async innovationPackageValidation(resultId: number) {
+  //   try {
+  //     const validation: GreenchecksResponse = await this.getGreenchecksByinnovationPackage(resultId);
+
+  //     return {
+  //       message: 'Sections have been successfully validated',
+  //       status: HttpStatus.OK,
+  //     };
+  //   } catch (error) {
+  //     return this._handlersError.returnErrorRes(error);
+  //   }
+  // }
+
+
 }
