07867860f6b56f1d6d06f3218ec15a9c48cf54f2
jdmunozg <juan.david.m.g1004@gmail.com>
2023-05-12 11:10:33 -0500

    refactor(Geo scope update dropdowns and selected)

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-creator/components/ipsr-geoscope-creator/components/sub-geoscope/sub-geoscope.component.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-creator/components/ipsr-geoscope-creator/components/sub-geoscope/sub-geoscope.component.ts
@@ -11,10 +11,12 @@ export class SubGeoscopeComponent implements OnInit {
   @Input() index: any;
   countrySelected:any;
   subNationalOne:any = [];
-  subNationalOneSelected = [];
+  subNationalOneSelected = null;
   subNationalTwo:any = [];
-  subNationalTwoSelected = [];
+  subNationalTwoSelected = null;
   @Output() selectOptionEvent = new EventEmitter();
+  showNationalLevelSelect:boolean = true;
+  showNationalLevelTwoSelect:boolean = true;
   constructor(public api: ApiService) { }
 
   ngOnInit(): void {
@@ -22,7 +24,7 @@ export class SubGeoscopeComponent implements OnInit {
     
   }
 
-  getSubNationalLevelOne(){
+  getSubNationalLevelOne(index){
     this.subNationalOne = []
     let isoAlpha = this.body.filter((resp) => this.countrySelected == resp.id)[0]['iso_alpha_2'];
     console.log(isoAlpha);
@@ -30,23 +32,61 @@ export class SubGeoscopeComponent implements OnInit {
       this.subNationalOne = resp['response']
       console.log(resp);
     })
-    
+    this.subNationalOneSelected = null;
+    this.showNationalLevelSelect = false;
+    setTimeout(() => {
+      this.showNationalLevelSelect = true;
+    }, 300);
   }
 
-  getSSubNationalLevelTwo(){
+  getSSubNationalLevelTwo(index){
     this.subNationalTwo = []
     let isoAlpha = this.body.filter((resp) => this.countrySelected == resp.id)[0]['iso_alpha_2'];
     let adminCode = this.subNationalOne.filter((resp) => this.subNationalOneSelected == resp.geonameId)[0]['adminCode1'];
-    this.body.filter((resp) => this.countrySelected == resp.id)[0]['result_countries_sub_national'].push(this.subNationalOne.filter((resp) => this.subNationalOneSelected == resp.geonameId)[0])
-    console.log(isoAlpha);
     this.api.resultsSE.getSubNationalLevelTwo(isoAlpha,adminCode).subscribe((resp) => {
     this.subNationalTwo = resp['response']
       console.log(resp);
     })
+
+    this.subNationalTwoSelected = null;
+    this.showNationalLevelTwoSelect = false;
+    setTimeout(() => {
+      this.showNationalLevelTwoSelect = true;
+    }, 300);
+   
+    
   }
 
   delete(index){
     this.selectOptionEvent.emit(index);
+    this.body.filter((resp) => this.countrySelected == resp.id)[0]['result_countries_sub_national'].splice(index, 1);
+    console.log(this.body);
+    
+  }
+
+
+  selectSubLevelTwo(index){
+    let subContriesSave = this.body.filter((resp) => this.countrySelected == resp.id)[0]['result_countries_sub_national'];
+    let infoSublevel = this.subNationalTwo.filter((resp) => this.subNationalTwoSelected == resp.geonameId)[0];
+    let infoSublevelOne = this.subNationalOne.filter((resp) => this.subNationalOneSelected == resp.geonameId)[0];
+    if(subContriesSave.length >= index + 1){
+      const subCountriesSave = {
+        sub_level_one_id: infoSublevelOne['geonameId'],
+        sub_level_one_name:infoSublevelOne['name'],
+        sub_level_two_id: infoSublevel['geonameId'],
+        sub_level_two_name:infoSublevel['name']
+      }
+      subContriesSave[index] =  subCountriesSave;
+    }else{
+      const subCountriesSave = {
+        sub_level_one_id: infoSublevelOne['geonameId'],
+        sub_level_one_name:infoSublevelOne['name'],
+        sub_level_two_id: infoSublevel['geonameId'],
+        sub_level_two_name:infoSublevel['name']
+      }
+      subContriesSave.push(subCountriesSave);
+    }
+    console.log(this.body);
   }
 
 }
---
+++ onecgiar-pr-server/src/migrations/1683903888275-insertInTablecomplementaryInnovationEnablerType.ts
@@ -0,0 +1,12 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class insertInTablecomplementaryInnovationEnablerType1683903888275 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(``);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
