9cdf45e76e3f13325ac20faf277783f7f9405906
German Martinez <g.martinez@cgiar.org>
2023-05-15 08:46:05 -0500

    ♻️ refactor(PDF generation): Adjusting PDF footer and generation strategy. LFUB-476

--- onecgiar-pr-server/src/api/platform-report/platform-report.controller.ts
+++ onecgiar-pr-server/src/api/platform-report/platform-report.controller.ts
@@ -22,28 +22,14 @@ export class PlatformReportController {
     @Param('id', ParseIntPipe) id: number,
     @Res({ passthrough: true }) res: Response,
   ): Promise<StreamableFile> {
-    const utcDateString = new Date().toISOString();
-    /*
-      TODO to be fixed when the Temporal proposal on ECMA passes. This is a
-      dirty workaround to "get the current UTC Date". When this is done, the
-      "hackyUtcDate" Date object is created with the time as if it were on UTC, but
-      with on the "Colombia timezone". As I do not care about the timezone and
-      I am only interested in the milis to give a correct representation of
-      the date when the report is generated, this suffices for now.
-    */
-    const hackyUtcDate = new Date(
-      utcDateString.slice(0, utcDateString.length - 1),
-    );
-    const dateFormatted = DateFormatter.forFilename(hackyUtcDate);
+    const result =
+      await this._platformReportService.getFullResultReportByResultCode(id);
 
     res.set({
       //'Content-Type': 'application/pdf',
-      'Content-Disposition': `filename="PRMS-Result-${id}_${dateFormatted.date}_${dateFormatted.time}.pdf"`,
+      'Content-Disposition': `filename="PRMS-Result-${id}_${result.filename_date}.pdf"`,
     });
 
-    return new StreamableFile(
-      await this._platformReportService.getFullResultReportByResultCode(id),
-      { type: 'application/pdf' },
-    );
+    return new StreamableFile(result.pdf, { type: 'application/pdf' });
   }
 }
