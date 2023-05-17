9e83c2baaee7f75827a6cd06d05045c961834f61
yecksin <yecksin@gmail.com>
2023-05-17 16:05:43 -0500

    :recycle: refactor(PDF) LFUB-539: alert messages updated

--- onecgiar-pr-client/src/app/shared/components/page-alert/page-alert.component.ts
+++ onecgiar-pr-client/src/app/shared/components/page-alert/page-alert.component.ts
@@ -27,10 +27,10 @@ export class PageAlertComponent {
   get message() {
     switch (this.type) {
       case 'warning':
-        return 'Please review the parameters provided. It seems that some of them are incorrect or missing.';
+        return 'Please review the link provided. It seems there is something incorrect or missing.';
 
       case 'error':
-        return 'We are currently experiencing a technical issue with the tool. Please contact the support team at PRMSTechSupport@cgiar.org and try again in a few minutes.';
+        return 'We are currently experiencing a technical issue with the tool. Please contact the support team at PRMSTechSupport@cgiar.org and try again in a few hours.';
     }
     return '';
   }
