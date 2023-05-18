709db8bd4978994b122a07943cda086bd3d8f36d
Juan Delgado <juandelgadog98@gmail.com>
2023-05-18 11:07:22 -0500

    :recycle: refactor(Step 1): Update service to save or update the sex_and_age_disaggregation and how_many fields. LFUB-561

--- onecgiar-pr-server/src/api/ipsr/innovation-pathway/innovation-pathway.controller.spec.ts
+++ onecgiar-pr-server/src/api/ipsr/innovation-pathway/innovation-pathway.controller.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InnovationPathwayController } from './innovation-pathway.controller';
-import { InnovationPathwayService } from './innovation-pathway-step-one.service';
 
 describe('InnovationPathwayController', () => {
   let controller: InnovationPathwayController;
@@ -8,7 +7,6 @@ describe('InnovationPathwayController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [InnovationPathwayController],
-      providers: [InnovationPathwayService],
     }).compile();
 
     controller = module.get<InnovationPathwayController>(InnovationPathwayController);
