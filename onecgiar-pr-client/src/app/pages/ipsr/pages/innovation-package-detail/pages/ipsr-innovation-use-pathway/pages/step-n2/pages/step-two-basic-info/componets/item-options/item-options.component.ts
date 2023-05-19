df6d430d38cb7bd83aaf5820736215bbeb7294b8
jdmunozg <juan.david.m.g1004@gmail.com>
2023-05-19 11:09:53 -0500

    refactor( if admin go to step 2.2 else step 3) LFUB-581

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n2/pages/step-two-basic-info/componets/item-options/item-options.component.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n2/pages/step-two-basic-info/componets/item-options/item-options.component.ts
@@ -24,6 +24,8 @@ export class ItemOptionsComponent implements OnInit {
     console.log(index);
     if (index !=-1) {
     this.selectedOne = this.selectedOne.concat(category.subCategories)
+   }if(index ==-1){
+    this.selectedOne = []
    }
     console.log(this.selectedOne);
     console.log(this.selectedCategories);
@@ -31,8 +33,23 @@ export class ItemOptionsComponent implements OnInit {
     
   }
 
-  subSelectes(){
+  subSelectes(category){
     console.log(this.selectedOne);
+    if(category.subCategories.length != this.selectedOne.length){
+      let index = this.selectedCategories.findIndex(resp => category.complementary_innovation_enabler_types_id == resp.complementary_innovation_enabler_types_id);
+      if(index != -1){
+        this.selectedCategories.splice(index,1)
+        console.log(this.selectedCategories);
+        
+      }
+      
+    }else{
+      this.selectedCategories.push(category);
+      console.log(this.selectedCategories);
+      
+    }
   }
+
+  
 
 }
