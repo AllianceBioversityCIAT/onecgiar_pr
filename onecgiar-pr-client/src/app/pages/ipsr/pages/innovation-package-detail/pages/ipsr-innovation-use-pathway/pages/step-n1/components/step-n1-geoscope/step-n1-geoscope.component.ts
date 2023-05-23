1c9e348b71cd8f0be86ab9784c0a7797068e6d1c
jdmunozg <juan.david.m.g1004@gmail.com>
2023-05-23 11:12:05 -0500

    refactor(Read only in geo scope step 1)

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-geoscope/step-n1-geoscope.component.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n1/components/step-n1-geoscope/step-n1-geoscope.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RegionsCountriesService } from '../../../../../../../../../../shared/services/global/regions-countries.service';
 import { IpsrStep1Body } from '../../model/Ipsr-step-1-body.model';
+import { ApiService } from 'src/app/shared/services/api/api.service';
 
 @Component({
   selector: 'app-step-n1-geoscope',
@@ -15,7 +16,7 @@ export class StepN1GeoscopeComponent {
     { full_name: 'Country', id: 4 },
     { full_name: 'Sub-national', id: 5 }
   ];
-  constructor(public regionsCountriesSE: RegionsCountriesService) {}
+  constructor(public regionsCountriesSE: RegionsCountriesService, public api: ApiService,) {}
   get selectRegionsDescription() {
     return `The list of regions below follows the UN <a class="open_route" href="https://unstats.un.org/unsd/methodology/m49/" target='_blank'>(M.49)</a> standard`;
   }
