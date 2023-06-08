b5516d01cc818d3b0d564d8e386bd7329ab5cf82
Juan Delgado <juandelgadog98@gmail.com>
2023-06-08 09:38:24 -0500

    :sparkles: feat(Step 2.1): Updated entity, added migration, and updated services to save or updated the Working on Innovation field. LFUB-763

--- onecgiar-pr-server/src/api/ipsr/results-complementary-innovations/entities/results-complementary-innovation.entity.ts
+++ onecgiar-pr-server/src/api/ipsr/results-complementary-innovations/entities/results-complementary-innovation.entity.ts
@@ -1,52 +1,73 @@
-import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { BaseEntity } from '../../../../shared/entities/base-entity';
 import { Result } from '../../../results/entities/result.entity';
 import { Version } from '../../../results/versions/entities/version.entity';
 import { ResultsComplementaryInnovationsFunction } from '../../results-complementary-innovations-functions/entities/results-complementary-innovations-function.entity';
 
 @Entity('results_complementary_innovation')
-export class ResultsComplementaryInnovation extends BaseEntity{
-    @PrimaryGeneratedColumn({
-        name: 'result_complementary_innovation_id',
-        type: 'bigint'
-    })
-    result_complementary_innovation_id: number;
+export class ResultsComplementaryInnovation extends BaseEntity {
+  @PrimaryGeneratedColumn({
+    name: 'result_complementary_innovation_id',
+    type: 'bigint',
+  })
+  result_complementary_innovation_id: number;
 
-    @Column({
-        name: 'result_id',
-        type: 'bigint',
-        nullable: false
-    })
-    result_id: number;
+  @Column({
+    name: 'result_id',
+    type: 'bigint',
+    nullable: false,
+  })
+  result_id: number;
 
-    @Column({
-        name: 'short_title',
-        type: 'text',
-        nullable: true
-    })
-    short_title!: string;
+  @Column({
+    name: 'short_title',
+    type: 'text',
+    nullable: true,
+  })
+  short_title!: string;
 
-    @Column({
-        name: 'other_funcions',
-        type: 'text',
-        nullable: true
-    })
-    other_funcions!: string;
+  @Column({
+    name: 'other_funcions',
+    type: 'text',
+    nullable: true,
+  })
+  other_funcions!: string;
 
-    //
+  @Column({
+    name: 'projects_organizations_working_on_innovation',
+    type: 'boolean',
+  })
+  projects_organizations_working_on_innovation: boolean;
 
-    @ManyToOne(() => Result, r => r.id)
-    @JoinColumn({
-        name: 'result_id'
-    })
-    obj_result: Result;
+  @Column({
+    name: 'specify_projects_organizations',
+    type: 'text',
+    nullable: true,
+  })
+  specify_projects_organizations!: string;
 
-    @ManyToOne(() => Version, v => v.id)
-    @JoinColumn({
-        name: 'version_id'
-    })
-    obj_version: Version;
+  @ManyToOne(() => Result, (r) => r.id)
+  @JoinColumn({
+    name: 'result_id',
+  })
+  obj_result: Result;
 
-    @OneToMany(() => ResultsComplementaryInnovationsFunction, cif => cif.obj_result_complementary_innovation)
-    functions: ResultsComplementaryInnovationsFunction[];
+  @ManyToOne(() => Version, (v) => v.id)
+  @JoinColumn({
+    name: 'version_id',
+  })
+  obj_version: Version;
+
+  @OneToMany(
+    () => ResultsComplementaryInnovationsFunction,
+    (cif) => cif.obj_result_complementary_innovation,
+  )
+  functions: ResultsComplementaryInnovationsFunction[];
 }
