7ed5bd4c12ef3f6ea8450cd724b2f66dabc67834
yecksin <yecksin@gmail.com>
2023-06-15 01:21:32 -0500

    :recycle: refactor(Read only) LFUB-760: step 4 read only improved

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n4/components/step-n4-bilateral-investment-table/step-n4-bilateral-investment-table.component.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n4/components/step-n4-bilateral-investment-table/step-n4-bilateral-investment-table.component.ts
@@ -14,17 +14,20 @@ export class StepN4BilateralInvestmentTableComponent implements OnInit {
   @Input() body = new IpsrStep4Body();
   showModal = false;
   isInitiative = true;
-  constructor(public rolesSE: RolesService, public manageRipUnitTimeSE: ManageRipUnitTimeService, public ipsrDataControlSE: IpsrDataControlService, public api: ApiService ) {}
+  constructor(public rolesSE: RolesService, public manageRipUnitTimeSE: ManageRipUnitTimeService, public ipsrDataControlSE: IpsrDataControlService, public api: ApiService) {}
 
   ngOnInit(): void {
     setTimeout(() => {
-      this.isInitiative =  this.api.rolesSE.validateInitiative(this.ipsrDataControlSE.initiative_id);
+      this.isInitiative = this.api.rolesSE.validateInitiative(this.ipsrDataControlSE.initiative_id);
     }, 500);
-    
-       
   }
 
   deleteBilateral(bilateral) {
     bilateral.is_active = false;
+  }
+
+  hasElementsWithId(list, attr) {
+    const finalList = this.api.rolesSE.readOnly ? list.filter(item => item[attr]) : list.filter(item => item.is_active != false);
+    return finalList.length;
   }
 }
