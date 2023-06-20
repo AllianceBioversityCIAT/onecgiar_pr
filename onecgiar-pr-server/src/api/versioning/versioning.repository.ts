b8bfab9bd1a2e7ed8cf4185084106e431fada997
davinzifc <47566127+davinzifc@users.noreply.github.com>
2023-06-20 16:37:55 -0500

    :sparkles: feat(version service): the old findVersion service was updated and a new service was created for internal app use. LFUB-791

--- onecgiar-pr-server/src/api/versioning/versioning.repository.ts
+++ onecgiar-pr-server/src/api/versioning/versioning.repository.ts
@@ -15,7 +15,7 @@ export class VersionRepository extends Repository<Version> {
   async getBaseVersion() {
     const queryData = `
     select min(v.id) as id,
-    		v.version_name,
+    		v.phase_name as version_name,
     		null as start_date,
     		null as end_date
     from \`version\` v 
@@ -24,7 +24,8 @@ export class VersionRepository extends Repository<Version> {
     		and
     		(v.end_date  = ''
     		or v.end_date is null)
-    group by v.id, v.version_name;
+        and v.status > 0
+    group by v.id, v.phase_name;
     `;
     try {
       const version: Version[] = await this.query(queryData);
--- onecgiar-pr-server/src/api/versioning/versioning.service.ts
+++ onecgiar-pr-server/src/api/versioning/versioning.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateVersioningDto } from './dto/create-versioning.dto';
 import { UpdateVersioningDto } from './dto/update-versioning.dto';
+import { Version } from './entities/version.entity';
+import { VersionRepository } from './versioning.repository';
 
 @Injectable()
 export class VersioningService {
+  constructor(private readonly _versionRepository: VersionRepository) {}
+
+  async $_findActiveVersion(): Promise<Version> {
+    try {
+      const version = await this._versionRepository.findOne({
+        where: {
+          status: true,
+          is_active: true,
+        },
+      });
+
+      return version;
+    } catch (error) {
+      return null;
+    }
+  }
+
   create(createVersioningDto: CreateVersioningDto) {
     return 'This action adds a new versioning';
   }
