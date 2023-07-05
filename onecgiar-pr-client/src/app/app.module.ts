e51ab44c8c7cf0a40844a99938d2faf07a6c4870
yecksin <yecksin@gmail.com>
2023-07-05 08:32:43 -0500

    :recycle: refactor(footer) LFUB-857: footer developed

--- onecgiar-pr-client/src/app/app.module.ts
+++ onecgiar-pr-client/src/app/app.module.ts
@@ -17,10 +17,11 @@ import { GoogleAnalyticsComponent } from './shared/components/external-tools/com
 import { ShareRequestModalModule } from './pages/results/pages/result-detail/components/share-request-modal/share-request-modal.module';
 import { YmzListStructureItemModule } from './shared/directives/ymz-list-structure-item/ymz-list-structure-item.module';
 import { FooterModule } from './shared/components/footer/footer.module';
+import { DialogModule } from 'primeng/dialog';
 
 @NgModule({
   declarations: [AppComponent, NavigationBarComponent, HeaderPanelComponent, ExternalToolsComponent, TestEnvironmentLabelComponent, TawkComponent, GoogleAnalyticsComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule, CustomFieldsModule, ShareRequestModalModule, YmzListStructureItemModule, FooterModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule, CustomFieldsModule, ShareRequestModalModule, YmzListStructureItemModule, FooterModule, DialogModule],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: GeneralInterceptorService, multi: true }],
   bootstrap: [AppComponent]
 })
