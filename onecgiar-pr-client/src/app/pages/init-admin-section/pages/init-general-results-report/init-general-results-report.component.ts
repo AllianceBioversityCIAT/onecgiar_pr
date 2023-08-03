840da28175cd476e408d2b9b78c36c14ef3a2ffd
Juan Delgado <juandelgadog98@gmail.com>
2023-08-03 16:14:27 -0500

    :sparkles: feat(Excel export): Added new method in front to generate multiple excel sheets with the ToC info. LFUB-941

--- onecgiar-pr-client/src/app/pages/init-admin-section/pages/init-general-results-report/init-general-results-report.component.ts
+++ onecgiar-pr-client/src/app/pages/init-admin-section/pages/init-general-results-report/init-general-results-report.component.ts
@@ -63,25 +63,29 @@ export class InitGeneralResultsReportComponent {
   }
 
   dataToExport = [];
-
+  tocToExport = [];
+  
   async exportExcel(resultsRelected) {
     this.dataToExport = [];
+    this.tocToExport = [];
     this.requesting = true;
     this.requestCounter = 0;
-
+  
     const list = [];
     const uniqueResultCodesSet = new Set(resultsRelected.map((item: any) => item.result_code));
     const uniqueResultCodes = [...uniqueResultCodesSet];
-
+  
     uniqueResultCodes?.forEach(element => {
       list.push(element);
     });
-    for (const key in list) {
-      await this.POST_excelFullReportPromise(list[key], key);
-    }
-    this.exportTablesSE.exportExcel(this.dataToExport, 'results_list');
+  
+    // Usar Promise.all para esperar a que todas las promesas se resuelvan
+    await Promise.all(list.map((element, key) => this.POST_excelFullReportPromise(element, key)));
+  
+    this.exportTablesSE.exportMultipleSheetsExcel(this.dataToExport, 'results_list', null, this.tocToExport);
     this.requesting = false;
   }
+  
 
   // validateLength(obj) {
   //   Object.keys(obj[0]).forEach(item => console.log(item + ': ' + obj[0][item]?.length));
@@ -95,7 +99,8 @@ export class InitGeneralResultsReportComponent {
           // this.validateLength(response);
           //(response);
           this.requestCounter++;
-          this.dataToExport.push(...response);
+          this.dataToExport.push(...response.fullReport);
+          this.tocToExport.push(...response.resultsAgaintsToc);
           resolve(null);
         },
         err => {
