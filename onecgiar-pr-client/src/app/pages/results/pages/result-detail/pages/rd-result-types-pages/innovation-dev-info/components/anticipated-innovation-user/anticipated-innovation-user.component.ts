3f9ef141801f024746075811b4309aca101361ea
yecksin <yecksin@gmail.com>
2023-08-14 11:37:27 -0500

    :sparkles: feat(Innovations Dev) LFUB-567:  Missing field group segments were created

---
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-result-types-pages/innovation-dev-info/components/anticipated-innovation-user/anticipated-innovation-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anticipated-innovation-user',
+  templateUrl: './anticipated-innovation-user.component.html',
+  styleUrls: ['./anticipated-innovation-user.component.scss']
+})
+export class AnticipatedInnovationUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-result-types-pages/innovation-dev-info/components/estimates/estimates.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estimates',
+  templateUrl: './estimates.component.html',
+  styleUrls: ['./estimates.component.scss']
+})
+export class EstimatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
