1374c9295ee81af3f48dac3b750ea0833353505a
yecksin <yecksin@gmail.com>
2023-08-18 08:28:40 -0500

    :recycle: refactor (Innovations Dev) LFUB-567: Consume control list in intellectual-property-rights

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-result-types-pages/innovation-dev-info/model/InnovationDevelopmentQuestions.model.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-result-types-pages/innovation-dev-info/model/InnovationDevelopmentQuestions.model.ts
@@ -1,123 +1,123 @@
-export interface InnovationDevelopmentQuestions {
-  responsible_innovation_and_scaling: Responsibleinnovationandscaling[];
-  intellectual_property_rights: Intellectualpropertyrights;
-  innovation_team_diversity: Innovationteamdiversity;
+export class InnovationDevelopmentQuestions {
+  responsible_innovation_and_scaling: Responsibleinnovationandscaling[] = [];
+  intellectual_property_rights: Intellectualpropertyrights = new Intellectualpropertyrights();
+  innovation_team_diversity: Innovationteamdiversity = new Innovationteamdiversity();
 }
 
-interface Innovationteamdiversity {
-  result_question_id: string;
-  question_text: string;
-  question_description: string;
-  result_type_id: number;
+export class Innovationteamdiversity {
+  result_question_id: string = '';
+  question_text: string = '';
+  question_description: string = '';
+  result_type_id: number = 0;
   parent_question_id?: any;
-  question_type_id: string;
-  question_level: string;
-  options: Option3[];
+  question_type_id: string = '';
+  question_level: string = '';
+  options: Option3[] = [];
 }
 
-interface Option3 {
-  result_question_id: string;
-  question_text: string;
+export class Option3 {
+  result_question_id: string = '';
+  question_text: string = '';
   question_description?: any;
-  result_type_id: number;
-  parent_question_id: string;
-  question_type_id: string;
-  question_level: string;
-  subOptions: Option2[];
+  result_type_id: number = 0;
+  parent_question_id: string = '';
+  question_type_id: string = '';
+  question_level: string = '';
+  subOptions: Option2[] = [];
 }
 
-interface Intellectualpropertyrights {
-  result_question_id: string;
-  question_text: string;
+export class Intellectualpropertyrights {
+  result_question_id: string = '';
+  question_text: string = '';
   question_description?: any;
-  result_type_id: number;
+  result_type_id: number = 0;
   parent_question_id?: any;
-  question_type_id: string;
-  question_level: string;
-  q1: Q12;
-  q2: Q12;
-  q3: Q3;
+  question_type_id: string = '';
+  question_level: string = '';
+  q1: Q12 = new Q12();
+  q2: Q12 = new Q12();
+  q3: Q3 = new Q3();
 }
 
-interface Q3 {
-  result_question_id: string;
-  question_text: string;
-  question_description: string;
-  result_type_id: number;
-  parent_question_id: string;
-  question_type_id: string;
-  question_level: string;
-  options: Option2[];
+export class Q3 {
+  result_question_id: string = '';
+  question_text: string = '';
+  question_description: string = '';
+  result_type_id: number = 0;
+  parent_question_id: string = '';
+  question_type_id: string = '';
+  question_level: string = '';
+  options: Option2[] = [];
 }
 
-interface Q12 {
-  result_question_id: string;
-  question_text: string;
+export class Q12 {
+  result_question_id: string = '';
+  question_text: string = '';
   question_description?: any;
-  result_type_id: number;
-  parent_question_id: string;
-  question_type_id: string;
-  question_level: string;
-  options: Option2[];
+  result_type_id: number = 0;
+  parent_question_id: string = '';
+  question_type_id: string = '';
+  question_level: string = '';
+  options: Option2[] = [];
 }
 
-interface Option2 {
-  result_question_id: string;
-  question_text: string;
+export class Option2 {
+  result_question_id: string = '';
+  question_text: string = '';
   question_description?: any;
-  result_type_id: number;
-  parent_question_id: string;
-  question_type_id: string;
-  question_level: string;
+  result_type_id: number = 0;
+  parent_question_id: string = '';
+  question_type_id: string = '';
+  question_level: string = '';
   answer_boolean?: any;
   answer_text?: any;
-  subOptions: any[];
+  subOptions: any[] = [];
 }
 
-interface Responsibleinnovationandscaling {
-  result_question_id: string;
-  question_text: string;
+export class Responsibleinnovationandscaling {
+  result_question_id: string = '';
+  question_text: string = '';
   question_description?: any;
-  result_type_id: number;
+  result_type_id: number = 0;
   parent_question_id?: any;
-  question_type_id: string;
-  question_level: string;
-  q1: Q1;
-  q2: Q1;
+  question_type_id: string = '';
+  question_level: string = '';
+  q1: Q1 = new Q1();
+  q2: Q1 = new Q1();
 }
 
-interface Q1 {
-  result_question_id: string;
-  question_text: string;
-  question_description: string;
-  result_type_id: number;
-  parent_question_id: string;
-  question_type_id: string;
-  question_level: string;
-  options: Option[];
+export class Q1 {
+  result_question_id: string = '';
+  question_text: string = '';
+  question_description: string = '';
+  result_type_id: number = 0;
+  parent_question_id: string = '';
+  question_type_id: string = '';
+  question_level: string = '';
+  options: Option[] = [];
 }
 
-interface Option {
-  result_question_id: string;
-  question_text: string;
+export class Option {
+  result_question_id: string = '';
+  question_text: string = '';
   question_description?: any;
-  result_type_id: number;
-  parent_question_id: string;
-  question_type_id: string;
-  question_level: string;
+  result_type_id: number = 0;
+  parent_question_id: string = '';
+  question_type_id: string = '';
+  question_level: string = '';
   answer_boolean?: any;
   answer_text?: any;
-  subOptions: SubOption[];
+  subOptions: SubOption[] = [];
 }
 
-interface SubOption {
-  result_question_id: string;
-  question_text: string;
+export class SubOption {
+  result_question_id: string = '';
+  question_text: string = '';
   question_description?: any;
-  result_type_id: number;
-  parent_question_id: string;
-  question_type_id: string;
-  question_level: string;
+  result_type_id: number = 0;
+  parent_question_id: string = '';
+  question_type_id: string = '';
+  question_level: string = '';
   answer_boolean?: any;
   answer_text?: any;
 }
