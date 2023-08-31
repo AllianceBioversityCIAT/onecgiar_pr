2aee1ea06beaac622cc5d7fd7f06fb2baad9f895
davinzifc <47566127+davinzifc@users.noreply.github.com>
2023-08-31 11:56:58 -0500

    :bug: fix(actors): fixed bug replacing actors

--- onecgiar-pr-server/src/api/ipsr/innovation-pathway/innovation-pathway-step-one.service.ts
+++ onecgiar-pr-server/src/api/ipsr/innovation-pathway/innovation-pathway-step-one.service.ts
@@ -1152,7 +1152,8 @@ export class InnovationPathwayStepOneService {
           const whereOptions: any = {
             actor_type_id: el.actor_type_id,
             result_id: result.id,
-            result_actors_id: el.result_actors_id,
+            result_actors_id: el.result_actors_id ?? IsNull(),
+            is_active: true,
           };
 
           if (!el?.result_actors_id) {
@@ -1162,11 +1163,9 @@ export class InnovationPathwayStepOneService {
                   el?.other_actor_type || IsNull();
                 break;
             }
-            delete whereOptions.result_actors_id;
           } else {
             delete whereOptions.actor_type_id;
           }
-
           actorExists = await this._resultActorRepository.findOne({
             where: whereOptions,
           });
