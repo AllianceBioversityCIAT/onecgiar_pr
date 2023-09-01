aa3240e0c82977b53c718664033914802e58545b
davinzifc <47566127+davinzifc@users.noreply.github.com>
2023-09-01 14:06:25 -0500

    :bug: fix(partners): Fixed bug that prevented partners from being saved.

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-partners/rd-partners.service.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-partners/rd-partners.service.ts
@@ -39,41 +39,16 @@ export class RdPartnersService {
   }*/
 
   getSectionInformation(no_applicable_partner?) {
-    this.api.resultsSE
-      .GET_partnersSection()
-      /*
-        after the first request, we need to get the centers, so we pipe
-        the response of the first observable to perform two operations
-      */
-      .pipe(
-        /*
-          we condition the execution of the second observable (get centers) 
-          depending if the current result is a knowledge product
-        */
-        filter(() => {
-          return this.api.dataControlSE.isKnowledgeProduct;
-        }),
-        /*
-          we perform the operations of the first observable and return the
-          second observable, depending on the previous filter
-        */
-        concatMap(({ response }) => {
-          this.partnersBody = response;
-          if (no_applicable_partner === true || no_applicable_partner === false) this.partnersBody.no_applicable_partner = no_applicable_partner;
-
-          return this.api.resultsSE.GET_centers();
-        })
-      )
-      .subscribe({
-        //we perform the operations of the second observable
-        next: ({ response }) => {
-          this.centers = response;
-        },
-        // we handle the errors of both observables
-        error: err => {
-          if (no_applicable_partner === true || no_applicable_partner === false) this.partnersBody.no_applicable_partner = no_applicable_partner;
-        }
-      });
+    this.api.resultsSE.GET_partnersSection().subscribe(
+      ({ response }) => {
+        //(response);
+        this.partnersBody = response;
+        if (no_applicable_partner === true || no_applicable_partner === false) this.partnersBody.no_applicable_partner = no_applicable_partner;
+      },
+      err => {
+        if (no_applicable_partner === true || no_applicable_partner === false) this.partnersBody.no_applicable_partner = no_applicable_partner;
+      }
+    );
   }
 }
 /*
