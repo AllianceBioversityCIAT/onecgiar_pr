59471ecc75338b201f0e3820a9c2b80b2d458365
yecksin <yecksin@gmail.com>
2023-09-01 09:18:16 -0500

    :sparkles: feat(innovation-use-info) LFUB-1018: Join service with the innovation-use-form.component information

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-result-types-pages/innovation-use-info/model/innovationUseInfoBody.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-result-types-pages/innovation-use-info/model/innovationUseInfoBody.ts
@@ -1,12 +1,5 @@
 export class InnovationUseInfoBody {
-  public result_innovation_use_id?: number;
-  public male_using: number;
-  public female_using: number;
-  public other: otherMeasuresInterface[] = [];
-}
-
-interface otherMeasuresInterface {
-  result_innovations_use_measure_id?: number;
-  unit_of_measure?: string;
-  quantity?: number;
+  actors: any[];
+  measures: any[];
+  organization: any[];
 }
