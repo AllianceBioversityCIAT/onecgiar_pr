8ce0ed311725bf90fec8cf2ffd1d6877eb4bede9
Kevin Collazos <kevcollazos@gmail.com>
2023-09-25 15:21:04 -0500

    :recycle: refactor (notifications) LFUB-1117:  Changes in validations for notifications with result_level_id equal to 3

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/components/share-request-modal/share-request-modal.component.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/components/share-request-modal/share-request-modal.component.ts
@@ -5,7 +5,6 @@ import { RolesService } from '../../../../../../shared/services/global/roles.ser
 import { ShareRequestModalService } from './share-request-modal.service';
 import { Router } from '@angular/router';
 import { RetrieveModalService } from '../retrieve-modal/retrieve-modal.service';
-import { environment } from 'src/environments/environment';
 import { ResultsNotificationsService } from '../../../results-outlet/pages/results-notifications/results-notifications.service';
 import { RdTheoryOfChangesServicesService } from '../../pages/rd-theory-of-change/rd-theory-of-changes-services.service';
 
@@ -14,7 +13,7 @@ import { RdTheoryOfChangesServicesService } from '../../pages/rd-theory-of-chang
   templateUrl: './share-request-modal.component.html',
   styleUrls: ['./share-request-modal.component.scss']
 })
-export class ShareRequestModalComponent {
+export class ShareRequestModalComponent implements OnInit {
   requesting = false;
   allInitiatives = [];
   showForm = true;
@@ -24,23 +23,19 @@ export class ShareRequestModalComponent {
       initiative_id: 10
     }
   ];
+
   constructor(public retrieveModalSE: RetrieveModalService, public api: ApiService, public rolesSE: RolesService, public shareRequestModalSE: ShareRequestModalService, private router: Router, public resultsNotificationsSE: ResultsNotificationsService, public theoryOfChangesServices: RdTheoryOfChangesServicesService) {}
+
   ngOnInit(): void {
     this.shareRequestModalSE.shareRequestBody = new ShareRequestBody();
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    //(this.api.resultsSE.ipsrDataControlSE.inIpsr);
     this.GET_AllInitiatives();
   }
 
   cleanObject() {
-    //('cleanForm');
     this.showForm = false;
     this.shareRequestModalSE.shareRequestBody = new ShareRequestBody();
 
     setTimeout(() => {
-      //(this.allInitiatives);
-
       this.showForm = true;
     }, 0);
   }
@@ -48,10 +43,9 @@ export class ShareRequestModalComponent {
   onRequest() {
     this.requesting = true;
     this.shareRequestModalSE.shareRequestBody.initiativeShareId.push(this.shareRequestModalSE.shareRequestBody.initiative_id);
-    //(this.shareRequestModalSE.shareRequestBody);
+
     this.api.resultsSE.POST_createRequest(this.shareRequestModalSE.shareRequestBody).subscribe(
       resp => {
-        //(this.api.dataControlSE.currentResult);
         this.api.dataControlSE.showShareRequest = false;
 
         this.api.alertsFe.show({ id: 'requesqshared', title: `Request sent`, description: `Once your request is accepted, the result can be mapped to your Initiative's ToC.`, status: 'success' });
@@ -73,7 +67,6 @@ export class ShareRequestModalComponent {
   }
 
   modelChange() {
-    //('modelChange');
     this.showTocOut = false;
 
     setTimeout(() => {
@@ -81,18 +74,16 @@ export class ShareRequestModalComponent {
       this.shareRequestModalSE.shareRequestBody['official_code'] = iniciativeSelected[0].official_code;
       this.shareRequestModalSE.shareRequestBody['short_name'] = iniciativeSelected[0].short_name;
       this.showTocOut = true;
-      //(this.shareRequestModalSE.shareRequestBody.initiative_id);
     }, 500);
   }
 
   acceptOrReject() {
     const body = { ...this.api.dataControlSE.currentNotification, ...this.shareRequestModalSE.shareRequestBody, request_status_id: 2, bodyNewTheoryOfChanges: this.theoryOfChangesServices.body };
-    //(this.api.resultsSE.ipsrDataControlSE.inIpsr);
+
     this.requesting = true;
 
     this.api.resultsSE.PATCH_updateRequest(body).subscribe(
       resp => {
-        //(resp);
         this.api.dataControlSE.showShareRequest = false;
         this.api.alertsFe.show({ id: 'noti', title: `Request sent`, description: `Once your request is accepted, the result can be mapped to your Initiative's ToC.`, status: 'success' });
         this.requesting = false;
