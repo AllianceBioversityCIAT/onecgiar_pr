d35e404850d679b0417adcda588a90a3d1288225
Kevin Collazos <kevcollazos@gmail.com>
2023-09-26 16:21:39 -0500

    :sparkles: feat(Policy Change) LFUB-1192: added validations and new fields for policy change type result

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-result-types-pages/policy-change-info/policy-change-info.component.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-result-types-pages/policy-change-info/policy-change-info.component.ts
@@ -3,7 +3,6 @@ import { ApiService } from '../../../../../../../shared/services/api/api.service
 import { InnovationUseInfoBody } from './model/innovationUseInfoBody';
 import { PolicyControlListService } from '../../../../../../../shared/services/global/policy-control-list.service';
 import { InstitutionsService } from '../../../../../../../shared/services/global/institutions.service';
-import { formatCurrency, getCurrencySymbol } from '@angular/common';
 
 @Component({
   selector: 'app-policy-change-info',
@@ -13,6 +12,12 @@ import { formatCurrency, getCurrencySymbol } from '@angular/common';
 export class PolicyChangeInfoComponent implements OnInit {
   innovationUseInfoBody = new InnovationUseInfoBody();
   cantidad: string = '';
+  relatedTo: string = '';
+  relatedToOptions = [
+    { value: 'policy-change', label: 'Policy change' },
+    { value: 'capacity-development', label: 'The capacity development of key actors in a policy process' }
+  ];
+
   constructor(public api: ApiService, public policyControlListSE: PolicyControlListService, public institutionsService: InstitutionsService) {}
 
   ngOnInit(): void {
@@ -26,12 +31,13 @@ export class PolicyChangeInfoComponent implements OnInit {
       } catch (error) {}
     });
   }
+
   getSectionInformation() {
     this.api.resultsSE.GET_policyChanges().subscribe(({ response }) => {
-      //(response);
       this.innovationUseInfoBody = response;
     });
   }
+
   policyTypeDescriptions() {
     return `<strong>Policy type guidance</strong> <ul>
     <li><strong>Policy or strategy:</strong> Policies or strategies include written decisions on, or commitments to, a particular course of action by an institution (policy); or a (government, NGO, private sector) high-level plan outlining how a particular course of action will be carried out (strategy). These documents show the intent of an organization or entity. Examples are country growth strategies, country agricultural policies, organization strategic plans or road maps. This could also be observed as information campaigns (e.g., for improved diets). These documents set the goalposts but then require other instruments for implementation.</li>
@@ -39,22 +45,13 @@ export class PolicyChangeInfoComponent implements OnInit {
     <li><strong>Program, budget or investment:</strong> These are implementing mechanisms that often follow from a strategy, policy or law. There is typically a well-defined set of actions outlined over a specific period of time and with a specific budgetary amount attached. National Agricultural Investment Plans is an example, the budget within a ministry is another, investments from the private sector fit here, as well as programs launched by public, private and NGO sectors.</li>
     </ul>`;
   }
+
   onSaveSection() {
     this.api.resultsSE.PATCH_policyChanges(this.innovationUseInfoBody).subscribe(resp => {
-      //(resp);
       this.getSectionInformation();
     });
+    console.log(this.relatedTo);
   }
-  showAlerts() {}
 
-  // updateValue(value: string) {
-  //   let val = parseFloat(value.replace(/\$/g, ''));
-  //   this.innovationUseInfoBody.amount = val;
-  //   if (Number.isNaN(val)) {
-  //     val = 0;
-  //   }
-  //   this.cantidad = formatCurrency(val, 'en-US', getCurrencySymbol('USD', 'wide'));
-  //   //(this.innovationUseInfoBody.amount);
-  //   //(this.innovationUseInfoBody.amount);
-  // }
+  showAlerts() {}
 }
