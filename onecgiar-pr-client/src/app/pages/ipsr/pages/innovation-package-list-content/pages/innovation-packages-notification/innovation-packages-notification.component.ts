08cf8ca21bf3a104cd640d9a454a32f5b7a26b97
Kevin Collazos <kevcollazos@gmail.com>
2023-09-28 09:07:45 -0500

    :sparkles: feat (IPSR Notifications): added sharing functionality for IPSR notification section

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-list-content/pages/innovation-packages-notification/innovation-packages-notification.component.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-list-content/pages/innovation-packages-notification/innovation-packages-notification.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ShareRequestModalService } from 'src/app/pages/results/pages/result-detail/components/share-request-modal/share-request-modal.service';
 import { ResultsNotificationsService } from 'src/app/pages/results/pages/results-outlet/pages/results-notifications/results-notifications.service';
+import { ModuleTypeEnum, StatusPhaseEnum } from 'src/app/shared/enum/api.enum';
 import { ApiService } from 'src/app/shared/services/api/api.service';
 
 @Component({
@@ -9,15 +10,34 @@ import { ApiService } from 'src/app/shared/services/api/api.service';
   styleUrls: ['./innovation-packages-notification.component.scss']
 })
 export class InnovationPackagesNotificationComponent implements OnInit {
+  allInitiatives = [];
+  phaseList = [];
+  phaseFilter = null;
+  initiativeIdFilter = null;
+
   constructor(public api: ApiService, private shareRequestModalSE: ShareRequestModalService, public resultsNotificationsSE: ResultsNotificationsService) {}
+
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
+    this.getAllPhases();
+    this.GET_AllInitiatives();
     this.api.updateUserData(() => {
       this.resultsNotificationsSE.get_section_innovation_packages();
     });
-    ////(this.resultsNotificationsSE);
 
     this.api.dataControlSE.inNotifications = true;
+  }
+
+  GET_AllInitiatives() {
+    if (!this.api.rolesSE.isAdmin) return;
+    this.api.resultsSE.GET_AllInitiatives().subscribe(({ response }) => {
+      this.allInitiatives = response;
+    });
+  }
+
+  getAllPhases() {
+    this.api.resultsSE.GET_versioning(StatusPhaseEnum.ALL, ModuleTypeEnum.IPSR).subscribe(({ response }) => {
+      this.phaseList = response;
+      this.phaseFilter = (this.phaseList.find(phase => phase.status) as any)?.id;
+    });
   }
 }
