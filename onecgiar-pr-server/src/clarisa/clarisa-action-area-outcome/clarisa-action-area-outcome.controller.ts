8f7deedaa5bf349a90ae897aa8d3d6906e692298
davinzifc <47566127+davinzifc@users.noreply.github.com>
2023-11-01 08:24:30 -0500

    :bug: fix(action-areas-outcomes): get was added again

--- onecgiar-pr-server/src/clarisa/clarisa-action-area-outcome/clarisa-action-area-outcome.controller.ts
+++ onecgiar-pr-server/src/clarisa/clarisa-action-area-outcome/clarisa-action-area-outcome.controller.ts
@@ -1,9 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { ClarisaActionAreaOutcomeService } from './clarisa-action-area-outcome.service';
+import { ResponseInterceptor } from '../../shared/Interceptors/Return-data.interceptor';
 
 @Controller()
 export class ClarisaActionAreaOutcomeController {
   constructor(
     private readonly clarisaActionAreaOutcomeService: ClarisaActionAreaOutcomeService,
   ) {}
+
+  @Get('all')
+  @UseInterceptors(ResponseInterceptor)
+  async findAll() {
+    return this.clarisaActionAreaOutcomeService.findAll();
+  }
 }
