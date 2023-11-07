714fe7ae34645fcd93d5fd7f431dca43cdfd7a3b
Juan Delgado <juandelgadog98@gmail.com>
2023-11-07 14:41:13 -0500

    :sparkles: feat(AA, IA and SDGs): Removed Green Checks validations for AA, IA and SDG. LFUB-1396

--- onecgiar-pr-server/src/api/results/results-toc-results/results-toc-results.service.ts
+++ onecgiar-pr-server/src/api/results/results-toc-results/results-toc-results.service.ts
@@ -778,7 +778,6 @@ export class ResultsTocResultsService {
   }
 
   async getVersionId(result_id: number, init: number) {
-    console.log("🚀 ~ file: results-toc-results.service.ts:781 ~ ResultsTocResultsService ~ getVersionId ~ result_id:", result_id)
     try {
       const resultinit = await this._clarisaInitiatives.findOne({
         select: ['toc_id'],
