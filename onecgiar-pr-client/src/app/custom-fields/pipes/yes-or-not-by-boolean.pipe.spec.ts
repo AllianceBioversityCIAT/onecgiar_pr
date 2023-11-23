77d4d43c52d63be5fadaf610cfb6cf5f7fde5d44
Stephany Ceron Salas <stephany1803@gmail.com>
2023-11-23 18:43:26 -0500

    test(YesOrNotByBooleanPipe): add unit tests

--- onecgiar-pr-client/src/app/custom-fields/pipes/yes-or-not-by-boolean.pipe.spec.ts
+++ onecgiar-pr-client/src/app/custom-fields/pipes/yes-or-not-by-boolean.pipe.spec.ts
@@ -1,8 +1,25 @@
 import { YesOrNotByBooleanPipe } from './yes-or-not-by-boolean.pipe';
 
 describe('YesOrNotByBooleanPipe', () => {
-  it('create an instance', () => {
-    const pipe = new YesOrNotByBooleanPipe();
-    expect(pipe).toBeTruthy();
+  let pipe: YesOrNotByBooleanPipe;
+
+  beforeEach(() => {
+    pipe = new YesOrNotByBooleanPipe();
+  });
+
+  it('should transform true to "Yes"', () => {
+    const result = pipe.transform(true);
+    expect(result).toBe('Yes');
+  });
+
+  it('should transform false to "No"', () => {
+    const result = pipe.transform(false);
+    expect(result).toBe('No');
+  });
+
+  it('should return the same value', () => {
+    const value = 'SomeValue';
+    const result = pipe.transform(value);
+    expect(result).toBe(value);
   });
 });
