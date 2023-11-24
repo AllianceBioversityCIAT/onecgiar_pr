d75edaa5b42b1b56cb8516ac4d89139d6d67a7f7
Juan Delgado <juandelgadog98@gmail.com>
2023-11-24 14:41:50 -0500

    :recycle: refactor(WPs): Fix planned_result condition in ResultsPackageTocResultService and ResultsTocResultsService

--- onecgiar-pr-server/src/api/ipsr/results-package-toc-result/results-package-toc-result.service.ts
+++ onecgiar-pr-server/src/api/ipsr/results-package-toc-result/results-package-toc-result.service.ts
@@ -481,11 +481,10 @@ export class ResultsPackageTocResultService {
             false,
             [init.id],
           );
-        result_toc_results.forEach((el) => {
-          el['toc_level_id'] =
-            el['planned_result'] == false && el['planned_result'] != null
-              ? 3
-              : el['toc_level_id'];
+        result_toc_results.forEach(el => {
+          if (el['planned_result'] === false) {
+            el['toc_level_id'] = 3;
+          }
         });
 
         resTocResConResponse = [
