1571f4a0496919dc552ff8bfa53ed957e31a2aaf
stephanyceron <stephany1803@gmail.com>
2023-12-03 23:30:44 -0500

    test(PdfActionsComponent): add unit tests

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/components/pdf-actions/pdf-actions.component.spec.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/components/pdf-actions/pdf-actions.component.spec.ts
@@ -1,14 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PdfActionsComponent } from './pdf-actions.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PdfIconComponent } from '../../../../../../shared/icon-components/pdf-icon/pdf-icon.component';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { ResultsApiService } from '../../../../../../shared/services/api/results-api.service';
+import { environment } from '../../../../../../../environments/environment';
 
 describe('PdfActionsComponent', () => {
   let component: PdfActionsComponent;
   let fixture: ComponentFixture<PdfActionsComponent>;
+  let mockResultsApiService:any
 
   beforeEach(async () => {
+    mockResultsApiService = {
+      currentResultCode: 1,
+      currentResultPhase: 1
+    }
     await TestBed.configureTestingModule({
-      declarations: [ PdfActionsComponent ]
+      declarations: [ 
+        PdfActionsComponent,
+        PdfIconComponent
+      ],
+      providers: [
+        {
+          provide: ResultsApiService,
+          useValue: mockResultsApiService
+        },
+      ],
+      imports: [
+        HttpClientTestingModule,
+        ClipboardModule
+      ],
     })
     .compileComponents();
 
@@ -17,7 +39,21 @@ describe('PdfActionsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Copy Link', () => {
+    it('should emit copyEvent when copyLink is called', () => {
+      const spy = jest.spyOn(component.copyEvent, 'emit');
+
+      component.copyLink();
+
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe('Link Generation', () => {
+    it('should generate the correct link', () => {
+      const link = component.link;
+
+      expect(link).toBe(`${environment.frontBaseUrl}reports/result-details/1?phase=1`);
+    });
   });
 });
