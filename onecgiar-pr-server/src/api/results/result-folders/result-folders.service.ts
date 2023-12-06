24be6ba26e2b21bfb32502d87d049ce715de0d92
davinzifc <47566127+davinzifc@users.noreply.github.com>
2023-12-06 15:11:29 -0500

    :sparkles: feat(result folders): The type one report was modified and the entity result folders was created. LFUB-1481

---
+++ onecgiar-pr-server/src/api/results/result-folders/result-folders.service.ts
@@ -0,0 +1,129 @@
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { CreateResultFolderDto } from './dto/create-result-folder.dto';
+import { TokenDto } from '../../../shared/globalInterfaces/token.dto';
+import { ResultFolderRepository } from './repositories/result-folder-type.repository copy';
+import { ResultFolderTypeRepository } from './repositories/result-folder-type.repository';
+import { ReturnResponse } from '../../../shared/handlers/error.utils';
+import { ResultFolder } from './entities/result-folder.entity';
+import { env } from 'process';
+import { CreateResultFolderTypeDto } from './dto/create-result-folder-type.dto';
+import { ResultFolderType } from './entities/result-folder-type.entity';
+import { ResultFolderTypeData } from '../../../shared/constants/result-folder-type.enum';
+import { ActiveElementData } from '../../../shared/constants/active-element.enum';
+
+@Injectable()
+export class ResultFoldersService {
+  constructor(
+    private readonly _returnResponse: ReturnResponse,
+    private readonly _resultFolderRepository: ResultFolderRepository,
+    private readonly _resultFolderTypeRepository: ResultFolderTypeRepository,
+  ) {}
+
+  async createResultFolder(
+    user: TokenDto,
+    createResultFolderDto: CreateResultFolderDto,
+  ) {
+    try {
+      const responseData: ResultFolder =
+        await this._resultFolderRepository.save({
+          created_by: user.id,
+          ...createResultFolderDto,
+        });
+
+      return this._returnResponse.format({
+        message: 'Result folder created successfully',
+        response: responseData,
+        statusCode: HttpStatus.CREATED,
+      });
+    } catch (error) {
+      return this._returnResponse.format(error, !env.IS_PRODUCTION);
+    }
+  }
+
+  async createResultFolderType(
+    user: TokenDto,
+    createResultFolderTypeDto: CreateResultFolderTypeDto,
+  ) {
+    try {
+      const responseData: ResultFolderType =
+        await this._resultFolderTypeRepository.save({
+          created_by: user.id,
+          ...createResultFolderTypeDto,
+        });
+      return this._returnResponse.format({
+        message: 'Result folder type created successfully',
+        response: responseData,
+        statusCode: HttpStatus.CREATED,
+      });
+    } catch (error) {
+      return this._returnResponse.format(error, !env.IS_PRODUCTION);
+    }
+  }
+
+  async findResultFolders(
+    type: string,
+    status: string,
+    id: number,
+    phase: number,
+  ) {
+    try {
+      let where: any = {};
+
+      if (id) {
+        where.result_folders_id = id;
+      }
+
+      if (phase) {
+        where.phase_id = phase;
+      }
+
+      const objStatus = ActiveElementData.getFromName(status);
+      if (objStatus) {
+        where.is_active = objStatus.value;
+      }
+
+      const objFolderType = ResultFolderTypeData.getFromName(type);
+      if (objFolderType) {
+        where.folder_type_id = objFolderType.value;
+      }
+
+      const responseData: ResultFolder[] =
+        await this._resultFolderRepository.find({ where: where });
+
+      return this._returnResponse.format({
+        message: 'Result folders retrieved successfully',
+        response: responseData,
+        statusCode: HttpStatus.OK,
+      });
+    } catch (error) {
+      return this._returnResponse.format(error, !env.IS_PRODUCTION);
+    }
+  }
+
+  async findResultFoldersType(status: string, id: number) {
+    try {
+      let where: any = {};
+
+      if (id) {
+        where.result_folders_type_id = id;
+      }
+
+      const objStatus = ActiveElementData.getFromName(status);
+      if (objStatus) {
+        where.is_active = objStatus.value;
+      }
+
+      const responseData: ResultFolderType[] =
+        await this._resultFolderTypeRepository.find({ where: where });
+
+      return this._returnResponse.format({
+        message: 'Result folders retrieved successfully',
+        response: responseData,
+        statusCode: HttpStatus.OK,
+      });
+    } catch (error) {
+      return this._returnResponse.format(error, !env.IS_PRODUCTION);
+    }
+  }
+}
+
