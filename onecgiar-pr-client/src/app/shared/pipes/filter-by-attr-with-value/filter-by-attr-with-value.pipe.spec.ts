9bd9a1585298ab2a8bd059c13e6888e1156c6430
Kevin Collazos <kevcollazos@gmail.com>
2023-12-14 09:25:57 -0500

    shared/components .spec.ts removed

--- onecgiar-pr-client/src/app/shared/pipes/filter-by-attr-with-value/filter-by-attr-with-value.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterByAttrWithValuePipe } from './filter-by-attr-with-value.pipe';
-
-describe('FilterByAttrWithValuePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterByAttrWithValuePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- onecgiar-pr-client/src/app/shared/pipes/filter-by-text.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterByTextPipe } from './filter-by-text.pipe';
-
-describe('FilterByTextPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterByTextPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
