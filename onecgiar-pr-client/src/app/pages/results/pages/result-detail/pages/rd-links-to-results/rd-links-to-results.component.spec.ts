ab27f73a806877bcf609bd6c4836abfdf2773d7b
stephanyceron <stephany1803@gmail.com>
2023-12-13 21:45:43 -0500

    test(SimilarResultsPipe): update unit tests

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-links-to-results/rd-links-to-results.component.spec.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-links-to-results/rd-links-to-results.component.spec.ts
@@ -3,7 +3,10 @@ import { RdLinksToResultsComponent } from './rd-links-to-results.component';
 import { LinksToResultsGlobalComponent } from '../../../../../../shared/sections-components/links-to-results-global/links-to-results-global.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FilterResultNotLinkedPipe } from './pipe/filter-result-not-linked.pipe';
-
+import { PrFieldHeaderComponent } from '../../../../../../custom-fields/pr-field-header/pr-field-header.component';
+import { SaveButtonComponent } from '../../../../../../custom-fields/save-button/save-button.component';
+import { TableModule } from 'primeng/table';
+import { DetailSectionTitleComponent } from '../../../../../../custom-fields/detail-section-title/detail-section-title.component';
 describe('RdLinksToResultsComponent', () => {
   let component: RdLinksToResultsComponent;
   let fixture: ComponentFixture<RdLinksToResultsComponent>;
@@ -13,16 +16,19 @@ describe('RdLinksToResultsComponent', () => {
       declarations: [
         RdLinksToResultsComponent,
         LinksToResultsGlobalComponent,
-        FilterResultNotLinkedPipe
+        FilterResultNotLinkedPipe,
+        PrFieldHeaderComponent,
+        SaveButtonComponent,
+        DetailSectionTitleComponent
       ],
       imports: [
         HttpClientTestingModule,
+        TableModule
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RdLinksToResultsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
