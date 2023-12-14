e9306e82318ef297b447547aa16347cdbca3738d
stephanyceron <stephany1803@gmail.com>
2023-12-13 19:50:52 -0500

    test(ConfirmationKPComponent): add unit tests

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-general-information/components/confirmation-kp/confirmation-kp.component.spec.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-general-information/components/confirmation-kp/confirmation-kp.component.spec.ts
@@ -1,23 +1,92 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ConfirmationKPComponent } from './confirmation-kp.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PrSelectComponent } from '../../../../../../../../custom-fields/pr-select/pr-select.component';
+import { PdfIconComponent } from '../../../../../../../../shared/icon-components/pdf-icon/pdf-icon.component';
+import { LabelNamePipe } from '../../../../../../../../custom-fields/pr-select/label-name.pipe';
+import { FormsModule } from '@angular/forms';
+import { PrFieldHeaderComponent } from '../../../../../../../../custom-fields/pr-field-header/pr-field-header.component';
+import { of, throwError, throwIfEmpty } from 'rxjs';
+import { ApiService } from '../../../../../../../../shared/services/api/api.service';
+import { HttpHeaders } from '@angular/common/http';
+
 
 describe('ConfirmationKPComponent', () => {
   let component: ConfirmationKPComponent;
   let fixture: ComponentFixture<ConfirmationKPComponent>;
+  let mockApiService: any;
+  const mBlob = { size: 1024, type: "application/pdf" };
+  const mockResponse = {
+    body: mBlob,
+    headers: new HttpHeaders({
+      'content-disposition': `attachment; filename="ResultReport.pdf"`,
+    })
+  }
 
   beforeEach(async () => {
+    mockApiService = {
+      resultsSE: {
+        GET_downloadPDF: () => of(mockResponse)
+      }
+    }
+
     await TestBed.configureTestingModule({
-      declarations: [ ConfirmationKPComponent ]
+      declarations: [
+        ConfirmationKPComponent,
+        PrSelectComponent,
+        PdfIconComponent,
+        LabelNamePipe,
+        PrFieldHeaderComponent
+      ],
+      imports: [
+        HttpClientTestingModule,
+        FormsModule,
+      ],
+      providers: [
+        {
+          provide: ApiService,
+          useValue: mockApiService
+        },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ConfirmationKPComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('downloadPDF', () => {
+    beforeEach(() => {
+      const mockBody = {
+        result_code: 1,
+        version_id: 1
+      }
+      component.body = mockBody;
+    })
+    it('should set isDownloading to false after download', () => {
+      global.URL.createObjectURL = jest.fn();
+      global.URL.revokeObjectURL = jest.fn();
+
+      jest.spyOn(document, 'createElement');
+      const spy = jest.spyOn(mockApiService.resultsSE, 'GET_downloadPDF');
+      const spyCreateElement = jest.spyOn(document, 'createElement');
+
+      component.downloadPDF();
+
+      expect(spy).toHaveBeenCalled();
+      expect(spyCreateElement).toHaveBeenCalled();
+      expect(component.isDownloading).toBeFalsy();
+    });
+
+    it('should handle errors during PDF download and set isDownloading to false', () => {
+      const mockError = new Error('Download error');
+      const spy = jest.spyOn(mockApiService.resultsSE, 'GET_downloadPDF')
+      .mockReturnValue(throwError(mockError));
+  
+      component.downloadPDF();
+  
+      expect(component.isDownloading).toBeFalsy();
+    });
   });
 });
