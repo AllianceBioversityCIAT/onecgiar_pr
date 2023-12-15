9810b5306cdc33682c6b29ab1bad35fda3894f97
Juan Delgado <juandelgadog98@gmail.com>
2023-12-15 09:12:21 -0500

    :sparkles: feat(ToC Indicators V2): Add ToC progressive narrative to excel report

--- onecgiar-pr-server/src/api/results/result.repository.ts
+++ onecgiar-pr-server/src/api/results/result.repository.ts
@@ -2045,7 +2045,8 @@ left join clarisa_countries cc3
         DATE(tri.target_date) AS 'Target date',
         tri.unit_messurament AS 'Unit of measure',
         tri.target_value AS 'Target value',
-        IFNULL(rtri.indicator_contributing, 'Not provided') AS 'Target contribution from the result'
+        IFNULL(rtri.indicator_contributing, 'Not provided') AS 'Target contribution from the result',
+        IFNULL(rtr.toc_progressive_narrative, 'Not aplicable') AS 'ToC progressive narrative'
     FROM
         result r
         LEFT JOIN results_toc_result rtr ON rtr.results_id = r.id
