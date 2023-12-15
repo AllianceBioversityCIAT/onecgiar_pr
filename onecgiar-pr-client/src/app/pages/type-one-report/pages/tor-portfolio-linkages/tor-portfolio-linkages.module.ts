3e9312b65c64930c5cfd1d077814aaecf44fc9a8
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2023-12-15 10:57:08 -0500

    :recycle: refactor (T1R) LFUB-1444: tor-portfolio-linkages updated

--- onecgiar-pr-client/src/app/pages/type-one-report/pages/tor-portfolio-linkages/tor-portfolio-linkages.module.ts
+++ onecgiar-pr-client/src/app/pages/type-one-report/pages/tor-portfolio-linkages/tor-portfolio-linkages.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { TorPortfolioLinkagesRoutingModule } from './tor-portfolio-linkages-routing.module';
 import { TorPortfolioLinkagesComponent } from './tor-portfolio-linkages.component';
-
+import { CustomFieldsModule } from '../../../../custom-fields/custom-fields.module';
 
 @NgModule({
-  declarations: [
-    TorPortfolioLinkagesComponent
-  ],
-  imports: [
-    CommonModule,
-    TorPortfolioLinkagesRoutingModule
-  ]
+  declarations: [TorPortfolioLinkagesComponent],
+  imports: [CommonModule, TorPortfolioLinkagesRoutingModule, CustomFieldsModule]
 })
-export class TorPortfolioLinkagesModule { }
+export class TorPortfolioLinkagesModule {}
