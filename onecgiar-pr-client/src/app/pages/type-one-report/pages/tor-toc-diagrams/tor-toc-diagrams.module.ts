be1e928558765a925f07b83a24e616b2b8664dc8
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2023-12-15 09:46:35 -0500

    :recycle: refactor (T1R) LFUB-1444: toc-diagrams updated

--- onecgiar-pr-client/src/app/pages/type-one-report/pages/tor-toc-diagrams/tor-toc-diagrams.module.ts
+++ onecgiar-pr-client/src/app/pages/type-one-report/pages/tor-toc-diagrams/tor-toc-diagrams.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { TorTocDiagramsRoutingModule } from './tor-toc-diagrams-routing.module';
 import { TorTocDiagramsComponent } from './tor-toc-diagrams.component';
+import { CustomFieldsModule } from '../../../../custom-fields/custom-fields.module';
 
 @NgModule({
   declarations: [TorTocDiagramsComponent],
-  imports: [CommonModule, TorTocDiagramsRoutingModule]
+  imports: [CommonModule, TorTocDiagramsRoutingModule, CustomFieldsModule]
 })
 export class TorTocDiagramsModule {}
