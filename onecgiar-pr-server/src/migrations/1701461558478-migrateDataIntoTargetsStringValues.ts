d0867628721ffbddf03f0fa2848ef2cafd951928
Juan Delgado <juandelgadog98@gmail.com>
2023-12-15 10:02:40 -0500

    :recycle: refactor(ToC Indicators V2): Update migrateDataIntoTargetsStringValues migration

--- onecgiar-pr-server/src/migrations/1701461558478-migrateDataIntoTargetsStringValues.ts
+++ onecgiar-pr-server/src/migrations/1701461558478-migrateDataIntoTargetsStringValues.ts
@@ -86,6 +86,85 @@ export class migrateDataIntoTargetsStringValues1701461558478
         `);
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            UPDATE
+                result_indicators_targets rit
+            SET
+                contributing_indicator = target_progress_narrative
+            WHERE
+                target_progress_narrative REGEXP '[a-zA-Z]|\[^a-zA-Z0-9]';
+        `);
+
+    await queryRunner.query(`
+            UPDATE
+                result_indicators_targets rit
+            SET
+                target_progress_narrative = NULL
+            WHERE
+                target_progress_narrative REGEXP '[a-zA-Z]|\[^a-zA-Z0-9]';
+        `);
+
+    await queryRunner.query(`
+            ALTER TABLE
+                result_indicators_targets
+            ADD
+                COLUMN contributing_indicator_tmp TEXT NULL;
+        `);
+
+    await queryRunner.query(`
+        UPDATE
+            result_indicators_targets rit
+        SET
+            contributing_indicator_tmp = target_progress_narrative
+        WHERE
+            target_progress_narrative IS NOT NULL;
+        `);
+
+    await queryRunner.query(`
+        UPDATE
+            result_indicators_targets rit
+        SET
+            contributing_indicator_tmp = NULL
+        WHERE
+            contributing_indicator_tmp IS NULL
+            OR contributing_indicator_tmp = ''
+            OR contributing_indicator_tmp = 0;
+        `);
+
+    await queryRunner.query(`
+        UPDATE
+            result_indicators_targets rit
+        SET
+            contributing_indicator = NULL
+        WHERE
+            contributing_indicator IS NULL
+            OR contributing_indicator = ''
+            OR contributing_indicator = 0;
+        `);
+
+    await queryRunner.query(`
+        ALTER TABLE
+            result_indicators_targets
+        MODIFY
+            COLUMN contributing_indicator DECIMAL(6, 2);
+        `);
+
+    await queryRunner.query(`
+        UPDATE
+            result_indicators_targets rit
+        SET
+            contributing_indicator = CAST(contributing_indicator_tmp AS DECIMAL(6, 2))
+        WHERE
+            contributing_indicator_tmp IS NOT NULL;
+        `);
+
+    await queryRunner.query(`
+        ALTER TABLE
+            result_indicators_targets
+        DROP
+            COLUMN contributing_indicator_tmp;
+        `);
+  }
 }
 
