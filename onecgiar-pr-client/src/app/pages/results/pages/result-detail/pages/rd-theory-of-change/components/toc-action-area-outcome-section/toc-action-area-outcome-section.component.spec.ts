279085de8bf80f70ef74149923216af79fe6a96d
stephanyceron <stephany1803@gmail.com>
2024-01-02 20:07:07 -0500

    test(TocActionAreaOutcomeSectionComponent): add unit tests

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-theory-of-change/components/toc-action-area-outcome-section/toc-action-area-outcome-section.component.spec.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-theory-of-change/components/toc-action-area-outcome-section/toc-action-area-outcome-section.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TocActionAreaOutcomeSectionComponent } from './toc-action-area-outcome-section.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoDataTextComponent } from '../../../../../../../../custom-fields/no-data-text/no-data-text.component';
+import { TocInitiativeAaoComponent } from '../shared/toc-initiative-aao/toc-initiative-aao.component';
+import { PrFieldHeaderComponent } from '../../../../../../../../custom-fields/pr-field-header/pr-field-header.component';
+import { AlertStatusComponent } from '../../../../../../../../custom-fields/alert-status/alert-status.component';
 
 describe('TocActionAreaOutcomeSectionComponent', () => {
   let component: TocActionAreaOutcomeSectionComponent;
@@ -8,15 +12,24 @@ describe('TocActionAreaOutcomeSectionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TocActionAreaOutcomeSectionComponent]
+      declarations: [
+        TocActionAreaOutcomeSectionComponent,
+        TocInitiativeAaoComponent,
+        PrFieldHeaderComponent,
+        NoDataTextComponent,
+        AlertStatusComponent
+      ],
+      imports: [
+        HttpClientTestingModule
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(TocActionAreaOutcomeSectionComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should initialize resultActionArea array', () => {
+    expect(component.theoryOfChangesServices.resultActionArea).toEqual([]);
   });
 });
