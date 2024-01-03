9ebfc33056a4070041351cf9aa5eec21aee7698d
stephanyceron <stephany1803@gmail.com>
2024-01-03 02:10:20 -0500

    test(RdTheoryOfChangesServicesService): add unit tests

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-theory-of-change/rd-theory-of-changes-services.service.spec.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-theory-of-change/rd-theory-of-changes-services.service.spec.ts
@@ -1,16 +1,46 @@
 import { TestBed } from '@angular/core/testing';
-
 import { RdTheoryOfChangesServicesService } from './rd-theory-of-changes-services.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of, throwError } from 'rxjs';
 
 describe('RdTheoryOfChangesServicesService', () => {
   let service: RdTheoryOfChangesServicesService;
+  let mockApiService: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(RdTheoryOfChangesServicesService);
+    mockApiService = {
+      resultsSE: {
+        get_vesrsionDashboard: () => of({ response: {version_id: 1}}),
+      }
+    };
+
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+    });
+    service = new RdTheoryOfChangesServicesService(mockApiService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('get_versionDashboard()', () => {
+    it('should set fullInitiativeToc when get_vesrsionDashboard call is successful', () => {
+      const spy = jest.spyOn(mockApiService.resultsSE, 'get_vesrsionDashboard');
+
+      service.get_versionDashboard(1);
+
+      expect(spy).toHaveBeenCalled()
+      expect(service.fullInitiativeToc).toEqual(1);
+    });
+
+    it('should log an error when get_vesrsionDashboard call fails', () => {
+      const mockError = 'error';
+      jest.spyOn(mockApiService.resultsSE, 'get_vesrsionDashboard')
+        .mockReturnValue(throwError(mockError));
+      const spy = jest.spyOn(console, 'error');
+
+      service.get_versionDashboard(1);
+
+      expect(spy).toHaveBeenCalledWith(mockError);
+    });
   });
 });
