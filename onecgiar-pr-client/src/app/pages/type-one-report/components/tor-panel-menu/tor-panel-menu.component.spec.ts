934a9db5b3f73f74a2fbac9274c26d392a700e98
stephanyceron <stephany1803@gmail.com>
2024-01-09 02:30:14 -0500

    test(TorPanelMenuComponent): add unit tests

--- onecgiar-pr-client/src/app/pages/type-one-report/components/tor-panel-menu/tor-panel-menu.component.spec.ts
+++ onecgiar-pr-client/src/app/pages/type-one-report/components/tor-panel-menu/tor-panel-menu.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TorPanelMenuComponent } from './tor-panel-menu.component';
+import { PrButtonComponent } from '../../../../custom-fields/pr-button/pr-button.component';
+import { TooltipModule } from 'primeng/tooltip';
 
 describe('TorPanelMenuComponent', () => {
   let component: TorPanelMenuComponent;
@@ -8,7 +9,13 @@ describe('TorPanelMenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TorPanelMenuComponent ]
+      declarations: [ 
+        TorPanelMenuComponent,
+        PrButtonComponent
+      ],
+      imports: [
+        TooltipModule
+      ]
     })
     .compileComponents();
 
