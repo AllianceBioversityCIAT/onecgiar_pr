70db0e14a719d18c88493e219536203c25406486
Juan Delgado <juandelgadog98@gmail.com>
2024-01-16 10:15:05 -0500

    :wrench: Chore(Clean): Removed unnecessary logs print.

--- onecgiar-pr-server/src/api/results/results-toc-results/results-toc-results.service.ts
+++ onecgiar-pr-server/src/api/results/results-toc-results/results-toc-results.service.ts
@@ -591,12 +591,6 @@ export class ResultsTocResultsService {
         toc_result_id,
       );
 
-      for (const i of informationIndicator) {
-        for (const t of i.targets) {
-          console.log("🚀 ~ ResultsTocResultsService ~ t:", t)
-        }
-      }
-
       return {
         response: {
           initiative: init,
