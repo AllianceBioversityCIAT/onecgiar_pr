5f7d90605ed3c801693627d7b2cd5a1d9b5b5d18
stephanyceron <stephany1803@gmail.com>
2024-01-15 21:31:55 -0500

    test(InitAdminSectionComponent): add unit tests

--- onecgiar-pr-client/src/app/pages/init-admin-section/init-admin-section.component.spec.ts
+++ onecgiar-pr-client/src/app/pages/init-admin-section/init-admin-section.component.spec.ts
@@ -3,15 +3,35 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TooltipModule } from 'primeng/tooltip';
 import { InitAdminSectionComponent } from './init-admin-section.component';
 import { DynamicPanelMenuComponent } from '../../shared/components/dynamic-panel-menu/dynamic-panel-menu.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DataControlService } from '../../shared/services/data-control.service';
 
 describe('InitAdminSectionComponent', () => {
   let component: InitAdminSectionComponent;
   let fixture: ComponentFixture<InitAdminSectionComponent>;
+  let mockDataControlService;
 
   beforeEach(async () => {
+    mockDataControlService = {
+      detailSectionTitle: jest.fn()
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [InitAdminSectionComponent, DynamicPanelMenuComponent],
-      imports: [HttpClientTestingModule, TooltipModule]
+      declarations: [
+        InitAdminSectionComponent, 
+        DynamicPanelMenuComponent
+      ],
+      imports: [
+        HttpClientTestingModule, 
+        TooltipModule,
+        RouterTestingModule
+      ],
+      providers: [
+        {
+          provide: DataControlService,
+          useValue: mockDataControlService
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(InitAdminSectionComponent);
@@ -19,7 +39,13 @@ describe('InitAdminSectionComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('ngOnInit()', () => {
+    it('should set the detailSectionTitle on ngOnInit', () => {
+      const spyDetailSectionTitle = jest.spyOn(mockDataControlService, 'detailSectionTitle');
+  
+      component.ngOnInit();
+  
+      expect(spyDetailSectionTitle).toHaveBeenCalledWith('INIT Admin Module');
+    });
   });
 });
