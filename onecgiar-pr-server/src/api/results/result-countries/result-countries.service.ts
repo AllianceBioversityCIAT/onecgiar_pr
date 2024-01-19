667f88a8820a0e43c09fb21831afeb0e66a897a1
davinzifc <47566127+davinzifc@users.noreply.github.com>
2024-01-19 16:11:15 -0500

    :recycle: refactor(Sonar): second batch of issues resolved

--- onecgiar-pr-server/src/api/results/result-countries/result-countries.service.ts
+++ onecgiar-pr-server/src/api/results/result-countries/result-countries.service.ts
@@ -28,7 +28,6 @@ export class ResultCountriesService {
           status: HttpStatus.BAD_REQUEST,
         };
       }
-      //!importante hay una tabla por cada uno pero fijo se mandara a un solo enpoint y que el haga el restos
       const result: Result = await this._resultRepository.getResultById(
         createResultCountryDto.result_id,
       );
@@ -41,76 +40,13 @@ export class ResultCountriesService {
       }
 
       const countries = createResultCountryDto.countries;
-      if (
-        (!createResultCountryDto.has_countries &&
-          createResultCountryDto.geo_scope_id != 3) ||
-        createResultCountryDto.geo_scope_id == 4
-      ) {
-        await this._resultCountryRepository.updateCountries(result.id, []);
-        result.has_countries = false;
-      } else if (
-        createResultCountryDto.geo_scope_id == 3 ||
-        createResultCountryDto.has_countries
-      ) {
-        if (countries) {
-          await this._resultCountryRepository.updateCountries(
-            result.id,
-            createResultCountryDto.countries.map((e) => e.id),
-          );
-          if (countries?.length) {
-            let resultCountryArray: ResultCountry[] = [];
-            const existingCountries: ResultCountry[] = [];
-            for (let index = 0; index < countries?.length; index++) {
-              const exist =
-                await this._resultCountryRepository.getResultCountrieByIdResultAndCountryId(
-                  result.id,
-                  countries[index].id,
-                );
-              if (!exist) {
-                const newCountry = new ResultCountry();
-                newCountry.country_id = countries[index].id;
-                newCountry.result_id = result.id;
-                resultCountryArray.push(newCountry);
-              } else {
-                existingCountries.push(exist);
-              }
-            }
-
-            resultCountryArray =
-              await this._resultCountryRepository.save(resultCountryArray);
-            resultCountryArray = resultCountryArray.concat(existingCountries);
-
-            await Promise.all(
-              resultCountryArray.map(async (rc) => {
-                const subnationalStringCodes =
-                  createResultCountryDto.geo_scope_id == 5
-                    ? (
-                        countries.find((c) => c.id == rc.country_id)
-                          ?.sub_national ?? []
-                      ).map((sn) => sn.code)
-                    : [];
-
-                await this._resultCountrySubnationalRepository.bulkUpdateSubnational(
-                  rc.result_country_id,
-                  subnationalStringCodes,
-                  user.id,
-                );
-
-                await this._resultCountrySubnationalRepository.upsertSubnational(
-                  rc.result_country_id,
-                  subnationalStringCodes,
-                  user.id,
-                );
-              }),
-            );
-          }
-        }
-        if (createResultCountryDto.geo_scope_id == 3) {
-          result.has_countries = true;
-        } else {
-          result.has_countries = createResultCountryDto.has_countries;
-        }
-      }
+      await this.handleCountries(
+        result,
+        countries,
+        createResultCountryDto.geo_scope_id,
+        createResultCountryDto.has_countries,
+        user,
+      );
 
       if (countries && createResultCountryDto.geo_scope_id == 3) {
         result.geographic_scope_id =
@@ -133,5 +69,89 @@ export class ResultCountriesService {
     } catch (error) {
       return this._handlersError.returnErrorRes({ error, debug: true });
     }
+  }
+
+  async handleCountries(
+    result,
+    countries,
+    geo_scope_id,
+    has_countries,
+    user: TokenDto,
+  ) {
+    if ((!has_countries && geo_scope_id != 3) || geo_scope_id == 4) {
+      await this._resultCountryRepository.updateCountries(result.id, []);
+      result.has_countries = false;
+    } else if (geo_scope_id == 3 || has_countries) {
+      if (countries) {
+        await this._resultCountryRepository.updateCountries(
+          result.id,
+          countries.map((e) => e.id),
+        );
+        if (countries?.length) {
+          let resultCountryArray = await this.handleResultCountryArray(
+            result,
+            countries,
+          );
+          await this.handleSubnationals(
+            resultCountryArray,
+            countries,
+            geo_scope_id,
+            user.id,
+          );
+        }
+      }
+      result.has_countries = geo_scope_id == 3 ? true : has_countries;
+    }
+  }
+
+  async handleResultCountryArray(result, countries) {
+    let resultCountryArray: ResultCountry[] = [];
+    const existingCountries: ResultCountry[] = [];
+    for (let index = 0; index < countries?.length; index++) {
+      const exist =
+        await this._resultCountryRepository.getResultCountrieByIdResultAndCountryId(
+          result.id,
+          countries[index].id,
+        );
+      if (!exist) {
+        const newCountry = new ResultCountry();
+        newCountry.country_id = countries[index].id;
+        newCountry.result_id = result.id;
+        resultCountryArray.push(newCountry);
+      } else {
+        existingCountries.push(exist);
+      }
+    }
+    resultCountryArray =
+      await this._resultCountryRepository.save(resultCountryArray);
+    return resultCountryArray.concat(existingCountries);
+  }
+
+  async handleSubnationals(
+    resultCountryArray,
+    countries,
+    geo_scope_id,
+    userId,
+  ) {
+    await Promise.all(
+      resultCountryArray.map(async (rc) => {
+        const subnationalStringCodes =
+          geo_scope_id == 5
+            ? (
+                countries.find((c) => c.id == rc.country_id)?.sub_national ?? []
+              ).map((sn) => sn.code)
+            : [];
+        await this._resultCountrySubnationalRepository.bulkUpdateSubnational(
+          rc.result_country_id,
+          subnationalStringCodes,
+          userId,
+        );
+        await this._resultCountrySubnationalRepository.upsertSubnational(
+          rc.result_country_id,
+          subnationalStringCodes,
+          userId,
+        );
+      }),
+    );
   }
 }
