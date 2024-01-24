20469e3ec5163d114dda7c75ab8f978c19cc3652
stephanyceron <stephany1803@gmail.com>
2024-01-24 02:04:39 -0500

    test(IpsrNonPooledProjectsComponent): add unit tests

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-contributors/components/ipsr-non-pooled-projects/ipsr-non-pooled-projects.component.spec.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-contributors/components/ipsr-non-pooled-projects/ipsr-non-pooled-projects.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { IpsrNonPooledProjectsComponent } from './ipsr-non-pooled-projects.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoDataTextComponent } from '../../../../../../../../custom-fields/no-data-text/no-data-text.component';
+import { PrFieldHeaderComponent } from '../../../../../../../../custom-fields/pr-field-header/pr-field-header.component';
 
 describe('IpsrNonPooledProjectsComponent', () => {
   let component: IpsrNonPooledProjectsComponent;
@@ -8,7 +10,14 @@ describe('IpsrNonPooledProjectsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ IpsrNonPooledProjectsComponent ]
+      declarations: [ 
+        IpsrNonPooledProjectsComponent,
+        NoDataTextComponent,
+        PrFieldHeaderComponent
+      ],
+      imports: [
+        HttpClientTestingModule
+      ],
     })
     .compileComponents();
 
@@ -17,7 +26,34 @@ describe('IpsrNonPooledProjectsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('deleteEvidence', () => {
+    it('should delete evidence from contributing_np_projects array', () => {
+      component.contributorsBody.contributing_np_projects = [{
+        funder: 1, 
+        grant_title: '', 
+        center_grant_id: '', 
+        lead_center: ''
+      }];
+  
+      component.deleteEvidence(0);
+  
+      expect(component.contributorsBody.contributing_np_projects).toEqual([]);
+    });
+  });
+
+  describe('addBilateralContribution', () => {
+    it('should add a new donorInterfaceToc to contributing_np_projects array', () => {
+      const initialLength = component.contributorsBody.contributing_np_projects.length;
+  
+      component.addBilateralContribution();
+  
+      expect(component.contributorsBody.contributing_np_projects.length).toBe(initialLength + 1);
+      expect(component.contributorsBody.contributing_np_projects[initialLength]).toEqual({
+        funder: null, 
+        grant_title: null, 
+        center_grant_id: null, 
+        lead_center: null
+      });
+    });
   });
 });
