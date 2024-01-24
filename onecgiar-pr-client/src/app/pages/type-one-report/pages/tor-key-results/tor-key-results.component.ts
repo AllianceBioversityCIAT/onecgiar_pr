209b62a611f68117f0bd3e1c887a97329f1bf848
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-01-24 14:09:45 -0500

    :recycle: refactor(T1R) LFUB-1444: Some texts were updated

--- onecgiar-pr-client/src/app/pages/type-one-report/pages/tor-key-results/tor-key-results.component.ts
+++ onecgiar-pr-client/src/app/pages/type-one-report/pages/tor-key-results/tor-key-results.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
 })
 export class TorKeyResultsComponent {
   keyResultsDesc = `This section provides an overview of results reported by the CGIAR Initiative on [Initiative short name]. These results align with the CGIAR Results Framework and [Initiative short name’s] theory of change. Further information on these results is available through the CGIAR Results Dashboard.
+
   The following diagrams have been produced using quality assessed results entered into the CGIAR reporting system and are based on data extracted on [DATE].`;
 }
