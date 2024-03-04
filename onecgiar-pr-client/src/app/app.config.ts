1d7cd39b1621689573bc23efa636f3406919ddc1
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-03-04 14:43:15 -0500

    :recycle: refactor(angular-17-migration): Import interceptor

--- onecgiar-pr-client/src/app/app.config.ts
+++ onecgiar-pr-client/src/app/app.config.ts
@@ -1,9 +1,18 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './routes/app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GeneralInterceptorService } from './shared/interceptors/general-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withViewTransitions()), importProvidersFrom(HttpClientModule, BrowserAnimationsModule)]
+  providers: [
+    provideRouter(routes, withViewTransitions()),
+    importProvidersFrom(HttpClientModule, BrowserAnimationsModule),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: GeneralInterceptorService,
+      multi: true
+    }
+  ]
 };
