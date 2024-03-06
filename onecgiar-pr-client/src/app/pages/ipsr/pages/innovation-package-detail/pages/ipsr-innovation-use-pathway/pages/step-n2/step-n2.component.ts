945a497442c94dee8f600a341c60139aa5ed07cc
xKeCo <kevcollazos@gmail.com>
2024-03-06 16:35:31 -0500

    :recycle: refactor(angular-17-migration): update ipsr step 2 imports

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n2/step-n2.component.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n2/step-n2.component.ts
@@ -2,13 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { IpsrGreenCheckComponent } from '../../../../../../components/ipsr-green-check/ipsr-green-check.component';
 import { ApiService } from '../../../../../../../../shared/services/api/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-step-n2',
   standalone: true,
   templateUrl: './step-n2.component.html',
   styleUrls: ['./step-n2.component.scss'],
-  imports: [RouterModule, RouterLink, RouterLinkActive, IpsrGreenCheckComponent]
+  imports: [
+    CommonModule,
+    RouterModule,
+    RouterLink,
+    RouterLinkActive,
+    IpsrGreenCheckComponent
+  ]
 })
 export class StepN2Component implements OnInit {
   constructor(public api: ApiService) {}
