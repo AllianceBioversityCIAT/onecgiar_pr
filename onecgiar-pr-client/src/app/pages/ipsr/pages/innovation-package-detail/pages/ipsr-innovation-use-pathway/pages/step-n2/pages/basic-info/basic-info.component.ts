5715cff34e81cbe14b8eae3b104dc75eeb8614b8
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-03-06 11:58:15 -0500

    :recycle: refactor(angular-17-migration): removed unused basic-info component

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n2/pages/basic-info/basic-info.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../../../../../../../../../../shared/services/api/api.service';
-
-@Component({
-  selector: 'app-basic-info',
-  templateUrl: './basic-info.component.html',
-  styleUrls: ['./basic-info.component.scss']
-})
-export class BasicInfoComponent {}
