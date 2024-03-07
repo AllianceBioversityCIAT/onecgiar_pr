4205a12d184599dbe4250cabc22c606d33cb9a5a
xKeCo <kevcollazos@gmail.com>
2024-03-07 08:58:01 -0500

    :recycle: refactor(angular-17-migration): update navigation bar imports

--- onecgiar-pr-client/src/app/shared/components/navigation-bar/navigation-bar.component.ts
+++ onecgiar-pr-client/src/app/shared/components/navigation-bar/navigation-bar.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationBarService } from '../../services/navigation-bar.service';
 import { RolesService } from '../../services/global/roles.service';
-import { environment } from 'src/environments/environment';
 import { DataControlService } from '../../services/data-control.service';
 import { AuthService } from '../../services/api/auth.service';
 import { ApiService } from '../../services/api/api.service';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { PrRoute } from '../../routing/routing-data';
-import { routingApp } from 'src/app/shared/routing/routing-data';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { PrRoute, routingApp } from '../../routing/routing-data';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.scss'],
-  imports: [CommonModule, RouterLink]
+  imports: [CommonModule, RouterLink, RouterLinkActive]
 })
 export class NavigationBarComponent implements OnInit {
   navigationOptions: PrRoute[] = routingApp;
@@ -65,11 +64,8 @@ export class NavigationBarComponent implements OnInit {
 
   validateAdminModuleAndRole(option) {
     if (option.onlytest && environment.production) return true;
-    // if (option?.path == 'ipsr' && !this.hasAccess()) return true;
     if (this.rolesSE.isAdmin) return false;
     if (option?.path == 'init-admin-module') return this.validateCoordAndLead();
-    // if (option?.path == 'quality-assurance' && !this.rolesSE.isAdmin) return true;
-    // if (option?.path == 'type-one-report' && !this.rolesSE.isAdmin) return true;
 
     return false;
   }
