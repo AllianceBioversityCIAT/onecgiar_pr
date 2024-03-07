14b8d8df89733022a785f1e4dc723d4a3918f7fd
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-03-07 10:36:41 -0500

    :recycle: refactor(angular-17-migration): removed step-n1-innovaton-use

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-list/components/innovation-package-custom-table/pipes/innovation-package-list-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InnovationPackageListFilterPipe } from './innovation-package-list-filter.pipe';
-
-describe('InnovationPackageListFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new InnovationPackageListFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
