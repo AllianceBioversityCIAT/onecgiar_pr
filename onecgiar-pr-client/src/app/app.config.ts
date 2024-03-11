a29c25b0deb550811ad61255aba01d336f280ae7
yecksin <yecksin@gmail.com>
2024-03-11 14:19:00 -0500

    :recycle: refactor(angular-17-migration): import NgCircleProgressModule.forRoot() in app.config.ts

--- onecgiar-pr-client/src/app/app.config.ts
+++ onecgiar-pr-client/src/app/app.config.ts
@@ -4,11 +4,16 @@ import { routes } from './routes/app.routes';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GeneralInterceptorService } from './shared/interceptors/general-interceptor.service';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withViewTransitions()),
-    importProvidersFrom(HttpClientModule, BrowserAnimationsModule),
+    importProvidersFrom(
+      HttpClientModule,
+      BrowserAnimationsModule,
+      NgCircleProgressModule.forRoot()
+    ),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: GeneralInterceptorService,
