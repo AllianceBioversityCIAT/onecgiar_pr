c506cade7f351b76fa139c1bf50a354a9480c1b0
German Martinez <g.martinez@cgiar.org>
2024-03-19 10:22:31 -0500

    🐛fix(PDF): fix issue autodownload instead of previsualization

--- onecgiar-pr-server/src/api/platform-report/platform-report.controller.ts
+++ onecgiar-pr-server/src/api/platform-report/platform-report.controller.ts
@@ -22,41 +22,13 @@ export class PlatformReportController {
     @Query() query: { phase: string; downloadable: boolean },
     @Res({ passthrough: true }) res: Response,
   ): Promise<StreamableFile | returnErrorDto> {
-    return this.getReportByResultCode(
-      code,
-      query.phase,
-      res,
-      PlatformReportEnum.FULL_RESULT_REPORT,
-    );
-  }
-
-  @Get('/ipsr/:code')
-  async getFullIPSRReportByResultCode(
-    @Param('code') code: string,
-    @Query() query: { phase: string; downloadable: boolean },
-    @Res({ passthrough: true }) res: Response,
-  ): Promise<StreamableFile | returnErrorDto> {
-    return this.getReportByResultCode(
-      code,
-      query.phase,
-      res,
-      PlatformReportEnum.FULL_IPSR_REPORT,
-    );
-  }
-
-  private async getReportByResultCode(
-    code: string,
-    phase: string,
-    res: Response,
-    reportType: PlatformReportEnum,
-  ): Promise<StreamableFile | returnErrorDto> {
     code = code?.trim();
-    phase = phase?.trim();
+    query.phase = query.phase?.trim();
     let result =
       await this._platformReportService.getFullResultReportByResultCode(
         code,
-        phase,
-        reportType,
+        query.phase,
+        PlatformReportEnum.FULL_RESULT_REPORT,
       );
 
     if (result?.['message']) {
@@ -64,7 +36,41 @@ export class PlatformReportController {
     }
     result = <{ pdf: ReadStream; filename_date: any }>result;
 
-    const contentDisposition = `attachment;filename="PRMS-Result-${code}_${result.filename_date}.pdf"`;
+    const contentDisposition = `${
+      query.downloadable ?? false ? 'attachment;' : ''
+    }filename="PRMS-Result-${code}_${result.filename_date}.pdf"`;
+
+    res.set({
+      'Content-Disposition': contentDisposition,
+      'Access-Control-Expose-Headers': 'Content-Disposition',
+    });
+
+    return new StreamableFile(result.pdf, { type: 'application/pdf' });
+  }
+
+  @Get('/ipsr/:code')
+  async getFullIPSRReportByCode(
+    @Param('code') code: string,
+    @Query() query: { phase: string; downloadable: boolean },
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<StreamableFile | returnErrorDto> {
+    code = code?.trim();
+    query.phase = query.phase?.trim();
+    let result =
+      await this._platformReportService.getFullResultReportByResultCode(
+        code,
+        query.phase,
+        PlatformReportEnum.FULL_IPSR_REPORT,
+      );
+
+    if (result?.['message']) {
+      return <returnErrorDto>result;
+    }
+    result = <{ pdf: ReadStream; filename_date: any }>result;
+
+    const contentDisposition = `${
+      query.downloadable ?? false ? 'attachment;' : ''
+    }filename="PRMS-Result-${code}_${result.filename_date}.pdf"`;
 
     res.set({
       'Content-Disposition': contentDisposition,
