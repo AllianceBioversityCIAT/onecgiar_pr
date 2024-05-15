012f5509f9a032ce693070f4328941ff9808fad2
xKeCo <kevcollazos@gmail.com>
2024-05-15 12:44:29 -0500

    :white_check_mark: test(Unit test): Improving branch and functions coverage

--- onecgiar-pr-client/src/app/shared/components/footer/footer.component.spec.ts
+++ onecgiar-pr-client/src/app/shared/components/footer/footer.component.spec.ts
@@ -1,7 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { FooterComponent } from './footer.component';
-import { HttpClientModule } from '@angular/common/http';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -10,15 +11,43 @@ describe('FooterComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [FooterComponent],
-      imports: [HttpClientModule]
+      imports: [HttpClientModule, RouterTestingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set isFloating to true when route is in the list', () => {
+    component.routes = [{ path: '/', floating: true }];
+    component.showIfRouteIsInList();
+    expect(component.isFloating).toBe(true);
+  });
+
+  it('should set isFloatingFix to true when route is in the list', () => {
+    component.routes = [{ path: '/', floatingFix: true }];
+    component.showIfRouteIsInList();
+    expect(component.isFloatingFix).toBe(true);
+  });
+
+  it('should set isFloatingFix and isFloating to false when route is not in the list', () => {
+    component.routes = [{ path: '/login', floatingFix: true }];
+    component.showIfRouteIsInList();
+    expect(component.isFloatingFix).toBe(false);
+    expect(component.isFloating).toBe(false);
+  });
+
+  it('should set isHover to true on mouse enter', () => {
+    component.onMouseEnter();
+    expect(component.isHover).toBe(true);
+  });
+
+  it('should set isHover to false on mouse leave', () => {
+    component.onMouseLeave();
+    expect(component.isHover).toBe(false);
   });
 });
