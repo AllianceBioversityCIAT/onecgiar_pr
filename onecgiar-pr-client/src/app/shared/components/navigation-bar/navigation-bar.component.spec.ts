3540be4510bac0aecfdea229c96af9da5865b8a3
yecksin <yecksin@gmail.com>
2024-05-15 11:37:37 -0500

    :sparkles: feat(Jest): adding branch coverage in tests

--- onecgiar-pr-client/src/app/shared/components/navigation-bar/navigation-bar.component.spec.ts
+++ onecgiar-pr-client/src/app/shared/components/navigation-bar/navigation-bar.component.spec.ts
@@ -1,24 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavigationBarComponent } from './navigation-bar.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('NavigationBarComponent', () => {
   let component: NavigationBarComponent;
   let fixture: ComponentFixture<NavigationBarComponent>;
 
+  // Mock service
+  const mockNavigationBarService = { navbar_fixed: false };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [NavigationBarComponent],
-      imports: [HttpClientModule]
+      imports: [HttpClientTestingModule],
+      declarations: [NavigationBarComponent]
     }).compileComponents();
-
     fixture = TestBed.createComponent(NavigationBarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.ngOnInit();
   });
 
-  it('should create', () => {
+  it('should create the app', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set navbar_fixed to true when scroll is over 70', () => {
+    // Simulate scroll event
+    Object.defineProperty(window, 'pageYOffset', { value: 200 });
+    window.dispatchEvent(new Event('scroll'));
+
+    expect(mockNavigationBarService.navbar_fixed).toBe(false);
+  });
+
+  it('should set navbar_fixed to false when scroll is under 70', () => {
+    // Simulate scroll event
+    Object.defineProperty(window, 'pageYOffset', { value: 50 });
+    window.dispatchEvent(new Event('scroll'));
+
+    expect(mockNavigationBarService.navbar_fixed).toBe(false);
   });
 });
