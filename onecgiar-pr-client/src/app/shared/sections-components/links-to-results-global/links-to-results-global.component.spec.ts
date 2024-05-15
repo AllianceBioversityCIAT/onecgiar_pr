63ff56a5fe329d6386a8135d354eaa9ed9de3a07
yecksin <yecksin@gmail.com>
2024-05-15 11:07:11 -0500

    :sparkles: feat(Jest): adding branch coverage in tests

--- onecgiar-pr-client/src/app/shared/sections-components/links-to-results-global/links-to-results-global.component.spec.ts
+++ onecgiar-pr-client/src/app/shared/sections-components/links-to-results-global/links-to-results-global.component.spec.ts
@@ -31,4 +31,58 @@ describe('LinksToResultsGlobalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  describe('validateOrder', () => {
+    let mockElement;
+
+    beforeEach(() => {
+      mockElement = document.createElement('table');
+      mockElement.id = 'resultListTable';
+      document.body.appendChild(mockElement);
+    });
+
+    afterEach(() => {
+      document.body.removeChild(mockElement);
+    });
+
+    it('should set combine to true when columnAttr is "result_code"', done => {
+      component.validateOrder('result_code');
+
+      setTimeout(() => {
+        expect(component.combine).toBe(true);
+        done();
+      }, 100);
+    });
+
+    it('should set combine to true when there are no sorted columns', done => {
+      component.validateOrder('other_attr');
+
+      setTimeout(() => {
+        expect(component.combine).toBe(true);
+        done();
+      }, 100);
+    });
+  });
+  describe('contributeDescription', () => {
+    it('should return the correct HTML string', () => {
+      const expectedHTML = `<ul>
+      <li>To search for results that have already been reported, enter keywords into the title box below and click on the link button of the result found if it contributes to this result you are reporting.</li>
+      <li>Users will be able to select other results from previous phase</li>
+    </ul>`;
+
+      const result = component.contributeDescription();
+
+      expect(result).toEqual(expectedHTML);
+    });
+  });
+  describe('getFirstByDate', () => {
+    it('should return the result with the most recent date', () => {
+      const results = [{ created_date: '2022-01-01' }, { created_date: '2022-01-02' }, { created_date: '2022-01-03' }];
+
+      const expected = { created_date: '2022-01-03' };
+
+      const result = component.getFirstByDate(results);
+
+      expect(result).toEqual(expected);
+    });
+  });
 });
