37efe14bad3ef5925ac67e319497791670b5f31f
Kevin Collazos <kevcollazos@gmail.com>
2024-05-29 16:35:20 -0500

    :recycle: refactor(IPSR Excel Report) P2-691: Update IPSR table total results and refactor search filter validation

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-list-content/pages/innovation-package-list/innovation-package-list.component.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-list-content/pages/innovation-package-list/innovation-package-list.component.ts
@@ -11,6 +11,8 @@ import { IpsrDataControlService } from '../../../../services/ipsr-data-control.s
   styleUrls: ['./innovation-package-list.component.scss']
 })
 export class InnovationPackageListComponent implements OnInit, OnDestroy {
+  totalResults: number = 0;
+
   constructor(
     public api: ApiService,
     public phaseServices: PhasesService,
@@ -34,6 +36,7 @@ export class InnovationPackageListComponent implements OnInit, OnDestroy {
   GETAllInnovationPackages() {
     this.api.resultsSE.GETAllInnovationPackages().subscribe(({ response }) => {
       this.ipsrDataControlSE.ipsrResultList = response;
+      this.totalResults = response.filter((thing, index, self) => self.findIndex(t => t.result_code === thing.result_code) === index).length;
 
       this.ipsrDataControlSE.ipsrResultList.map((inno: any) => {
         inno.full_name = `${inno?.result_code} ${inno?.title} ${inno?.official_code}`;
