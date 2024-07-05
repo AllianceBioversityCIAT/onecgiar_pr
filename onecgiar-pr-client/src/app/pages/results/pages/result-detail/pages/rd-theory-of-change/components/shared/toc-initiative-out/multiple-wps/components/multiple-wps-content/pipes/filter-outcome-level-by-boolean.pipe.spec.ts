87d0feca13d5c4f5e5b3f8e37a74b28ca901ebbf
yecksin <yecksin@gmail.com>
2024-07-05 12:14:08 -0500

    :recycle: refactor P2-847: test updated

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-theory-of-change/components/shared/toc-initiative-out/multiple-wps/components/multiple-wps-content/pipes/filter-outcome-level-by-boolean.pipe.spec.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-theory-of-change/components/shared/toc-initiative-out/multiple-wps/components/multiple-wps-content/pipes/filter-outcome-level-by-boolean.pipe.spec.ts
@@ -4,6 +4,28 @@ import { DataControlService } from '../../../../../../../../../../../../../share
 
 describe('FilterOutcomeLevelByBooleanPipe', () => {
   let pipe: FilterOutcomeLevelByBooleanPipe;
+  const list = [
+    {
+      toc_level_id: 1,
+      name: 'Work package Output',
+      description: ''
+    },
+    {
+      toc_level_id: 2,
+      name: 'Work package Outcome',
+      description: ''
+    },
+    {
+      toc_level_id: 3,
+      name: 'End of Initiative Outcome',
+      description: ''
+    },
+    {
+      toc_level_id: 4,
+      name: 'Action Area Outcome',
+      description: ''
+    }
+  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -31,5 +53,20 @@ describe('FilterOutcomeLevelByBooleanPipe', () => {
 
   it('should return empty array if list is empty', () => {
     expect(pipe.transform([], true)).toEqual([]);
+  });
+
+  it('should return list with toc_level_id 3 if yes is true and appliesforTOCMWfilter is false', () => {
+    expect(pipe.transform(list, false)).toEqual([
+      {
+        toc_level_id: 3,
+        name: 'End of Initiative Outcome',
+        description: ''
+      },
+      {
+        toc_level_id: 4,
+        name: 'Action Area Outcome',
+        description: ''
+      }
+    ]);
   });
 });
