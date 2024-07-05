3e94a1f7ff391657181779161aa7f3d7b918b41e
yecksin <yecksin@gmail.com>
2024-07-05 08:22:34 -0500

    :recycle: refactor P2-847: Tests added

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-theory-of-change/components/shared/toc-initiative-out/multiple-wps/components/multiple-wps-content/pipes/filter-outcome-level-by-boolean.pipe.spec.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-theory-of-change/components/shared/toc-initiative-out/multiple-wps/components/multiple-wps-content/pipes/filter-outcome-level-by-boolean.pipe.spec.ts
@@ -1,8 +1,35 @@
+import { TestBed } from '@angular/core/testing';
 import { FilterOutcomeLevelByBooleanPipe } from './filter-outcome-level-by-boolean.pipe';
+import { DataControlService } from '../../../../../../../../../../../../../shared/services/data-control.service';
 
 describe('FilterOutcomeLevelByBooleanPipe', () => {
+  let pipe: FilterOutcomeLevelByBooleanPipe;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: DataControlService,
+          useValue: {
+            currentResult: {
+              appliesforTOCMWfilter: true
+            },
+            rolesSE: {
+              isAdmin: false
+            }
+          }
+        },
+        FilterOutcomeLevelByBooleanPipe
+      ]
+    });
+    pipe = TestBed.inject(FilterOutcomeLevelByBooleanPipe);
+  });
+
   it('create an instance', () => {
-    const pipe = new FilterOutcomeLevelByBooleanPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return empty array if list is empty', () => {
+    expect(pipe.transform([], true)).toEqual([]);
   });
 });
