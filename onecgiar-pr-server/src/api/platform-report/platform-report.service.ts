f08587ecbe2c9664aaa8316fac86ada3ac18fb63
Kevin Collazos <kevcollazos@gmail.com>
2024-07-12 14:42:06 -0500

    :recycle: refactor(Results PDF) P2-781: update PDF footer styles

--- onecgiar-pr-server/src/api/platform-report/platform-report.service.ts
+++ onecgiar-pr-server/src/api/platform-report/platform-report.service.ts
@@ -222,12 +222,18 @@ export class PlatformReportService {
                 width: 100%;
                 font-size: .7rem !important;
               }
+              .page_counter {
+                min-width: 150px;
+                text-align: right;
+                font-size: .8rem;
+              }
             </style>
             <table class="footer">
               <tr>
                 <td class="text">
-                  This report was generated on ${data.generation_date_footer}. Please note that the contents of this report may change in the future as it is dependent on the data entered into the PRMS Reporting tool at a given time during a specific phase
+                  This report was automatically generated on ${data.generation_date_footer}. Please note that the contents of this report may change in the future as it is dependent on the data entered into the PRMS Reporting tool at a given time during a specific phase
                 </td>
+                <td class="page_counter">{{page}}/{{pages}}</td>
               </tr>
             </table>
               `,
