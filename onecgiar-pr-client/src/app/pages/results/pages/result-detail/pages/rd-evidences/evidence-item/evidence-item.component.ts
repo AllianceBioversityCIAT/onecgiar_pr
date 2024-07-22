59365afabd7bced947318de2e62b7578aa441d0a
German Martinez <g.martinez@cgiar.org>
2024-07-22 11:03:59 -0500

    ✨feat(Evidences): adding message when a cgspace link is being saved

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-evidences/evidence-item/evidence-item.component.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-evidences/evidence-item/evidence-item.component.ts
@@ -34,6 +34,11 @@ export class EvidenceItemComponent {
 
   constructor(public dataControlSE: DataControlService, public api: ApiService) {}
 
+  validateCGLink() {
+    const regex = /^https:\/\/(?:cgspace\.cgiar\.org\/items\/[0-9a-f-]{36}|cgspace\.cgiar\.org\/handle\/10568\/\d+)$/;
+    return this.evidence.link && regex.test(this.evidence.link?.trim());
+  }
+
   validateFileTypes(file: File) {
     const validFileTypes = ['.jpg', '.png', '.pdf', '.doc', '.docx', '.pptx', '.jpeg', '.xlsx'];
     const extension = '.' + file.name.split('.').pop();
@@ -42,15 +47,20 @@ export class EvidenceItemComponent {
   }
 
   isInvalidLink(value: string = '') {
-    const regex = new RegExp(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/\S*)?$/i);
+    const regex = new RegExp(
+      /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/\S*)?$/i
+    );
 
     return regex.test(value.trim());
   }
 
   deleteItem() {
-    this.api.alertsFe.show({ id: 'confirm-delete-evidence', title: `Are you sure you want to delete this evidence?`, status: 'warning', confirmText: 'Yes, delete' }, () => {
-      this.deleteEvent.emit();
-    });
+    this.api.alertsFe.show(
+      { id: 'confirm-delete-evidence', title: `Are you sure you want to delete this evidence?`, status: 'warning', confirmText: 'Yes, delete' },
+      () => {
+        this.deleteEvent.emit();
+      }
+    );
   }
 
   onFileSelected(event: any) {
