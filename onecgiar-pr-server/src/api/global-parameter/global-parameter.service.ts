1b875ca073247bf03488642c90a30bab487a2c07
German Martinez <g.martinez@cgiar.org>
2024-08-09 14:19:18 -0500

    ♻️refactor(Global Parameter): refactoring code to remove sonar alert

--- onecgiar-pr-server/src/api/global-parameter/global-parameter.service.ts
+++ onecgiar-pr-server/src/api/global-parameter/global-parameter.service.ts
@@ -106,14 +106,14 @@ export class GlobalParameterService {
     user: TokenDto,
   ) {
     const isAdmin = await this._roleByUseRepository.isUserAdmin(user.id);
-    if (isAdmin?.is_admin == false) {
+    if (!isAdmin?.is_admin) {
       return this._returnResponse.format({
         message: 'You do not have permission to perform this action',
         statusCode: HttpStatus.FORBIDDEN,
       });
     }
 
-    if (!updateGlobalParameterDto || !updateGlobalParameterDto.name) {
+    if (!updateGlobalParameterDto?.name) {
       return this._returnResponse.format({
         message: 'Invalid data',
         statusCode: HttpStatus.BAD_REQUEST,
