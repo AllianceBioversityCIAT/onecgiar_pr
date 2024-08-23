0a1cbb2d9d622cb0d93668e436f25f7dd4024255
Kevin Collazos <kevcollazos@gmail.com>
2024-08-23 08:25:30 -0500

    :recycle: refactor(Header Panel) P2-933: update notification badge validation

--- onecgiar-pr-client/src/app/shared/components/header-panel/header-panel.component.ts
+++ onecgiar-pr-client/src/app/shared/components/header-panel/header-panel.component.ts
@@ -14,29 +14,33 @@ export class HeaderPanelComponent implements OnInit {
   internationalizationData = internationalizationData;
   inLocal = (environment as any)?.inLocal;
   mockNotifications = [
-    {
-      init: 'INIT-17',
-      message: '<b>Stewie Griffin submitted the result</b> 6172-The CGIAR Climate Security Observatory (CSO)',
-      date: new Date(new Date().setMinutes(new Date().getMinutes() - 3))
-    },
-    {
-      init: 'INIT-28',
-      message:
-        '<b>Jane Cole from INIT-10 has requested to include INIT-04 as a contributor</b> to result 1050 - Manhat, a participant in the F2R-CWANA Agritech4Morocco Innovation Challenge 2022, was one named in the UAE government\'s "Future 100" list for 2023',
-      date: new Date(new Date().setDate(new Date().getDate() - 2))
-    },
-    {
-      init: 'INIT-17',
-      message:
-        '<b>Mohan Rao from INIT-08 has requested the inclusion of INIT-17 as a contributor</b> to result 7535-Natural regeneration of severely degraded terrestrial arid ecosystems needs more than just removing the cause of the degradation',
-      date: new Date(new Date().setDate(new Date().getDate() - 3))
-    }
+    // {
+    //   init: 'INIT-17',
+    //   message: '<b>Stewie Griffin submitted the result</b> 6172-The CGIAR Climate Security Observatory (CSO)',
+    //   date: new Date(new Date().setMinutes(new Date().getMinutes() - 3))
+    // },
+    // {
+    //   init: 'INIT-28',
+    //   message:
+    //     '<b>Jane Cole from INIT-10 has requested to include INIT-04 as a contributor</b> to result 1050 - Manhat, a participant in the F2R-CWANA Agritech4Morocco Innovation Challenge 2022, was one named in the UAE government\'s "Future 100" list for 2023',
+    //   date: new Date(new Date().setDate(new Date().getDate() - 2))
+    // }
+    // // {
+    // //   init: 'INIT-17',
+    // //   message:
+    // //     '<b>Mohan Rao from INIT-08 has requested the inclusion of INIT-17 as a contributor</b> to result 7535-Natural regeneration of severely degraded terrestrial arid ecosystems needs more than just removing the cause of the degradation',
+    // //   date: new Date(new Date().setDate(new Date().getDate() - 3))
+    // // }
   ];
 
   constructor(public api: ApiService, public dataControlSE: DataControlService, public globalLinksSE: GlobalLinksService, private router: Router) {}
 
   ngOnInit(): void {
     this.api.updateUserData(() => {});
+  }
+
+  notificationBadgeLength() {
+    return `${this.mockNotifications.length}`;
   }
 
   openInfoLink() {
