7dcffc3ed621728496cca93169c0bcb1646696d5
xKeCo <kevcollazos@gmail.com>
2024-09-10 12:14:55 -0500

    :recycle: refactor(Notification Setting) P2-933: update notifications settigns update functionality and notification item styles

--- onecgiar-pr-client/src/app/pages/results/pages/results-outlet/pages/results-notifications/pages/settings/settings.component.spec.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/results-outlet/pages/results-notifications/pages/settings/settings.component.spec.ts
@@ -1,213 +1,141 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { SettingsComponent } from './settings.component';
-import { ApiService } from '../../../../../../../../shared/services/api/api.service';
-import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
-import { ResultsNotificationsService } from '../../results-notifications.service';
-import { Observable, of, throwError } from 'rxjs';
-import { RouterTestingModule } from '@angular/router/testing';
-import { InputSwitchModule } from 'primeng/inputswitch';
-import { AlertStatusComponent } from '../../../../../../../../custom-fields/alert-status/alert-status.component';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { of, throwError } from 'rxjs';
+import { ApiService } from '../../../../../../../../shared/services/api/api.service';
+import { SettingsComponent } from './settings.component';
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
   let fixture: ComponentFixture<SettingsComponent>;
-  let mockApiService: jest.Mocked<ApiService>;
-  let mockRouter: jest.Mocked<Router>;
-  let mockActivatedRoute: Partial<ActivatedRoute>;
-  let mockResultsNotificationsService: jest.Mocked<ResultsNotificationsService>;
+  let apiServiceMock: any;
+  let activatedRouteMock: any;
+  let messageServiceMock: any;
+  let routerMock: any;
 
-  beforeEach(() => {
-    mockApiService = {
+  beforeEach(async () => {
+    apiServiceMock = {
       dataControlSE: {
-        myInitiativesList: [{ id: '1', name: 'Initiative 1' }]
+        myInitiativesList: [{ id: '1' }]
       },
       resultsSE: {
-        GET_userNotificationSettingsByInitiativeId: jest.fn(),
-        PATCH_userNotificationSettingsByInitiativeId: jest.fn()
-      }
-    } as any;
-
-    mockRouter = {
-      navigate: jest.fn()
-    } as any;
-
-    mockActivatedRoute = {
-      snapshot: {
-        queryParams: {},
-        url: [],
-        params: {},
-        fragment: '',
-        data: {},
-        outlet: '',
-        component: undefined,
-        routeConfig: undefined,
-        root: new ActivatedRouteSnapshot(),
-        parent: new ActivatedRouteSnapshot(),
-        firstChild: new ActivatedRouteSnapshot(),
-        children: [],
-        pathFromRoot: [],
-        paramMap: undefined,
-        queryParamMap: undefined
+        GET_userAllNotificationSettings: jest.fn().mockReturnValue(of({ response: [] })),
+        PATCH_userNotificationSettingsByInitiativeId: jest.fn().mockReturnValue(of({}))
       }
     };
 
-    mockResultsNotificationsService = {} as any;
+    activatedRouteMock = {
+      snapshot: {
+        queryParams: {
+          init: '1'
+        }
+      }
+    };
 
-    TestBed.configureTestingModule({
-      declarations: [SettingsComponent, AlertStatusComponent],
-      imports: [RouterTestingModule, InputSwitchModule],
+    messageServiceMock = {
+      add: jest.fn()
+    };
+
+    routerMock = {
+      navigate: jest.fn()
+    };
+
+    await TestBed.configureTestingModule({
+      declarations: [SettingsComponent],
       providers: [
-        { provide: ApiService, useValue: mockApiService },
-        { provide: Router, useValue: mockRouter },
-        { provide: ActivatedRoute, useValue: mockActivatedRoute },
-        { provide: ResultsNotificationsService, useValue: mockResultsNotificationsService },
-        MessageService
+        { provide: ApiService, useValue: apiServiceMock },
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
+        { provide: MessageService, useValue: messageServiceMock },
+        { provide: Router, useValue: routerMock }
       ]
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(SettingsComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('ngOnInit', () => {
-    it('should navigate to settings with first initiative id if no init param', () => {
-      component.ngOnInit();
-      expect(mockRouter.navigate).toHaveBeenCalledWith(['result/results-outlet/results-notifications/settings'], { queryParams: { init: '1' } });
-    });
-
-    it('should call getNotificationSettings if init param exists', () => {
-      mockActivatedRoute.snapshot.queryParams['init'] = '1';
-
-      const spy = jest.spyOn(component, 'getNotificationSettings');
-      const mockGetUserNotificationSettingsSpy = jest.spyOn(mockApiService.resultsSE, 'GET_userNotificationSettingsByInitiativeId').mockReturnValue({
-        subscribe: jest.fn(),
-        source: undefined,
-        operator: undefined,
-        lift: function <R>(operator?: any): Observable<R> {
-          throw new Error('Function not implemented.');
-        },
-        forEach: function (next: (value: any) => void): Promise<void> {
-          throw new Error('Function not implemented.');
-        },
-        pipe: function (): Observable<any> {
-          throw new Error('Function not implemented.');
-        },
-        toPromise: function (): Promise<any> {
-          throw new Error('Function not implemented.');
-        }
-      });
-
-      component.ngOnInit();
-
-      expect(spy).toHaveBeenCalledWith('1');
-      expect(mockGetUserNotificationSettingsSpy).toHaveBeenCalledWith('1');
-    });
+  it('should initialize with correct settings', () => {
+    expect(apiServiceMock.resultsSE.GET_userAllNotificationSettings).toHaveBeenCalled();
+    expect(component.settingsArray.length).toBe(1);
   });
 
-  describe('getNotificationSettings', () => {
-    it('should set notification settings on success', () => {
-      const mockResponse = {
-        response: {
-          email_notifications_contributing_request_enabled: true,
-          email_notifications_updates_enabled: false
-        }
-      };
-      jest.spyOn(mockApiService.resultsSE, 'GET_userNotificationSettingsByInitiativeId').mockReturnValue(of(mockResponse));
+  it('should navigate to settings with myInitiativesList first id when queryParams init is undefined', () => {
+    activatedRouteMock.snapshot.queryParams.init = undefined;
 
-      component.getNotificationSettings('1');
+    component.ngOnInit();
 
-      expect(component.emailNotifications).toBe(true);
-      expect(component.systemNotifications).toBe(false);
-      expect(component.isLoading).toBe(false);
-    });
-
-    it('should handle error', () => {
-      const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation();
-      jest.spyOn(mockApiService.resultsSE, 'GET_userNotificationSettingsByInitiativeId').mockReturnValue(throwError(() => new Error('Test error')));
-
-      component.getNotificationSettings('1');
-
-      expect(consoleErrorSpy).toHaveBeenCalled();
-      expect(component.isLoading).toBe(false);
-    });
+    expect(routerMock.navigate).toHaveBeenCalledWith(['result/results-outlet/results-notifications/settings'], { queryParams: { init: '1' } });
   });
 
-  describe('handleInitiative', () => {
-    it('should not navigate if no initiative', () => {
-      component.handleInitiative(null);
-      expect(mockRouter.navigate).not.toHaveBeenCalled();
-    });
-
-    it('should navigate and get notification settings', () => {
-      const initiative = { id: '2', name: 'Initiative 2' };
-      const getNotificationSettingsSpy = jest.spyOn(component, 'getNotificationSettings');
-      const mockGetUserNotificationSettingsSpy = jest.spyOn(mockApiService.resultsSE, 'GET_userNotificationSettingsByInitiativeId').mockReturnValue({
-        subscribe: jest.fn(),
-        source: undefined,
-        operator: undefined,
-        lift: function <R>(operator?: any): Observable<R> {
-          throw new Error('Function not implemented.');
-        },
-        forEach: function (next: (value: any) => void): Promise<void> {
-          throw new Error('Function not implemented.');
-        },
-        pipe: function (): Observable<any> {
-          throw new Error('Function not implemented.');
-        },
-        toPromise: function (): Promise<any> {
-          throw new Error('Function not implemented.');
-        }
-      });
-
-      component.handleInitiative(initiative);
-
-      expect(mockRouter.navigate).toHaveBeenCalledWith(['result/results-outlet/results-notifications/settings'], { queryParams: { init: '2' } });
-      expect(mockGetUserNotificationSettingsSpy).toHaveBeenCalledWith('2');
-      expect(component.emailNotifications).toBe(false);
-      expect(component.systemNotifications).toBe(false);
-      expect(getNotificationSettingsSpy).toHaveBeenCalledWith('2');
-    });
+  it('should get notification settings when queryParams init is defined', () => {
+    component.ngOnInit();
+    expect(apiServiceMock.resultsSE.GET_userAllNotificationSettings).toHaveBeenCalled();
   });
 
-  describe('onSaveSection', () => {
-    it('should not save notification settings if isSaving or no init param', () => {
-      component.onSaveSection();
-      expect(mockApiService.resultsSE.PATCH_userNotificationSettingsByInitiativeId).not.toHaveBeenCalled();
-    });
+  it('should handle initiative settings change', () => {
+    component.emailNotifications = true;
+    component.systemNotifications = true;
+    component.handleInitiativeSettingsChange();
+    expect(component.settingsArray[0].email_notifications_contributing_request_enabled).toBe(true);
+    expect(component.settingsArray[0].email_notifications_updates_enabled).toBe(true);
+  });
 
-    it('should save notification settings', () => {
-      mockActivatedRoute.snapshot.queryParams['init'] = '1';
-      component.emailNotifications = true;
-      component.systemNotifications = false;
-      const mockResponse = { success: true };
-      jest.spyOn(mockApiService.resultsSE, 'PATCH_userNotificationSettingsByInitiativeId').mockReturnValue(of(mockResponse));
-      const getNotificationSettingsSpy = jest.spyOn(component, 'getNotificationSettings');
+  it('should get notification settings', () => {
+    component.getNotificationSettings('1');
+    expect(component.isLoading).toBe(false);
+    expect(component.settingsArray.length).toBe(1);
+  });
 
-      component.onSaveSection();
+  it('should handle get notification settings error', () => {
+    const spyConsole = jest.spyOn(console, 'error').mockImplementation(() => {});
+    apiServiceMock.resultsSE.GET_userAllNotificationSettings.mockReturnValue(throwError(() => 'error'));
 
-      expect(mockApiService.resultsSE.PATCH_userNotificationSettingsByInitiativeId).toHaveBeenCalledWith({
-        initiative_id: '1',
-        email_notifications_contributing_request_enabled: true,
-        email_notifications_updates_enabled: false
-      });
-      expect(component.isSaving).toBe(false);
-      expect(getNotificationSettingsSpy).toHaveBeenCalledWith('1');
-    });
+    component.getNotificationSettings('1');
 
-    it('should handle error when saving', () => {
-      mockActivatedRoute.snapshot.queryParams['init'] = '1';
-      const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation();
-      jest.spyOn(mockApiService.resultsSE, 'PATCH_userNotificationSettingsByInitiativeId').mockReturnValue(throwError(() => new Error('Test error')));
+    expect(component.isLoading).toBe(false);
+    expect(spyConsole).toHaveBeenCalled();
+  });
 
-      component.onSaveSection();
+  it('should handle initiative', () => {
+    component.handleInitiative({ id: '1' });
+    expect(routerMock.navigate).toHaveBeenCalledWith(['result/results-outlet/results-notifications/settings'], { queryParams: { init: '1' } });
+  });
 
-      expect(consoleErrorSpy).toHaveBeenCalled();
-      expect(component.isSaving).toBe(false);
-    });
+  it('should not handle initiative if initiative is undefined', () => {
+    component.handleInitiative(undefined);
+    expect(routerMock.navigate).not.toHaveBeenCalled();
+  });
+
+  it('should return if it is saving', () => {
+    component.isSaving = true;
+    component.onSaveSection();
+    expect(apiServiceMock.resultsSE.PATCH_userNotificationSettingsByInitiativeId).not.toHaveBeenCalled();
+  });
+
+  it('should save section', () => {
+    component.onSaveSection();
+    expect(apiServiceMock.resultsSE.PATCH_userNotificationSettingsByInitiativeId).toHaveBeenCalled();
+  });
+
+  it('should handle save error', () => {
+    const spyConsole = jest.spyOn(console, 'error').mockImplementation(() => {});
+    apiServiceMock.resultsSE.PATCH_userNotificationSettingsByInitiativeId.mockReturnValue(throwError(() => 'error'));
+
+    component.onSaveSection();
+
+    expect(component.isSaving).toBe(false);
+    expect(spyConsole).toHaveBeenCalled();
+  });
+
+  it('should handle save success', () => {
+    component.handleSaveSuccess();
+    expect(messageServiceMock.add).toHaveBeenCalledWith({ severity: 'success', summary: 'Success', detail: 'Settings saved successfully!' });
   });
 });
