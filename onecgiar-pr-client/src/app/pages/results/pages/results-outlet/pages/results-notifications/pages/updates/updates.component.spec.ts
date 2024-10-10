d668b42537ecebba30a732f30fcd950475d3bdb3
xKeCo <kevcollazos@gmail.com>
2024-10-10 08:30:55 -0500

    ♻️ refactor: Remove unused imports and commented-out styles in component specs

--- onecgiar-pr-client/src/app/pages/results/pages/results-outlet/pages/results-notifications/pages/updates/updates.component.spec.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/results-outlet/pages/results-notifications/pages/updates/updates.component.spec.ts
@@ -2,7 +2,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UpdatesComponent } from './updates.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CustomFieldsModule } from '../../../../../../../../custom-fields/custom-fields.module';
-import { FilterNotificationByInitiativePipe } from '../../pipes/filter-notification-by-initiative.pipe';
 import { NotificationItemModule } from '../../components/notification-item/notification-item.module';
 
 describe('UpdatesComponent', () => {
