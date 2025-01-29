10c66b2d6f880c8e6baac56b7c14fd212cf4d955
xKeCo <kevcollazos@gmail.com>
2025-01-29 11:18:05 -0500

    ✨ feat(outcome-indicator) P2-1240: Add download excel button for End of Initiative and Work Package Outcome Indicators

--- onecgiar-pr-client/src/app/pages/outcome-indicator/pages/eioi-home/eoio-home.component.spec.ts
+++ onecgiar-pr-client/src/app/pages/outcome-indicator/pages/eioi-home/eoio-home.component.spec.ts
@@ -3,20 +3,28 @@ import { EoioHomeComponent } from './eoio-home.component';
 import { CommonModule } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ExportTablesService } from '../../../../shared/services/export-tables.service';
+import { OutcomeIndicatorService } from '../../services/outcome-indicator.service';
+import { MessageService } from 'primeng/api';
+jest.useFakeTimers();
 
 describe('EoioHomeComponent', () => {
   let component: EoioHomeComponent;
   let fixture: ComponentFixture<EoioHomeComponent>;
+  let exportTablesService: ExportTablesService;
+  let outcomeIndicatorService: OutcomeIndicatorService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CommonModule, EoioHomeComponent, HttpClientTestingModule, RouterTestingModule],
-      declarations: []
+      declarations: [],
+      providers: [ExportTablesService, OutcomeIndicatorService, MessageService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(EoioHomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    exportTablesService = TestBed.inject(ExportTablesService);
+    outcomeIndicatorService = TestBed.inject(OutcomeIndicatorService);
   });
 
   it('should create', () => {
@@ -27,5 +35,58 @@ describe('EoioHomeComponent', () => {
     const spy = jest.spyOn(component.outcomeIService.searchText, 'set');
     component.ngOnDestroy();
     expect(spy).toHaveBeenCalledWith('');
+  });
+
+  describe('ngOnInit', () => {
+    it('should set detail section title', () => {
+      const spy = jest.spyOn(component.api.dataControlSE, 'detailSectionTitle');
+      component.ngOnInit();
+      expect(spy).toHaveBeenCalledWith('End of initiative outcome indicators list');
+    });
+  });
+
+  describe('exportProgressEoioExcel', () => {
+    it('should call exportOutcomesIndicatorsToExcel with correct parameters', () => {
+      const exportSpy = jest.spyOn(exportTablesService, 'exportOutcomesIndicatorsToExcel').mockResolvedValue();
+
+      const wscolsEOIs = [
+        { header: 'Outcome', key: 'toc_result_title', width: 50 },
+        { header: 'Indicator', key: 'indicator_name', width: 50 },
+        { header: 'Indicator Type', key: 'indicator_type', width: 50 },
+        { header: 'Expected target', key: 'expected_target', width: 22 },
+        { header: 'Actual target achieved', key: 'actual_target_achieved', width: 30 },
+        { header: 'Achieved status', key: 'achieved_status', width: 22 },
+        { header: 'Reporting status', key: 'reporting_status', width: 22 },
+        { header: 'Narrative', key: 'indicator_achieved_narrative', width: 50 },
+        { header: 'Supporting results', key: 'indicator_supporting_results', width: 60 }
+      ];
+
+      outcomeIndicatorService.eoisData = [{ indicators: [{ indicator_name: 'test' }] }];
+      outcomeIndicatorService.loading.set(false);
+      outcomeIndicatorService.initiativeIdFilter = 'test';
+
+      component.exportProgressEoioExcel();
+      jest.runAllTimers();
+
+      expect(exportSpy).toHaveBeenCalledWith({
+        fileName: `test_EOIO_`,
+        EOIsConfig: {
+          data: outcomeIndicatorService.eoisData,
+          wscols: wscolsEOIs,
+          cellToCenter: [4, 5, 6, 7],
+          worksheetName: 'EoI outcomes'
+        }
+      });
+    });
+
+    it('should not call exportOutcomesIndicatorsToExcel if the conditions are not met', () => {
+      const exportSpy = jest.spyOn(exportTablesService, 'exportOutcomesIndicatorsToExcel').mockResolvedValue();
+      outcomeIndicatorService.eoisData = [];
+      outcomeIndicatorService.loading.set(true);
+
+      component.exportProgressEoioExcel();
+
+      expect(exportSpy).not.toHaveBeenCalled();
+    });
   });
 });
--- onecgiar-pr-client/src/app/pages/outcome-indicator/pages/eioi-home/eoio-home.component.ts
+++ onecgiar-pr-client/src/app/pages/outcome-indicator/pages/eioi-home/eoio-home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { ApiService } from '../../../../shared/services/api/api.service';
 import { ButtonModule } from 'primeng/button';
@@ -9,22 +9,81 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { FilterIndicatorBySearchPipe } from '../../pipes/filter-indicator-by-search.pipe';
 import { FormsModule } from '@angular/forms';
 import { TooltipModule } from 'primeng/tooltip';
+import { CustomFieldsModule } from '../../../../custom-fields/custom-fields.module';
+import { ExportTablesService } from '../../../../shared/services/export-tables.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-eioi-home',
   standalone: true,
-  imports: [CommonModule, TableModule, ButtonModule, CustomSpinnerModule, RouterLink, FilterIndicatorBySearchPipe, FormsModule, TooltipModule],
+  imports: [
+    CommonModule,
+    TableModule,
+    ButtonModule,
+    CustomSpinnerModule,
+    RouterLink,
+    FilterIndicatorBySearchPipe,
+    FormsModule,
+    TooltipModule,
+    CustomFieldsModule
+  ],
+  providers: [MessageService],
   templateUrl: './eoio-home.component.html',
   styleUrl: './eoio-home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EoioHomeComponent implements OnDestroy, OnInit {
+  requesting = signal(false);
   api = inject(ApiService);
   outcomeIService = inject(OutcomeIndicatorService);
   activatedRoute = inject(ActivatedRoute);
+  exportTablesSE = inject(ExportTablesService);
+  messageService = inject(MessageService);
 
   ngOnInit(): void {
     this.api.dataControlSE.detailSectionTitle('End of initiative outcome indicators list');
+  }
+
+  exportProgressEoioExcel() {
+    if (!this.outcomeIService.initiativeIdFilter || this.outcomeIService.loading() || !this.outcomeIService.eoisData) {
+      return;
+    }
+
+    this.requesting.set(true);
+
+    const wscolsEOIs = [
+      { header: 'Outcome', key: 'toc_result_title', width: 50 },
+      { header: 'Indicator', key: 'indicator_name', width: 50 },
+      { header: 'Indicator Type', key: 'indicator_type', width: 50 },
+      { header: 'Expected target', key: 'expected_target', width: 22 },
+      { header: 'Actual target achieved', key: 'actual_target_achieved', width: 30 },
+      { header: 'Achieved status', key: 'achieved_status', width: 22 },
+      { header: 'Reporting status', key: 'reporting_status', width: 22 },
+      { header: 'Narrative', key: 'indicator_achieved_narrative', width: 50 },
+      { header: 'Supporting results', key: 'indicator_supporting_results', width: 60 }
+    ];
+
+    this.exportTablesSE.exportOutcomesIndicatorsToExcel({
+      fileName: `${this.outcomeIService.initiativeIdFilter}_EOIO_`,
+      EOIsConfig: {
+        data: this.outcomeIService.eoisData,
+        wscols: wscolsEOIs,
+        cellToCenter: [4, 5, 6, 7],
+        worksheetName: 'EoI outcomes'
+      }
+    });
+
+    setTimeout(() => {
+      this.requesting.set(false);
+    }, 500);
+
+    this.messageService.add({
+      severity: 'success',
+      summary: 'File exported successfully',
+      detail: 'File exported successfully',
+      key: 'outcomeIndicators',
+      life: 3000
+    });
   }
 
   ngOnDestroy(): void {
