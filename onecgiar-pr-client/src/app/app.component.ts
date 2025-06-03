740af3428ecbf53d81ba220807ec6d8b811f6b3d
xKeCo <kevcollazos@gmail.com>
2025-06-03 12:06:35 -0500

    ♻️ refactor(app) P2-1401: Update conditional rendering for header, navigation, and footer components based on user login status and read-only validation

--- onecgiar-pr-client/src/app/app.component.ts
+++ onecgiar-pr-client/src/app/app.component.ts
@@ -28,8 +28,10 @@ export class AppComponent implements OnInit {
     Hotjar.init(environment.hotjarSiteId, environment.hotjarVersion);
     this.getGlobalParametersByCategory();
     setTimeout(() => {
-      if (!this.AuthService.inLogin()) this.rolesSE.validateReadOnly();
-    }, 500);
+      if (!this.AuthService.inLogin()) {
+        this.rolesSE.validateReadOnly();
+      }
+    }, 150);
 
     this.api.dataControlSE.findClassTenSeconds('pSelectP').then(resp => {
       try {
