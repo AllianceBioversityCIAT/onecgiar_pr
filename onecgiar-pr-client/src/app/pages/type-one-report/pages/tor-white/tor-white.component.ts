f7835eb12809d81a18d8d81384d2c488c12d3e1c
Kevin Collazos <kevcollazos@gmail.com>
2025-06-26 10:22:17 -0500

    ♻️ refactor(T1R): Remove unused WhitePage module and routing configuration

---
+++ onecgiar-pr-client/src/app/pages/type-one-report/pages/tor-white/tor-white.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tor-white',
+  standalone: true,
+  imports: [],
+  templateUrl: './tor-white.component.html',
+  styleUrl: './tor-white.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TorWhiteComponent { }
