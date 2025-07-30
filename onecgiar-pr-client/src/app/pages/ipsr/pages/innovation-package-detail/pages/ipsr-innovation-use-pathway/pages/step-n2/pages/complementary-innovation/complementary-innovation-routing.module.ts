7fe28329332a7e3936516582b2149e660b09ac76
xKeCo <kevcollazos@gmail.com>
2025-07-30 15:40:00 -0500

    🎨 style(General) P2-1539: update SCSS styles to use primary color variables for consistency across components

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n2/pages/complementary-innovation/complementary-innovation-routing.module.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n2/pages/complementary-innovation/complementary-innovation-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ComplementaryInnovationModule } from './complementary-innovation.module';
 import { ComplementaryInnovationComponent } from './complementary-innovation.component';
 
 const routes: Routes = [{ path: '', component: ComplementaryInnovationComponent}];
