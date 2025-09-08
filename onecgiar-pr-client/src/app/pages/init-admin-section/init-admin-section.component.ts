d9788666e6ab8ac578ec707dc2b2c55637d75dc4
xKeCo <kevcollazos@gmail.com>
2025-09-08 08:18:20 -0500

    ♻️ Refactor(init-admin-section) P2-1791: Update section title and routing references to 'Results Monitoring Center'

--- onecgiar-pr-client/src/app/pages/init-admin-section/init-admin-section.component.ts
+++ onecgiar-pr-client/src/app/pages/init-admin-section/init-admin-section.component.ts
@@ -3,19 +3,22 @@ import { ApiService } from '../../shared/services/api/api.service';
 import { DataControlService } from '../../shared/services/data-control.service';
 
 @Component({
-    selector: 'app-init-admin-section',
-    templateUrl: './init-admin-section.component.html',
-    styleUrls: ['./init-admin-section.component.scss'],
-    standalone: false
+  selector: 'app-init-admin-section',
+  templateUrl: './init-admin-section.component.html',
+  styleUrls: ['./init-admin-section.component.scss'],
+  standalone: false
 })
 export class InitAdminSectionComponent implements OnInit {
   sections = [
     { name: 'Completeness status', icon: 'check_circle', path: '/init-admin-module/init-completeness-status' },
     { name: 'General results report', icon: 'task', path: '/init-admin-module/init-general-results-report' }
   ];
-  constructor(private dataControlSE: DataControlService, public api: ApiService) {}
+  constructor(
+    private dataControlSE: DataControlService,
+    public api: ApiService
+  ) {}
 
   ngOnInit(): void {
-    this.dataControlSE.detailSectionTitle('INIT Admin Module');
+    this.dataControlSE.detailSectionTitle('Results Monitoring Center');
   }
 }
