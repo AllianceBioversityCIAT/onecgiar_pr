2458356070fa7bd6646897820b0ab707da307b0f
xKeCo <kevcollazos@gmail.com>
2025-10-01 16:27:51 -0500

    ✨feat(results-framework-reporting-home) P2-1874: Add loading skeletons for science programs and recent activity in result framework reporting

--- onecgiar-pr-client/src/app/pages/result-framework-reporting/pages/result-framework-reporting-home/result-framework-reporting-home.component.ts
+++ onecgiar-pr-client/src/app/pages/result-framework-reporting/pages/result-framework-reporting-home/result-framework-reporting-home.component.ts
@@ -5,10 +5,11 @@ import { ApiService } from '../../../../shared/services/api/api.service';
 import { ResultFrameworkReportingCardItemComponent } from './components/result-framework-reporting-card-item/result-framework-reporting-card-item.component';
 import { ResultFrameworkReportingRecentItemComponent } from './components/result-framework-reporting-recent-item/result-framework-reporting-recent-item.component';
 import { ResultFrameworkReportingHomeService } from './services/result-framework-reporting-home.service';
+import { SkeletonModule } from 'primeng/skeleton';
 
 @Component({
   selector: 'app-result-framework-reporting-home',
-  imports: [CommonModule, ProgressBarModule, ResultFrameworkReportingCardItemComponent, ResultFrameworkReportingRecentItemComponent],
+  imports: [CommonModule, ProgressBarModule, ResultFrameworkReportingCardItemComponent, ResultFrameworkReportingRecentItemComponent, SkeletonModule],
   templateUrl: './result-framework-reporting-home.component.html',
   styleUrl: './result-framework-reporting-home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
