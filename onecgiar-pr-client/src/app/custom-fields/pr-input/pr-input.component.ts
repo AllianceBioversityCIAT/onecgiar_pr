ad457a85752348872d9930597e6ebeeb826c79a6
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-10-06 15:48:41 -0500

    ♻️ Refactor(pr-input) P2-1880: Implement signal-based rendering and enhance field management for improved clarity

--- onecgiar-pr-client/src/app/custom-fields/pr-input/pr-input.component.ts
+++ onecgiar-pr-client/src/app/custom-fields/pr-input/pr-input.component.ts
@@ -1,8 +1,9 @@
-import { Component, forwardRef, Input } from '@angular/core';
+import { Component, computed, forwardRef, inject, Input } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { WordCounterService } from '../../shared/services/word-counter.service';
 import { RolesService } from '../../shared/services/global/roles.service';
 import { DataControlService } from '../../shared/services/data-control.service';
+import { FieldsManagerService } from '../../shared/services/fields-manager.service';
 
 @Component({
   selector: 'app-pr-input',
@@ -39,11 +40,23 @@ export class PrInputComponent implements ControlValueAccessor {
   @Input() showDescription?: boolean = true;
   @Input() InlineStyles?: string = '';
   @Input() descInlineStyles?: string = '';
+  @Input() fieldRef: string | number;
+
+  fieldsManager = inject(FieldsManagerService);
 
   private _value: any;
   private beforeValue: string;
   public wordCount: number = 0;
   public notProvidedText = "<div class='text-red-100 italic'>Not provided</div>";
+
+  preventFieldRender = computed<boolean>(() => {
+    if (!this.fieldRef) return true;
+    const { show, label, placeholder, description } = this.fieldsManager.fields()[this.fieldRef] || {};
+    this.label = label;
+    this.placeholder = placeholder;
+    this.description = description;
+    return show;
+  });
 
   constructor(
     private readonly wordCounterSE: WordCounterService,
