9db46bca6cf160446a59c28a9851e93c51ab7a37
Julian Sanchez <julian.sanchez@cgiar.org>
2025-10-09 08:26:19 -0500

    ♻️ Refactor(user-management): Update app role display logic and enhance styling for admin role

--- onecgiar-pr-client/src/app/pages/admin-section/pages/user-management/user-management.component.ts
+++ onecgiar-pr-client/src/app/pages/admin-section/pages/user-management/user-management.component.ts
@@ -205,7 +205,7 @@ export default class UserManagementComponent implements OnInit, OnDestroy {
   columns: UserColumn[] = [
     { label: 'User name', key: 'firstName', width: '200px' },
     { label: 'Email', key: 'emailAddress', width: '300px' },
-    { label: 'Platform role', key: 'platformRole', width: '200px' },
+    { label: 'Platform role', key: 'appRole', width: '200px' },
     { label: 'Reporting roles', key: 'entities', width: '120px' },
     { label: 'Is CGIAR', key: 'isCGIAR', width: '120px' },
     { label: 'User creation date', key: 'userCreationDate', width: '180px' },
