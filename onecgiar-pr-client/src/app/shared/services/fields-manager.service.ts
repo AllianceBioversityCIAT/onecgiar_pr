51388cd32ead985b3ab676efe04b88650729525a
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-10-10 15:27:40 -0500

    ♻️ Refactor(rd-general-information, fields-manager): Remove hardcoded impact area label from template and centralize in fields manager service

--- onecgiar-pr-client/src/app/shared/services/fields-manager.service.ts
+++ onecgiar-pr-client/src/app/shared/services/fields-manager.service.ts
@@ -14,6 +14,9 @@ export class FieldsManagerService {
   portfolioAcronym = signal('P25');
   isP25 = computed(() => Portfolios[this.portfolioAcronym()] == Portfolios.P25);
   isP22 = computed(() => Portfolios[this.portfolioAcronym()] == Portfolios.P22);
+
+  scoresImpactAreaLabel = 'Which component of the Impact Area is this result intended to impact?';
+
   fields = computed<Record<string, CustomField>>(() => {
     const fields: Record<string, CustomField> = {
       '[general-info]-title': {
@@ -47,20 +50,40 @@ export class FieldsManagerService {
         label: 'Is this result featured in a Key Result Story for the reporting year?',
         hide: this.isP25()
       },
+      //? score 1
       '[general-info]-gender_tag_id': {
         label: this.isP25() ? 'Gender equality, youth and social inclusion tag' : 'Gender equality score'
       },
+      '[general-info]-gender_impact_area_id': {
+        label: this.scoresImpactAreaLabel
+      },
+      //? score 2
       '[general-info]-climate_change_tag_id': {
         label: this.isP25() ? 'Climate adaptation and mitigation tag' : 'Climate change score'
       },
+      '[general-info]-climate_impact_area_id': {
+        label: this.scoresImpactAreaLabel
+      },
+      //? score 3
       '[general-info]-nutrition_tag_level_id': {
         label: `Nutrition, health and food security ${this.isP25() ? 'tag' : 'score'}`
       },
+      '[general-info]-nutrition_impact_area_id': {
+        label: this.scoresImpactAreaLabel
+      },
+      //? score 4
       '[general-info]-environmental_biodiversity_tag_level_id': {
         label: `Environmental health and biodiversity ${this.isP25() ? 'tag' : 'score'}`
       },
+      '[general-info]-environmental_biodiversity_impact_area_id': {
+        label: this.scoresImpactAreaLabel
+      },
+      //? score 5
       '[general-info]-poverty_tag_level_id': {
         label: `Poverty reduction, livelihoods and jobs ${this.isP25() ? 'tag' : 'score'}`
+      },
+      '[general-info]-poverty_impact_area_id': {
+        label: this.scoresImpactAreaLabel
       }
     };
     return fields;
