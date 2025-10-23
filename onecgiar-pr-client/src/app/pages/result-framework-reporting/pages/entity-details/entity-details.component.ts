20ca07ac9509989541672e6684ef9f98ef2050ef
xKeCo <kevcollazos@gmail.com>
2025-10-23 11:09:11 -0500

    ✨ feat(entity-details) P2-1979: Add bar charts for Outputs and Outcomes in entity details
    Introduces bar charts for Outputs and Outcomes using chart.js and chartjs-plugin-datalabels in the entity details page. Updates HTML and SCSS for new chart layout and styling, and adds chart data and configuration logic to the component. Also adds chartjs-plugin-datalabels as a dependency.

--- onecgiar-pr-client/src/app/pages/result-framework-reporting/pages/entity-details/entity-details.component.ts
+++ onecgiar-pr-client/src/app/pages/result-framework-reporting/pages/entity-details/entity-details.component.ts
@@ -1,7 +1,7 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { ApiService } from '../../../../shared/services/api/api.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { SelectModule } from 'primeng/select';
 import { FormsModule } from '@angular/forms';
 import { ProgressBarModule } from 'primeng/progressbar';
@@ -9,6 +9,9 @@ import { EntityAowCardComponent } from './components/entity-aow-card/entity-aow-
 import { EntityResultsByIndicatorCategoryCardComponent } from './components/entity-results-by-indicator-category-card/entity-results-by-indicator-category-card.component';
 import { EntityAowService } from '../entity-aow/services/entity-aow.service';
 import { SkeletonModule } from 'primeng/skeleton';
+import { ChartModule } from 'primeng/chart';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-entity-details',
@@ -20,7 +23,8 @@ import { SkeletonModule } from 'primeng/skeleton';
     ProgressBarModule,
     EntityAowCardComponent,
     EntityResultsByIndicatorCategoryCardComponent,
-    SkeletonModule
+    SkeletonModule,
+    ChartModule
   ],
   templateUrl: './entity-details.component.html',
   styleUrl: './entity-details.component.scss',
@@ -30,6 +34,8 @@ export class EntityDetailsComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
   api = inject(ApiService);
   entityAowService = inject(EntityAowService);
+
+  cd = inject(ChangeDetectorRef);
 
   summaryInsightsData = signal([
     {
@@ -54,5 +60,121 @@ export class EntityDetailsComponent implements OnInit {
       this.entityAowService.entityId.set(params['entityId']);
     });
     this.entityAowService.getAllDetailsData();
+    this.initChart();
+  }
+
+  dataOutputs: any;
+  dataOutcomes: any;
+  options: any;
+
+  platformId = inject(PLATFORM_ID);
+
+  initChart() {
+    if (isPlatformBrowser(this.platformId)) {
+      Chart.register(ChartDataLabels);
+
+      this.dataOutputs = {
+        labels: ['Knowledge product', 'Innovation development', 'Capacity sharing for development', 'Other output'],
+        datasets: [
+          {
+            type: 'bar',
+            label: 'Editing',
+            backgroundColor: '#60a5fa',
+            data: [42, 30, 10, 5]
+          },
+          {
+            type: 'bar',
+            label: 'Submitted',
+            backgroundColor: '#8e9be8',
+            data: [25, 18, 12, 0]
+          },
+          {
+            type: 'bar',
+            label: 'Quality assessed',
+            backgroundColor: '#5569dd',
+            data: [5, 4, 10, 1]
+          }
+        ]
+      };
+
+      this.dataOutcomes = {
+        labels: ['Policy change', 'Innovation use', 'Other outcome'],
+        datasets: [
+          {
+            type: 'bar',
+            label: 'Editing',
+            backgroundColor: '#60a5fa',
+            data: [14, 15, 10]
+          },
+          {
+            type: 'bar',
+            label: 'Submitted',
+            backgroundColor: '#8e9be8',
+            data: [17, 18, 12]
+          },
+          {
+            type: 'bar',
+            label: 'Quality assessed',
+            backgroundColor: '#5569dd',
+            data: [5, 4, 10]
+          }
+        ]
+      };
+
+      this.options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        aspectRatio: 0.8,
+        indexAxis: 'y',
+        plugins: {
+          tooltip: {
+            mode: 'index',
+            intersect: true
+          },
+          datalabels: {
+            color: '#fff',
+            font: {
+              weight: '400',
+              size: 9
+            },
+            formatter: (value: number) => {
+              return value > 1 ? value : '';
+            },
+            anchor: 'center',
+            align: 'center'
+          },
+          legend: {
+            labels: {
+              boxWidth: 10,
+              font: {
+                size: 8
+              }
+            }
+          }
+        },
+        scales: {
+          x: {
+            stacked: true,
+            ticks: {
+              font: {
+                size: 8
+              },
+              padding: 0,
+              minRotation: 45
+            }
+          },
+          y: {
+            stacked: true,
+            ticks: {
+              font: {
+                size: 8
+              },
+              padding: 0
+            }
+          }
+        }
+      };
+      this.cd.markForCheck();
+    }
   }
 }
