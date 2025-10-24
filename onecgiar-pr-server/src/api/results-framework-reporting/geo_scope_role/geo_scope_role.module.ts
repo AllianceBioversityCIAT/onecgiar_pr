303dfabcc16361f9c5b12f70d47693ded9249446
Natalia Higuita <n.higuita@cgiar.org>
2025-10-24 11:25:52 -0500

    ✨ Remove GeoScopeRoleController and GeoScopeRoleService for codebase simplification

--- onecgiar-pr-server/src/api/results-framework-reporting/geo_scope_role/geo_scope_role.module.ts
+++ onecgiar-pr-server/src/api/results-framework-reporting/geo_scope_role/geo_scope_role.module.ts
@@ -1,9 +1,5 @@
 import { Module } from '@nestjs/common';
-import { GeoScopeRoleService } from './geo_scope_role.service';
-import { GeoScopeRoleController } from './geo_scope_role.controller';
 
 @Module({
-  controllers: [GeoScopeRoleController],
-  providers: [GeoScopeRoleService],
 })
 export class GeoScopeRoleModule {}
