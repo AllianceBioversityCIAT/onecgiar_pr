0095b0199f192ba8df5460e12aff13cf78f25eb2
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-11-07 11:39:58 -0500

    ✨ Enhance AI review component with detailed sections for title, description, and innovation short title

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/components/ai-review/ai-review.component.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/components/ai-review/ai-review.component.ts
@@ -1,13 +1,22 @@
 import { Component, inject } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { AiReviewService } from '../../../../../../shared/services/api/ai-review.service';
+import { CustomFieldsModule } from '../../../../../../custom-fields/custom-fields.module';
+
 @Component({
   selector: 'app-ai-review',
-  imports: [DialogModule, ButtonModule],
+  imports: [DialogModule, ButtonModule, FormsModule, CommonModule, CustomFieldsModule],
   templateUrl: './ai-review.component.html',
   styleUrl: './ai-review.component.scss'
 })
 export class AiReviewComponent {
   aiReviewSE = inject(AiReviewService);
+
+  // Field values
+  titleCurrentVersion = 'Small-scale Fisheries and Aquaculture Ontology';
+  descriptionCurrentVersion = 'This result introduces a conceptual framework for the sustainable management of small-scale fisheries and aquaculture. It establishes a standardized ontology to classify species, practices, and environmental factors, supporting evidence-based decisions that strengthen local food systems, protect marine biodiversity, and sustain coastal livelihoods.';
+  innovationShortTitleCurrentVersion = '';
 }
