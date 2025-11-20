deae29409ccf6eb05b24e372ba790b1836c73eba
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-11-20 10:09:34 -0500

    ♻️ Refactor GreenChecksService to remove setTimeout and improve response handling

--- onecgiar-pr-client/src/app/shared/services/global/green-checks.service.ts
+++ onecgiar-pr-client/src/app/shared/services/global/green-checks.service.ts
@@ -15,27 +15,18 @@ export class GreenChecksService {
     private readonly fieldsManagerSE: FieldsManagerService
   ) {}
 
-  updateGreenChecks() {
-    setTimeout(() => {
-      if (this.resultsApiSE.currentResultId) {
-        this.api.resultsSE.PATCH_greenChecksByResultId().subscribe(resp => {
-          this.api.dataControlSE.green_checks = resp.response.green_checks;
-          this.submit = Boolean(resp.response?.submit);
-        });
-      }
-    }, 10);
-  }
-
   getGreenChecks() {
     if (this.resultsApiSE.currentResultId) {
       if (this.fieldsManagerSE.isP25()) {
         this.api.resultsSE.GET_p25GreenChecksByResultId().subscribe(({ response }) => {
           this.api.dataControlSE.green_checks = response.green_checks;
+          console.log(response);
           this.submit = response.submit;
         });
       } else {
         this.api.resultsSE.GET_greenChecksByResultId().subscribe(({ response }) => {
           this.api.dataControlSE.green_checks = response.green_checks;
+          console.log(response);
           this.submit = response.submit;
         });
       }
