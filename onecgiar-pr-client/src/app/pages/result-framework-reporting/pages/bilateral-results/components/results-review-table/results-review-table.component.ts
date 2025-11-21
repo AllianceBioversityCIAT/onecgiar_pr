c584bddeae156420a69eb1eb72eb117db6795f44
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-11-21 10:54:26 -0500

    ✨ Add ResultsReviewTable component with template, styles, and tests

---
+++ onecgiar-pr-client/src/app/pages/result-framework-reporting/pages/bilateral-results/components/results-review-table/results-review-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-results-review-table',
+  imports: [],
+  templateUrl: './results-review-table.component.html',
+  styleUrl: './results-review-table.component.scss'
+})
+export class ResultsReviewTableComponent {
+
+}
