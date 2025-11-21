0154fe690af6316bb2a7c30472616890f2d22a8c
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-11-21 09:48:37 -0500

    ✨ Update Bilateral Results component to include indicators sidebar and results review filters

--- onecgiar-pr-client/src/app/pages/result-framework-reporting/pages/bilateral-results/bilateral-results.component.ts
+++ onecgiar-pr-client/src/app/pages/result-framework-reporting/pages/bilateral-results/bilateral-results.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { PageHeaderComponent } from '../../../../shared/components/page-header/page-header.component';
+import { IndicatorsSidebarComponent } from './components/indicators-sidebar/indicators-sidebar.component';
+import { ResultsReviewContainerComponent } from './components/results-review-container/results-review-container.component';
+import { ResultsReviewFiltersComponent } from './components/results-review-filters/results-review-filters.component';
 
 @Component({
   selector: 'app-bilateral-results',
-  imports: [PageHeaderComponent],
+  imports: [PageHeaderComponent, IndicatorsSidebarComponent, ResultsReviewContainerComponent, ResultsReviewFiltersComponent],
   templateUrl: './bilateral-results.component.html',
   styleUrl: './bilateral-results.component.scss'
 })
