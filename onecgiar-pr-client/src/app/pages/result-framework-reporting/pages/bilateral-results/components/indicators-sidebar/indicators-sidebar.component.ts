90d58a8610644d2797ae329f5da7a83e78ee6b33
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-11-21 09:52:36 -0500

    ♻️ Refactor Bilateral Results component to use attribute selector for indicators sidebar and update results review container structure

--- onecgiar-pr-client/src/app/pages/result-framework-reporting/pages/bilateral-results/components/indicators-sidebar/indicators-sidebar.component.ts
+++ onecgiar-pr-client/src/app/pages/result-framework-reporting/pages/bilateral-results/components/indicators-sidebar/indicators-sidebar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-indicators-sidebar',
+  selector: '[app-indicators-sidebar]',
   imports: [],
   templateUrl: './indicators-sidebar.component.html',
   styleUrl: './indicators-sidebar.component.scss'
