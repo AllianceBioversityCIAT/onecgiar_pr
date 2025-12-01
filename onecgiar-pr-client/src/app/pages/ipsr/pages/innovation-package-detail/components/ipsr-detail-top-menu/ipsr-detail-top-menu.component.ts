153c013553b58dfa0b6198a912a44197936d26b3
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-12-01 15:26:36 -0500

    ♻️ Refactor ipsr-detail-top-menu component to utilize computed signals for menu options filtering and improve template structure

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/components/ipsr-detail-top-menu/ipsr-detail-top-menu.component.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/components/ipsr-detail-top-menu/ipsr-detail-top-menu.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { IPSRDetailRouting } from '../../../router/routing-data-ipsr';
 import { IpsrDataControlService } from '../../../../services/ipsr-data-control.service';
+import { FieldsManagerService } from '../../../../../../shared/services/fields-manager.service';
 
 @Component({
-    selector: 'app-ipsr-detail-top-menu',
-    templateUrl: './ipsr-detail-top-menu.component.html',
-    styleUrls: ['./ipsr-detail-top-menu.component.scss'],
-    standalone: false
+  selector: 'app-ipsr-detail-top-menu',
+  templateUrl: './ipsr-detail-top-menu.component.html',
+  styleUrls: ['./ipsr-detail-top-menu.component.scss'],
+  standalone: false
 })
 export class IpsrDetailTopMenuComponent {
-  menuOptions = IPSRDetailRouting;
+  fieldsManagerSE = inject(FieldsManagerService);
+  menuOptions = computed(() =>
+    IPSRDetailRouting.filter(option => {
+      console.log(option.path);
+      if (this.fieldsManagerSE.isP25() && option.path == 'link-to-results') return false;
+      return true;
+    })
+  );
   constructor(public ipsrDataControlSE: IpsrDataControlService) {}
 }
