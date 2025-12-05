5b7fc0bfae73c25e2ae8118108e1feb97eca5c64
Julian Sanchez <julian.sanchez@cgiar.org>
2025-12-05 01:48:25 -0500

    ♻️ Update validation feedback and required fields in multiple components for improved user experience

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-result-types-pages/policy-change-info/policy-change-info.module.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/pages/rd-result-types-pages/policy-change-info/policy-change-info.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { PolicyChangeInfoRoutingModule } from './policy-change-info-routing.module';
 import { PolicyChangeInfoComponent } from './policy-change-info.component';
 import { CustomFieldsModule } from '../../../../../../../custom-fields/custom-fields.module';
+import { FeedbackValidationDirectiveModule } from '../../../../../../../shared/directives/feedback-validation-directive.module';
 
 @NgModule({
   declarations: [PolicyChangeInfoComponent],
-  imports: [CommonModule, PolicyChangeInfoRoutingModule, CustomFieldsModule]
+  imports: [CommonModule, PolicyChangeInfoRoutingModule, CustomFieldsModule, FeedbackValidationDirectiveModule]
 })
 export class PolicyChangeInfoModule {}
