0d75f32dee0bcaade5c0054de05b7f69622994b5
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-12-10 14:51:44 -0500

    ♻️ Refactor step-n4 investment tables to implement conditional rendering for headers based on P25 status

--- onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n4/components/step-n4-partner-co-investment-table/step-n4-partner-co-investment-table.component.ts
+++ onecgiar-pr-client/src/app/pages/ipsr/pages/innovation-package-detail/pages/ipsr-innovation-use-pathway/pages/step-n4/components/step-n4-partner-co-investment-table/step-n4-partner-co-investment-table.component.ts
@@ -1,17 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { IpsrStep4Body } from '../../model/Ipsr-step-4-body.model';
 import { RolesService } from '../../../../../../../../../../shared/services/global/roles.service';
+import { FieldsManagerService } from '../../../../../../../../../../shared/services/fields-manager.service';
 
 @Component({
-    selector: 'app-step-n4-partner-co-investment-table',
-    templateUrl: './step-n4-partner-co-investment-table.component.html',
-    styleUrls: ['./step-n4-partner-co-investment-table.component.scss'],
-    standalone: false
+  selector: 'app-step-n4-partner-co-investment-table',
+  templateUrl: './step-n4-partner-co-investment-table.component.html',
+  styleUrls: ['./step-n4-partner-co-investment-table.component.scss'],
+  standalone: false
 })
 export class StepN4PartnerCoInvestmentTableComponent {
   @Input() body = new IpsrStep4Body();
   @Input() disabledOptionsPartners = [];
 
+  fieldsManagerSE = inject(FieldsManagerService);
   constructor(public rolesSE: RolesService) {}
 
   deletePartner(partner) {
