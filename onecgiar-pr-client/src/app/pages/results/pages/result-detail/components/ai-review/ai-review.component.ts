7bae0ec68ea58d40b092d0e463874b492bd7773b
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-12-12 10:54:44 -0500

    ♻️ Refactor AI review component: Update result version options and integrate score service

--- onecgiar-pr-client/src/app/pages/results/pages/result-detail/components/ai-review/ai-review.component.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/result-detail/components/ai-review/ai-review.component.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AiReviewService } from '../../../../../../shared/services/api/ai-review.service';
 import { CustomFieldsModule } from '../../../../../../custom-fields/custom-fields.module';
+import { ScoreService } from '../../../../../../shared/services/global/score.service';
 
 @Component({
   selector: 'app-ai-review',
@@ -14,19 +15,13 @@ import { CustomFieldsModule } from '../../../../../../custom-fields/custom-field
 })
 export class AiReviewComponent {
   aiReviewSE = inject(AiReviewService);
+  scoreSE = inject(ScoreService);
 
   // Field values
   titleCurrentVersion = 'Small-scale Fisheries and Aquaculture Ontology';
   descriptionCurrentVersion =
     'This result introduces a conceptual framework for the sustainable management of small-scale fisheries and aquaculture. It establishes a standardized ontology to classify species, practices, and environmental factors, supporting evidence-based decisions that strengthen local food systems, protect marine biodiversity, and sustain coastal livelihoods.';
   innovationShortTitleCurrentVersion = '';
-
-  // Result version options
-  resultVersionOptions = [
-    { id: '0', full_name: '(0) Targeted' },
-    { id: '1', full_name: '(1) Significant' },
-    { id: '2', full_name: '(2) Principal' }
-  ];
 
   moveTextToInput(field: any) {
     field.canSave = true;
