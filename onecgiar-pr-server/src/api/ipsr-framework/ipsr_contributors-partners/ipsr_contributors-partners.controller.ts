6ec79480f94fe0e72e8600cf6d9e2453ca00d75e
Juan D. Guzmán <juandelgadog98@gmail.com>
2025-12-16 10:33:16 -0500

    ✨ Refactor IPSR Contributors and Partners Module: Consolidate routes, enhance controller with versioning, and expand service functionality for contributors and partners management.

--- onecgiar-pr-server/src/api/ipsr-framework/ipsr_contributors-partners/ipsr_contributors-partners.controller.ts
+++ onecgiar-pr-server/src/api/ipsr-framework/ipsr_contributors-partners/ipsr_contributors-partners.controller.ts
@@ -1,13 +1,46 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  UseInterceptors,
+  Version,
+} from '@nestjs/common';
 import { IpsrContributorsPartnersService } from './ipsr_contributors-partners.service';
 import { ApiTags } from '@nestjs/swagger';
 import { ResponseInterceptor } from '../../../shared/Interceptors/Return-data.interceptor';
+import { UserToken } from '../../../shared/decorators/user-token.decorator';
+import { TokenDto } from '../../../shared/globalInterfaces/token.dto';
+import { UpdateContributorsPartnersDto } from './dto/update-contributors-partners.dto';
 
-@ApiTags('AI Review')
+@Controller()
 @UseInterceptors(ResponseInterceptor)
-@Controller('ipsr-contributors-partners')
+@ApiTags('IPSR Framework - Contributors and Partners')
 export class IpsrContributorsPartnersController {
   constructor(
     private readonly ipsrContributorsPartnersService: IpsrContributorsPartnersService,
   ) {}
+
+  @Version('2')
+  @Get(':resultId')
+  getContributorsPartners(@Param('resultId') resultId: number) {
+    return this.ipsrContributorsPartnersService.getContributorsPartnersByResultId(
+      resultId,
+    );
+  }
+
+  @Version('2')
+  @Patch(':resultId')
+  updateContributorsPartners(
+    @Param('resultId') resultId: number,
+    @Body() body: UpdateContributorsPartnersDto,
+    @UserToken() user: TokenDto,
+  ) {
+    return this.ipsrContributorsPartnersService.updateContributorsAndPartners(
+      resultId,
+      body,
+      user,
+    );
+  }
 }
