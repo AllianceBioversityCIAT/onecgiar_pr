e225b2254ea4c21e2118f3cd62c38984befb6dd5
Kevin Collazos <kevcollazos@gmail.com>
2026-01-07 08:40:10 -0500

    ♻️ Refactor(quality-assurance): Simplify initiative selection logic and improve iframe handling

--- onecgiar-pr-client/src/app/pages/quality-assurance/quality-assurance.component.ts
+++ onecgiar-pr-client/src/app/pages/quality-assurance/quality-assurance.component.ts
@@ -1,25 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../shared/services/api/api.service';
 import { ResultLevelService } from '../results/pages/result-creator/services/result-level.service';
-import { DomSanitizer, Title } from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
 import { QualityAssuranceService } from './quality-assurance.service';
 
 @Component({
-    selector: 'app-quality-assurance',
-    templateUrl: './quality-assurance.component.html',
-    styleUrls: ['./quality-assurance.component.scss'],
-    standalone: false
+  selector: 'app-quality-assurance',
+  templateUrl: './quality-assurance.component.html',
+  styleUrls: ['./quality-assurance.component.scss'],
+  standalone: false
 })
 export class QualityAssuranceComponent implements OnInit {
-  constructor(public api: ApiService, public resultLevelSE: ResultLevelService, public sanitizer: DomSanitizer, private qaSE: QualityAssuranceService) {}
+  constructor(
+    public api: ApiService,
+    public resultLevelSE: ResultLevelService,
+    public sanitizer: DomSanitizer,
+    private qaSE: QualityAssuranceService
+  ) {}
   allInitiatives = [];
   clarisaQaToken = null;
   official_code = null;
   showIframe = false;
   qaUrl = environment.qaUrl;
   sanitizedUrl: any = null;
+
   ngOnInit(): void {
     this.api.rolesSE.validateReadOnly();
     this.api.dataControlSE.detailSectionTitle('Quality Assurance');
@@ -43,22 +49,27 @@ export class QualityAssuranceComponent implements OnInit {
 
   GET_AllInitiatives() {
     if (!this.api.rolesSE.isAdmin) return;
-    this.api.resultsSE.GET_AllInitiatives().subscribe(({ response }) => {
+
+    const activePortfolio = this.api.dataControlSE?.reportingCurrentPhase?.portfolioAcronym;
+
+    this.api.resultsSE.GET_AllInitiatives(activePortfolio).subscribe(({ response }) => {
       this.allInitiatives = response;
       this.official_code = this.allInitiatives[0]?.official_code;
       if (this.official_code) this.selectOptionEvent({ official_code: this.official_code });
     });
   }
+
   GET_ClarisaQaToken(callback) {
-    this.api.resultsSE.GET_ClarisaQaToken(this.official_code).subscribe(
-      resp => {
-        this.clarisaQaToken = resp?.response?.token;
+    this.api.resultsSE.GET_ClarisaQaToken(this.official_code).subscribe({
+      next: ({ response }) => {
+        this.clarisaQaToken = response?.token;
         callback();
       },
-      err => {
+      error: err => {
+        console.error(err);
         callback();
       }
-    );
+    });
   }
 
   selectOptionEvent(option) {
