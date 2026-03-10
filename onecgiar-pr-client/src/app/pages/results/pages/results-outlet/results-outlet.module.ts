33ddc9c27db1f767f49d6f4fbdeb137be8a915fc
xKeCo <kevcollazos@gmail.com>
2026-03-10 09:11:26 -0500

    ♻️ refactor(global alert): Update entity-aow.service and integrate AlertGlobalInfo component in reporting pages

--- onecgiar-pr-client/src/app/pages/results/pages/results-outlet/results-outlet.module.ts
+++ onecgiar-pr-client/src/app/pages/results/pages/results-outlet/results-outlet.module.ts
@@ -5,9 +5,10 @@ import { ResultsOutletRoutingModule } from './results-outlet-routing.module';
 import { ResultsOutletComponent } from './results-outlet.component';
 import { CustomFieldsModule } from '../../../../custom-fields/custom-fields.module';
 import { PageHeaderComponent } from '../../../../shared/components/page-header/page-header.component';
+import { AlertGlobalInfoModule } from '../../../../shared/components/alert-global-info/alert-global-info.module';
 
 @NgModule({
   declarations: [ResultsOutletComponent],
-  imports: [CommonModule, ResultsOutletRoutingModule, CustomFieldsModule, PageHeaderComponent, PageHeaderComponent]
+  imports: [CommonModule, ResultsOutletRoutingModule, CustomFieldsModule, PageHeaderComponent, PageHeaderComponent, AlertGlobalInfoModule]
 })
 export class ResultsOutletModule {}
